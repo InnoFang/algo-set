994a591d6e3a7e7477389197693da0e0d599ff32
InnoFang <q984941707@qq.com>
2017-03-29 15:32:44 +0800

    add Selection Sort

---
+++ src/sort/SelectionSort.java
@@ -0,0 +1,18 @@
+package sort;
+
+import utils.compare.Comparable;
+
+/**
+ * Created by Inno Fang on 2017/3/29.
+ */
+public class SelectionSort extends ISort {
+
+    public SelectionSort(Comparable comparable) {
+        super(comparable);
+    }
+
+    @Override
+    public <T> void sort(T[] arr) {
+
+    }
+}
