1ef58f4df2b15d6a3ed48db1dec54bf424d6b6f7
InnoFang <q984941707@qq.com>
2017-03-30 15:05:35 +0800

    add Insertion Sort.

---
+++ src/sort/impl/InsertionSort.java
@@ -0,0 +1,19 @@
+package sort.impl;
+
+import sort.ISort;
+import utils.compare.Comparable;
+
+/**
+ * Created by Inno Fang on 2017/3/30.
+ *
+ */
+public class InsertionSort extends ISort {
+    public InsertionSort(Comparable comparable) {
+        super(comparable);
+    }
+
+    @Override
+    public <T> void sort(T[] arr) {
+
+    }
+}
