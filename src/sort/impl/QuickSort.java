be39a185a91cce9749e4d291d74643aa80c951e9
InnoFang <q984941707@qq.com>
2017-03-31 19:50:35 +0800

    add Quick Sort.

---
+++ src/sort/impl/QuickSort.java
@@ -0,0 +1,18 @@
+package sort.impl;
+
+import sort.ISort;
+import utils.compare.Comparable;
+
+/**
+ * Created by Inno Fang on 2017/3/31.
+ */
+public class QuickSort extends ISort {
+    public QuickSort(Comparable comparable) {
+        super(comparable);
+    }
+
+    @Override
+    public <T> void sort(T[] arr) {
+
+    }
+}
