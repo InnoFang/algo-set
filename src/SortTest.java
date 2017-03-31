f415defeb77f5504382efdc2c8b7b0e3bf8d00bb
InnoFang <q984941707@qq.com>
2017-03-31 21:13:51 +0800

    implement Merge Sort

--- src/SortTest.java
+++ src/SortTest.java
@@ -1,15 +1,16 @@
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import sort.impl.BubbleSort;
 import sort.impl.InsertionSort;
+import sort.impl.MergeSort;
 import sort.impl.SelectionSort;
 import utils.SortTestHelper;
+import utils.compare.Comparable;
 import utils.compare.impl.IntegerComparison;
 
 import java.util.Arrays;
-import java.util.Random;
 
 /**
  * Created by Inno Fang on 2017/3/27.
+ *
  */
 public final class SortTest {
 
@@ -17,26 +18,31 @@ public final class SortTest {
     }
 
     public static void main(String[] args) {
-        final int n = 10000;
+        final int n = 50000;
+        Comparable comparison = new IntegerComparison();
         int[] arr = SortTestHelper.generateRandomIntegerArray(n, 10, 1000);
         int[] arr1 = Arrays.copyOf(arr, arr.length);
         int[] arr2 = Arrays.copyOf(arr, arr.length);
+        int[] arr3 = Arrays.copyOf(arr, arr.length);
 
         System.out.println("Test for Random Array, size = " + n + ", random range [0, " + n + "]");
-        SortTestHelper.testSort(new BubbleSort(new IntegerComparison()), arr);
-        SortTestHelper.testSort(new SelectionSort(new IntegerComparison()), arr1);
-        SortTestHelper.testSort(new InsertionSort(new IntegerComparison()), arr2);
+        SortTestHelper.testSort(new BubbleSort(comparison), arr);
+        SortTestHelper.testSort(new SelectionSort(comparison), arr1);
+        SortTestHelper.testSort(new InsertionSort(comparison), arr2);
+        SortTestHelper.testSort(new MergeSort(comparison), arr3);
 
         System.out.println();
 
         arr = SortTestHelper.generateNearlyOrderedIntegerArray(n, 10);
         arr1 = Arrays.copyOf(arr, arr.length);
         arr2 = Arrays.copyOf(arr, arr.length);
+        arr3 = Arrays.copyOf(arr, arr.length);
 
         System.out.println("Test for Nearly Ordered Array, size = " + n + ", range [0, " + n + "]");
-        SortTestHelper.testSort(new BubbleSort(new IntegerComparison()), arr);
-        SortTestHelper.testSort(new SelectionSort(new IntegerComparison()), arr1);
-        SortTestHelper.testSort(new InsertionSort(new IntegerComparison()), arr2);
+        SortTestHelper.testSort(new BubbleSort(comparison), arr);
+        SortTestHelper.testSort(new SelectionSort(comparison), arr1);
+        SortTestHelper.testSort(new InsertionSort(comparison), arr2);
+        SortTestHelper.testSort(new MergeSort(comparison), arr3);
 
     }
 }
--- src/sort/impl/MergeSort.java
+++ src/sort/impl/MergeSort.java
@@ -5,7 +5,6 @@ import utils.compare.Comparable;
 
 /**
  * Created by Inno Fang on 2017/3/31.
- *
  */
 public class MergeSort extends ISort {
     public MergeSort(Comparable comparable) {
@@ -14,6 +13,43 @@ public class MergeSort extends ISort {
 
     @Override
     public <T> void sort(T[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+    }
 
+    /* Sort the array from [l...r] using merge sort */
+    private <T> void mergeSort(T[] arr, int l, int r) {
+        if (l >= r)
+            return;
+        int mid = (l + r) / 2; // FIXME: 2017/3/31
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    /* Merge arr[l...mid] and arr[mid+1...r] */
+    @SuppressWarnings("unchecked")
+    private <T> void merge(T[] arr, int l, int mid, int r) {
+        T[] aux = (T[]) new Object[r - l + 1]; // Auxiliary Space
+      /*  for (int i = l; i <= r; i++) {
+            aux[i - l] = arr[i];
+        }*/
+        /* Copy the array arr from l to (r-l+1) to array aux*/
+        System.arraycopy(arr, l, aux, 0, r - l + 1);
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {
+                arr[k] = aux[j - l];
+                j++;
+            } else if (j > r) {
+                arr[k] = aux[i - l];
+                i++;
+            } else if (comparable.moreThan(aux[i - l], aux[j - l])) {
+                arr[k] = aux[j - l];
+                j++;
+            } else {
+                arr[k] = aux[i - l];
+                i++;
+            }
+        }
     }
 }
