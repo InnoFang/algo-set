bf2498c4e155d74503036e93a8bb744ea7ff6313
InnoFang <q984941707@qq.com>
2017-04-02 09:38:51 +0800

    update ISearch

--- src/io/innofang/search/ISearch.java
+++ src/io/innofang/search/ISearch.java
@@ -1,6 +1,7 @@
 package io.innofang.search;
 
 
+import io.innofang.utils.WrapperUtil;
 import io.innofang.utils.comparator.Comparator;
 
 /**
@@ -14,7 +15,34 @@ public abstract class ISearch {
         this.comparator = comparator;
     }
 
-    public abstract <T> void search(T target);
+    public abstract <T> void search(T[] arr, T target);
 
+    public void search(char[] arr, char target) {
+        Character wrapTarget = target;
+        Character[] wrapArr = WrapperUtil.wrapperCharArray(arr);
+        search(wrapArr, wrapTarget);
+    }
+    public void search(int[] arr, int target) {
+        Integer wrapTarget = target;
+        Integer[] wrapArr = WrapperUtil.wrapperIntArray(arr);
+        search(wrapArr, wrapTarget);
+    }
 
+    public void search(long[] arr, long target) {
+        Long wrapTarget = target;
+        Long[] wrapArr = WrapperUtil.wrapperLongArray(arr);
+        search(wrapArr, wrapTarget);
+    }
+
+    public void search(float[] arr, float target) {
+        Float wrapTarget = target;
+        Float[] wrapArr = WrapperUtil.wrapperFloatArray(arr);
+        search(wrapArr, wrapTarget);
+    }
+
+    public void search(double[] arr, double target) {
+        Double wrapTarget = target;
+        Double[] wrapArr = WrapperUtil.wrapperDoubleArray(arr);
+        search(wrapArr, wrapTarget);
+    }
 }
