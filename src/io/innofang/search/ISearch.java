c8a5520a3ed6324993717c607cdbab4fdc02c89e
InnoFang <q984941707@qq.com>
2017-04-02 10:26:18 +0800

    update Binary Search

--- src/io/innofang/search/ISearch.java
+++ src/io/innofang/search/ISearch.java
@@ -15,34 +15,34 @@ public abstract class ISearch {
         this.comparator = comparator;
     }
 
-    public abstract <T> void search(T[] arr, T target);
+    public abstract <T> int[] search(T[] arr, T target);
 
-    public void search(char[] arr, char target) {
+    public int[] search(char[] arr, char target) {
         Character wrapTarget = target;
         Character[] wrapArr = WrapperUtil.wrapperCharArray(arr);
-        search(wrapArr, wrapTarget);
+        return search(wrapArr, wrapTarget);
     }
-    public void search(int[] arr, int target) {
+    public int[] search(int[] arr, int target) {
         Integer wrapTarget = target;
         Integer[] wrapArr = WrapperUtil.wrapperIntArray(arr);
-        search(wrapArr, wrapTarget);
+        return search(wrapArr, wrapTarget);
     }
 
-    public void search(long[] arr, long target) {
+    public int[] search(long[] arr, long target) {
         Long wrapTarget = target;
         Long[] wrapArr = WrapperUtil.wrapperLongArray(arr);
-        search(wrapArr, wrapTarget);
+        return search(wrapArr, wrapTarget);
     }
 
-    public void search(float[] arr, float target) {
+    public int[] search(float[] arr, float target) {
         Float wrapTarget = target;
         Float[] wrapArr = WrapperUtil.wrapperFloatArray(arr);
-        search(wrapArr, wrapTarget);
+        return search(wrapArr, wrapTarget);
     }
 
-    public void search(double[] arr, double target) {
+    public int[] search(double[] arr, double target) {
         Double wrapTarget = target;
         Double[] wrapArr = WrapperUtil.wrapperDoubleArray(arr);
-        search(wrapArr, wrapTarget);
+        return search(wrapArr, wrapTarget);
     }
 }
