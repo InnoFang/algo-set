5bf7ef30b4a877c29d895bccfeaea2c8252f8e3b
InnoFang <q984941707@qq.com>
2017-12-19 21:01:16 +0800

    :sparkles: add leetcode solution

---
+++ src/leetcode/_198_House_Robber/Solution.kt
@@ -0,0 +1,70 @@
+package leetcode._198_House_Robber
+
+/**
+ * Created by Inno Fang on 2017/12/7.
+ */
+// 198
+
+class Solution {
+    var memo: MutableList<Int> = MutableList(0) { -1 }
+    fun rob(nums: IntArray): Int {
+        memo.addAll(MutableList(nums.size) { -1 })
+        return tryRob(nums, 0)
+    }
+
+    private fun tryRob(nums: IntArray, index: Int): Int {
+        if (index >= nums.size)
+            return 0
+        if (memo[index] != -1)
+            return memo[index]
+
+        var res = 0
+        (index until nums.size).forEach {
+            res = maxOf(res, nums[it] + tryRob(nums, it + 2))
+        }
+        memo[index] = res
+        return res
+    }
+}
+
+class Solution2 {
+    fun rob(nums: IntArray): Int {
+        val n = nums.size
+        if (n == 0) return 0
+        val dp = Array(n) { -1 }
+        dp[n - 1] = nums[n - 1]
+        (n - 2 downTo 0).forEach { i ->
+            (i until n).forEach { j ->
+                dp[i] = maxOf(dp[i], nums[j] + (if (j + 2 < n) dp[j + 2] else 0))
+            }
+        }
+        return dp[0]
+    }
+
+}
+
+
+public class Solution3 {
+    fun rob(nums: IntArray): Int {
+        if (nums.isEmpty()) return 0
+        val n = nums.size
+        val dp = Array(nums.size) { -1 }
+        dp[0] = nums[0]
+        (1 until n).forEach { i ->
+            dp[i] = maxOf(dp[i - 1], nums[i] + if (i > 1) dp[i - 2] else 0)
+        }
+//        dp.forEach(::println)
+        return dp[n - 1]
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val solution = Solution3()
+    solution.rob(intArrayOf(3, 4, 2, 1)).let(::println)
+    solution.rob(intArrayOf(8, 4, 5, 3, 1)).let(::println)
+}
+
+// 213. House Robber II
+// 337. House Robber III
+// 309. Best Time to Buy and Sell Stock with Cooldown
---
+++ src/leetcode/_202_Happy_Number/Solution.kt
@@ -0,0 +1,80 @@
+package leetcode._202_Happy_Number
+
+import algorithm.PerfectSquares.numSquares
+import algorithm.fibnacci.num
+import com.sun.org.apache.xpath.internal.operations.Bool
+
+/**
+ * Created by Inno Fang on 2017/12/18.
+ */
+class Solution {
+    val memo = HashSet<Int>()
+    fun isHappy(n: Int): Boolean {
+        var num = n
+        while (!memo.contains(num)) {
+            memo.add(num)
+            var sum = 0
+            while (num != 0) {
+                sum += (num % 10) * (num % 10)
+                num /= 10
+            }
+            num = sum
+        }
+        if (num == 1) return true
+        return false
+    }
+}
+
+// 小于 10 的情况下，只有 1 和 7 是 Happy Number，所以可以拿 num 大于等于 10 做判定点，最小于 10 的结果进行判断
+class Solution2 {
+    fun isHappy(n: Int): Boolean {
+        var num = n
+        while (num >= 10) {
+            var sum = 0
+            while (num != 0) {
+                sum += (num % 10) * (num % 10)
+                num /= 10
+            }
+            num = sum
+        }
+        return num == 1 || num == 7
+    }
+}
+
+class Solution3 {
+    fun isHappy(n: Int): Boolean {
+        var one = n
+        var two = n
+        while (n != 1) {
+            one = squareSum(one)
+            two = squareSum(squareSum(two))
+            if (one == 1 || two == 1) return true
+            if (one == two) return false
+        }
+        return true
+    }
+
+    private fun squareSum(n: Int): Int {
+        var sum = 0
+        var num = n
+        while (num != 0) {
+            val mod = num % 10
+            sum += mod * mod
+            num /= 10
+        }
+        return sum
+    }
+}
+
+fun main(args: Array<String>) {
+    Solution3().isHappy(34).let(::println)
+    Solution3().isHappy(7).let(::println)
+    Solution3().isHappy(8).let(::println)
+    Solution3().isHappy(82).let(::println)
+    Solution3().isHappy(68).let(::println)
+    Solution3().isHappy(100).let(::println)
+    Solution3().isHappy(1).let(::println)
+    Solution3().isHappy(10000000).let(::println)
+    Solution3().isHappy(0x6ffffff).let(::println)
+    Solution3().isHappy(128319).let(::println)
+}
