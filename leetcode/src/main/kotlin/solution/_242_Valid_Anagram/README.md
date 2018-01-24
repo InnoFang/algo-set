c43b53a4a78e1b87f489a2161e86e5289c8ff3a9
InnoFang <q984941707@qq.com>
2018-01-24 11:28:37 +0800

    :sparkles: add T242 solution

---
+++ leetcode/src/main/kotlin/solution/_242_Valid_Anagram/README.md
@@ -0,0 +1,11 @@
+# 242. Valid Anagram
+
+![](https://img.shields.io/badge/Difficulty-Easy-green.svg)
+
+Given two strings _s_ and _t_, write a function to determine if _t_ is an anagram of _s_.
+
+For example,           
+
+_s_ = "anagram", _t_ = "nagaram", return true.
+
+_s_ = "rat", _t_ = "car", return false.
---
+++ leetcode/src/main/kotlin/solution/_242_Valid_Anagram/Solution.kt
@@ -0,0 +1,23 @@
+package solution._242_Valid_Anagram
+
+/**
+ * Created by Inno Fang on 2018/1/24.
+ */
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        val alpha = IntArray(26) { 0 }
+        s.forEach { alpha[it.toInt() - 'a'.toInt()]++ }
+        t.forEach { alpha[it.toInt() - 'a'.toInt()]-- }
+        return alpha.none { it != 0 }
+    }
+}
+
+fun main(args: Array<String>) {
+    Solution().isAnagram("anagram", "nagaram").let(::println)
+    Solution().isAnagram("rat", "car").let(::println)
+    Solution().isAnagram("", "car").let(::println)
+    Solution().isAnagram("", "").let(::println)
+    Solution().isAnagram("rat", "").let(::println)
+    Solution().isAnagram("aa", "a").let(::println)
+    Solution().isAnagram("ccac", "aacc").let(::println)
+}
