262f196c90b18af479272baab71d6d814f909ce3
InnoFang <q984941707@qq.com>
2018-02-10 19:08:50 +0800

    :sparkles: update T279 solution

--- leetcode/279. Perfect Squares/Solution.kt
+++ leetcode/279. Perfect Squares/Solution.kt
@@ -22,12 +22,42 @@ class Solution {
     }
 
 }
+/**
+ * 586 / 586 test cases passed.
+ * Status: Accepted
+ * Runtime: 494 ms
+ */
+class Solution2 {
+    fun numSquares(n: Int): Int {
+        val que = java.util.LinkedList<Pair<Int, Int>>()
+        val visited = MutableList(n) { false }
+        que.add(Pair(n, 0))
+        while (que.isNotEmpty()) {
+            val num = que.peek().first
+            val step = que.peek().second
+            que.poll()
+
+            for (i in (1..n)) {
+                val tmp = num - i * i
+                if (tmp < 0) break
+                if (tmp == 0) return step + 1
+                if (!visited[tmp]) {
+                    que.add(Pair(tmp, step + 1))
+                    visited[tmp] = true
+                }
+            }
+        }
+        // cannot find.
+        return -1
+    }
+}
 
 fun main(args: Array<String>) {
-    Solution().numSquares(4).let(::println) // 1
-    Solution().numSquares(12).let(::println) // 3
-    Solution().numSquares(13).let(::println) // 2
-    Solution().numSquares(16).let(::println) // 1
-    Solution().numSquares(24).let(::println) // 3
-    Solution().numSquares(50).let(::println) // 2
+    Solution2().numSquares(1).let(::println) // 1
+    Solution2().numSquares(4).let(::println) // 1
+    Solution2().numSquares(12).let(::println) // 3
+    Solution2().numSquares(13).let(::println) // 2
+    Solution2().numSquares(16).let(::println) // 1
+    Solution2().numSquares(24).let(::println) // 3
+    Solution2().numSquares(50).let(::println) // 2
 }
