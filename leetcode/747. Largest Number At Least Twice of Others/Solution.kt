8754002bc06ee0d2ab1a3349c06708f3e1840059
InnoFang <q984941707@qq.com>
2018-02-12 21:55:33 +0800

    :sparkles: upload T747 solution

---
+++ leetcode/747. Largest Number At Least Twice of Others/Solution.kt
@@ -0,0 +1,34 @@
+package algorithm._747_Largest_Number_At_Least_Twice_of_Others
+
+/**
+ * Created by Inno Fang on 2018/2/12.
+ */
+
+/**
+ * 250 / 250 test cases passed.
+ * Status: Accepted
+ * Runtime: 424 ms
+ */
+class Solution {
+    fun dominantIndex(nums: IntArray): Int {
+        if (nums.isEmpty()) return -1
+        if (nums.size == 1) return 0
+        var max = Int.MIN_VALUE + 1
+        var big = Int.MIN_VALUE
+        var index = -1
+        nums.forEachIndexed { i, num ->
+            if (num > max) {
+                big = max
+                max = num
+                index = i
+            } else if (num > big) big = num
+        }
+        return if (max >= big * 2) index else -1
+    }
+}
+
+fun main(args: Array<String>) {
+    Solution().dominantIndex(intArrayOf(3, 6, 1, 0)).let(::println)
+    Solution().dominantIndex(intArrayOf(1, 2, 3, 4)).let(::println)
+    Solution().dominantIndex(intArrayOf(1)).let(::println)
+}
