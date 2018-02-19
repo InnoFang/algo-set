ed02d76713e6e5ddcd42dcb4c2352bb73002ddaf
InnoFang <q984941707@qq.com>
2018-02-19 09:54:01 +0800

    :sparkles: upload T12 solution and description.

---
+++ leetcode/012. Integer to Roman/Solution.kt
@@ -0,0 +1,28 @@
+/**
+ * Created by Inno Fang on 2018/2/19.
+ */]
+
+class Solution {
+    fun intToRoman(num: Int): String {
+        val roman = arrayOf(
+                arrayOf("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"),
+                arrayOf("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"),
+                arrayOf("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"),
+                arrayOf("", "M", "MM", "MMM"))
+
+        return StringBuilder()
+                .append(roman[0][num % 10])
+                .insert(0, roman[1][(num / 10) % 10])
+                .insert(0, roman[2][(num / 100) % 10])
+                .insert(0, roman[3][(num / 1000) % 10])
+                .toString()
+    }
+}
+
+fun main(args: Array<String>) {
+    Solution().intToRoman(8).let(::println)
+    Solution().intToRoman(17).let(::println)
+    Solution().intToRoman(999).let(::println)
+    Solution().intToRoman(201).let(::println)
+    Solution().intToRoman(3012).let(::println)
+}
