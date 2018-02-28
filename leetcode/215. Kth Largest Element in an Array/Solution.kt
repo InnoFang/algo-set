9bb57f4ce580f1711080ae445d5ac067f3e937c0
InnoFang <q984941707@qq.com>
2018-02-28 09:03:12 +0800

    :sparkles: upload T215 solution and description

---
+++ leetcode/215. Kth Largest Element in an Array/Solution.kt
@@ -0,0 +1,36 @@
+/**
+ * Created by Inno Fang on 2018/2/28.
+ */
+
+/**
+ * 31 / 31 test cases passed.
+ * Status: Accepted
+ * Runtime: 480 ms
+ */
+class Solution {
+    fun findKthLargest(nums: IntArray, k: Int): Int {
+        return partition(nums, k, 0, nums.lastIndex)
+    }
+
+    private fun partition(nums: IntArray, k: Int, start: Int, end: Int): Int {
+        var lo = start
+        var hi = end
+        while (lo < hi) {
+            while (lo < hi && nums[hi] <= nums[start]) hi--
+            while (lo < hi && nums[lo] >= nums[start]) lo++
+            if (lo < hi) (nums[hi] to nums[lo]).run { nums[lo] = first; nums[hi] = second }
+        }
+        (nums[start] to nums[hi]).run { nums[start] = second; nums[hi] = first }
+        return when {
+            lo == k - 1 -> {
+                nums[lo]
+            }
+            lo > k - 1 -> partition(nums, k, start, lo - 1)
+            else -> partition(nums, k, lo + 1, end)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    Solution().findKthLargest(intArrayOf(3,5,9,1,4,7,2,6,8,0), 3).let(::println)
+}
