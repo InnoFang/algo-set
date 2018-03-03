41f1b3fb4a93dec826ce5b08141fdf8d2c947f24
InnoFang <q984941707@qq.com>
2018-03-03 12:54:29 +0800

    :sparkles: update T738 solution

--- leetcode/738. Monotone Increasing Digits/Solution.kt
+++ leetcode/738. Monotone Increasing Digits/Solution.kt
@@ -26,8 +26,24 @@ class Solution {
 
 }
 
+/**
+ * 302 / 302 test cases passed.
+ * Status: Accepted
+ * Runtime: 228 ms
+ */
+class Solution2 {
+    fun monotoneIncreasingDigits(N: Int): Int {
+        val nums = N.toString().toCharArray()
+        var i = 1
+        while (i < nums.size && nums[i-1] <= nums[i]) i++
+        while (i > 0 && i < nums.size && nums[i-1] > nums[i]) nums[--i]--
+        (i+1..nums.lastIndex).forEach { nums[it] = '9' }
+        return nums.joinToString("").toInt()
+    }
+}
+
 fun main(args: Array<String>) {
-    Solution().monotoneIncreasingDigits(332).let(::println)
-    Solution().monotoneIncreasingDigits(1234).let(::println)
-    Solution().monotoneIncreasingDigits(10).let(::println)
+    Solution2().monotoneIncreasingDigits(332).let(::println)
+    Solution2().monotoneIncreasingDigits(1234).let(::println)
+    Solution2().monotoneIncreasingDigits(10).let(::println)
 }
