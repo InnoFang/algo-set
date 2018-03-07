49874e824c0af374fd7f4f978ef8e2dffa8c593f
InnoFang <innofang@outlook.com>
2018-03-07 20:41:05 +0800

    :zap: :art: :bug: optimize UnionFind [path compression] and fix some bugs

--- src/io/innofang/kotlin/unionfind/UnionFind.kt
+++ src/io/innofang/kotlin/unionfind/UnionFind.kt
@@ -4,18 +4,25 @@ package io.innofang.kotlin.unionfind
  * Created by Inno Fang on 2018/3/7.
  */
 
-class UnionFind(val count: Int) {
+class UnionFind(private val count: Int) {
     private val parent: IntArray = IntArray(count) { it }
     private val rank: IntArray = IntArray(count) { 1 }
 
     public fun find(idx: Int): Int {
         assert(idx in 0..(count - 1))
+
+        // path compression 1
         var idx = idx
         while (idx != parent[idx]) {
             parent[idx] = parent[parent[idx]]
             idx = parent[idx]
         }
         return idx
+
+        // path compression 2
+//        if (idx != parent[idx])
+//            parent[idx] = find(parent[idx])
+//        return parent[idx]
     }
 
     public fun isConnected(idx1: Int, idx2: Int) = find(idx1) == find(idx2)
@@ -29,7 +36,7 @@ class UnionFind(val count: Int) {
         when {
             rank[root1] < rank[root2] -> parent[root1] = root2
             rank[root1] > rank[root2] -> parent[root2] = root1
-            else -> {
+            else -> { // rank[root1] == rank[root2]
                 parent[root1] = root2
                 rank[root2] += 1
             }
