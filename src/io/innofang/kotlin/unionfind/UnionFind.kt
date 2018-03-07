40831902780797aa9ad53cd631021a2191491661
InnoFang <innofang@outlook.com>
2018-03-07 20:09:34 +0800

    :zap: :art: optimize UnionFind code

--- src/io/innofang/kotlin/unionfind/UnionFind.kt
+++ src/io/innofang/kotlin/unionfind/UnionFind.kt
@@ -6,7 +6,7 @@ package io.innofang.kotlin.unionfind
 
 class UnionFind(val count: Int) {
     private val parent: IntArray = IntArray(count) { it }
-    private val size: IntArray = IntArray(count) { 1 }
+    private val rank: IntArray = IntArray(count) { 1 }
 
     public fun find(idx: Int): Int {
         assert(idx in 0..(count - 1))
@@ -24,12 +24,13 @@ class UnionFind(val count: Int) {
 
         if (root1 == root2) return
 
-        if (size[root1] < size[root2]) {
-            parent[root1] = root2
-            size[root2] += size[root1]
-        } else{
-            parent[root2] = root1
-            size[root1] += size[root2]
+        when {
+            rank[root1] < rank[root2] -> parent[root1] = root2
+            rank[root1] > rank[root2] -> parent[root2] = root1
+            else -> {
+                parent[root1] = root2
+                rank[root2] += 1
+            }
         }
     }
 }
