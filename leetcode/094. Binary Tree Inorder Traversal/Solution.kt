f3a14538ef3b7f44c6304c10cdb8651ba57c1184
InnoFang <innofang@outlook.com>
2018-03-08 21:24:47 +0800

    :sparkles: upload T94 solution

---
+++ leetcode/094. Binary Tree Inorder Traversal/Solution.kt
@@ -0,0 +1,42 @@
+/**
+ * Created by Inno Fang on 2018/1/17.
+ */
+
+class TreeNode(var `val`: Int = 0) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+
+/**
+ * 68 / 68 test cases passed.
+ * Status: Accepted
+ * Runtime: 284 ms
+ */
+class Solution {
+    class Command(var s: String, var node: TreeNode?)
+
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        val res = ArrayList<Int>()
+        if (null == root) return res
+
+        val stack = java.util.Stack<Command>()
+        stack.push(Command("go", root))
+
+        while (!stack.isEmpty()) {
+            val command = stack.pop()
+
+            if (command.s == "print")
+                res.add(command.node?.`val`!!)
+            else {
+                if (null != command.node?.right)
+                    stack.push(Command("go", command.node?.right))
+                stack.push(Command("print", command.node))
+                if (null != command.node?.left)
+                    stack.push(Command("go", command.node?.left))
+
+            }
+        }
+        return res
+    }
+}
