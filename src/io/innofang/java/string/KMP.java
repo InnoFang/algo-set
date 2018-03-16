08692472cdc603ea0197a6e7bf067156ba258ac2
InnoFang <innofang@outlook.com>
2018-03-16 13:20:06 +0800

    :art: optimized KMP substring search algorithm

--- src/io/innofang/java/string/KMP.java
+++ src/io/innofang/java/string/KMP.java
@@ -5,45 +5,55 @@ package io.innofang.java.string;
  */
 public class KMP {
 
-    private String pattern;
+    private char[] pattern;
     private int[] next;
+
+    public KMP(char[] pattern) {
+        resetPattern(pattern);
+    }
 
     public KMP(String pattern) {
         resetPattern(pattern);
     }
 
     public void resetPattern(String pattern) {
-        this.pattern = pattern;
+        this.pattern = pattern.toCharArray();
         next = generateNext(pattern.length());
+    }
+
+    public void resetPattern(char[] pattern) {
+        this.pattern = pattern;
+        next = generateNext(pattern.length);
+    }
+    public boolean matches(String text) {
+        return matches(text.toCharArray());
+    }
+
+    public boolean matches(char[] text) {
+        int t = 0;
+        int p = 0;
+        int n = 0;
+        while (t < text.length && p < pattern.length) {
+            if (text[t] == pattern[p]) {
+                t++;
+                p++;
+            } else if (p != 0) p = next[p - 1];
+            else t++;
+        }
+        return p == pattern.length;
     }
 
     private int[] generateNext(int length) {
         int[] tmp = new int[length];
-        char[] aux = pattern.toCharArray();
         int idx = 0;
         tmp[idx] = 0;
         for (int i = 1; i < length; ) {
-            if (aux[idx] == aux[i]) tmp[i++] = (idx++) + 1;
-            // tmp[idx] != tmp[i]
+            if (pattern[idx] == pattern[i]) tmp[i++] = (idx++) + 1;
+                // tmp[idx] != tmp[i]
             else if (idx == 0) tmp[i++] = 0;
             else idx = tmp[idx - 1];
         }
         return tmp;
     }
 
-    public boolean matches(String text) {
-        char[] textArr = text.toCharArray();
-        char[] patternArr = pattern.toCharArray();
-        int t = 0;
-        int p = 0;
-        int n = 0;
-        while (t < textArr.length && p < patternArr.length) {
-            if (textArr[t] == patternArr[p]) {
-                t++;
-                p++;
-            } else if (p != 0) p = next[p - 1];
-            else t++;
-        }
-        return p == patternArr.length;
-    }
 }
