1663d54833a4f1a0d79745073445217b2266e6b3
InnoFang <innofang@outlook.com>
2018-03-18 22:20:27 +0800

    :sparkles: update T392 java solution

--- leetcode/392. Is Subsequence/Solution.java
+++ leetcode/392. Is Subsequence/Solution.java
@@ -3,10 +3,35 @@
  */
 
 class Solution {
+
+    /**
+     * 14 / 14 test cases passed.
+     * Status: Accepted
+     * Runtime: 11 ms
+     * @param s
+     * @param t
+     * @return
+     */
     public boolean isSubsequence(String s, String t) {
         if (s.isEmpty()) return true;
         if (t.isEmpty()) return false;
         int i = t.indexOf(s.charAt(0));
         return i != -1 && (s.length() == 1 || isSubsequence(s.substring(1), t.substring(i + 1)));
+    }
+
+    /**
+     * 14 / 14 test cases passed.
+     * Status: Accepted
+     * Runtime: 3 ms
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence2(String s, String t) {
+        for (int i = 0, pos = 0, len = s.length(); i < len; i++, pos++){
+            pos = t.indexOf(s.charAt(i), pos);
+            if (pos == -1) return false;
+        }
+        return true;
     }
 }
