35766b1a44af48b694857fb4f562acfd67dd2cfe
InnoFang <innofang@outlook.com>
2018-03-18 13:31:24 +0800

    :art: update T50 kotlin solution

--- leetcode/050. Pow(x, n)/Solution.kt
+++ leetcode/050. Pow(x, n)/Solution.kt
@@ -21,6 +21,25 @@ class Solution {
     }
 }
 
+
+/**
+ * 303 / 303 test cases passed.
+ * Status: Accepted
+ * Runtime: 212 ms
+ */
+class Solution2 {
+    fun myPow(x: Double, n: Int): Double {
+        return when {
+            n < 0 -> (1.0 / x) * myPow(1.0 / x, Math.abs(n + 1))
+            n == 0 -> 1.0
+            n == 1 -> x
+            n == 2 -> x * x
+            n and 1 == 0 -> myPow(myPow(x, n / 2), 2)
+            else -> x * myPow(myPow(x, (n - 1) / 2), 2)
+        }
+    }
+}
+
 fun main(args: Array<String>) {
     Solution().myPow(2.00000, 10).let(::println)
     Solution().myPow(2.10000, 3).let(::println)
