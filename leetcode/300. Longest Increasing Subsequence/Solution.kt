100e938759ab7c75bef878c5b0cf48c6ad897719
InnoFang <innofang@outlook.com>
2018-03-22 20:28:25 +0800

    :sparkles: update T300 kotlin solution

--- leetcode/300. Longest Increasing Subsequence/Solution.kt
+++ leetcode/300. Longest Increasing Subsequence/Solution.kt
@@ -2,22 +2,11 @@
  * Created by Inno Fang on 2017/12/28.
  */
 
-/*
 
-// WA [4,10,4,3,8,9]
-class Solution {
-    fun lengthOfLIS(nums: IntArray): Int {
-        if (nums.isEmpty()) return 0
-        val dp = IntArray(nums.size)
-
-        dp[nums.size - 1] = 1
-
-        (nums.size - 2 downTo 0).forEach { i ->
-            dp[i] = if (nums[i] >= nums[i + 1]) dp[i + 1] else 1 + dp[i + 1]
-        }
-        return dp[0]
-    }
-}
+/**
+ * 24 / 24 test cases passed.
+ * Status: Accepted
+ * Runtime: 655 ms
  */
 class Solution {
     fun lengthOfLIS(nums: IntArray): Int {
@@ -33,20 +22,35 @@ class Solution {
     }
 }
 
-fun main(args: Array<String>) {
-    Solution().lengthOfLIS(intArrayOf(10, 9, 2, 5, 3, 7, 101, 18)).let(::println)
-    Solution().lengthOfLIS(intArrayOf(4, 10, 4, 3, 8, 9)).let(::println)
-    Solution().lengthOfLIS(intArrayOf(1, 0, 1, 0, 1, 0)).let(::println)
-    Solution().lengthOfLIS(intArrayOf(10, 15, 20, 11, 9, 101)).let(::println)
-    Solution().lengthOfLIS(intArrayOf()).let(::println)
+/**
+ * 24 / 24 test cases passed.
+ * Status: Accepted
+ * Runtime: 376 ms
+ */
+class Solution2 {
+    fun lengthOfLIS(nums: IntArray): Int {
+        val store = IntArray(nums.size) { 0 }
+        var len = 0
+        nums.forEach {
+            var l = 0
+            var r = len
+            while (l != r) {
+                val mid = l + (r - l) / 2
+                if (store[mid] < it) l = mid + 1
+                else r = mid
+            }
+            store[l] = it
+            if (l == len) ++len
+        }
+        return len
+    }
 }
 
-
-/*
-* [4,10,4,3,8,9]
-* [10, 9, 2, 5, 3, 7, 101, 18]
-* []
-* */
-
-
+fun main(args: Array<String>) {
+    Solution2().lengthOfLIS(intArrayOf(10, 9, 2, 5, 3, 7, 101, 18)).let(::println)
+    Solution2().lengthOfLIS(intArrayOf(4, 10, 4, 3, 8, 9)).let(::println)
+    Solution2().lengthOfLIS(intArrayOf(1, 0, 1, 0, 1, 0)).let(::println)
+    Solution2().lengthOfLIS(intArrayOf(10, 15, 20, 11, 9, 101)).let(::println)
+    Solution2().lengthOfLIS(intArrayOf()).let(::println)
+}
 // 376. Wiggle Subsequence
