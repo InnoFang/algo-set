915645f8202f1491dc5db74453af55ef5e360446
InnoFang <innofang@outlook.com>
2018-03-22 17:07:18 +0800

    :sparkles: update T643 kotlin solution

--- leetcode/643. Maximum Average Subarray I/Solution.kt
+++ leetcode/643. Maximum Average Subarray I/Solution.kt
@@ -4,8 +4,8 @@
 
 /**
  * 123 / 123 test cases passed.
-Status: Accepted
-Runtime: 2924 ms
+ * Status: Accepted
+ * Runtime: 2924 ms
  */
 class Solution {
     fun findMaxAverage(nums: IntArray, k: Int): Double {
@@ -20,7 +20,25 @@ class Solution {
     }
 }
 
+/**
+ * 123 / 123 test cases passed.
+ * Status: Accepted
+ * Runtime: 752 ms
+ */
+class Solution2 {
+    fun findMaxAverage(nums: IntArray, k: Int): Double {
+        if (nums.isEmpty() || k == 0) return 0.0
+        var average = nums.take(k).sum().toDouble() / k.toDouble()
+        var max = average
+        for (i in 0..nums.lastIndex - k) {
+            average += (nums[i + k] - nums[i]).toDouble() / k.toDouble()
+            if (average > max) max = average
+        }
+        return max
+    }
+}
+
 fun main(args: Array<String>) {
-    Solution().findMaxAverage(intArrayOf(1, 12, -5, -6, 50, 3), 4).let(::println)
-    Solution().findMaxAverage(intArrayOf(-1), 1).let(::println)
+    Solution2().findMaxAverage(intArrayOf(1, 12, -5, -6, 50, 3), 4).let(::println)
+    Solution2().findMaxAverage(intArrayOf(-1), 1).let(::println)
 }
