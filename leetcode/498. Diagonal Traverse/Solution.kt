c11dd51aafbf8b3b48eccf9025ea210d22a67fc5
InnoFang <innofang@outlook.com>
2018-03-23 15:12:37 +0800

    :sparkles: update T498 kotlin solution

--- leetcode/498. Diagonal Traverse/Solution.kt
+++ leetcode/498. Diagonal Traverse/Solution.kt
@@ -5,7 +5,7 @@
 /**
  * 32 / 32 test cases passed.
  * Status: Accepted
- * Runtime: 572 ms
+ * Runtime: 636 ms
  */
 class Solution {
     fun findDiagonalOrder(matrix: Array<IntArray>): IntArray {
@@ -18,18 +18,37 @@ class Solution {
                 store[i + j].add(matrix[i][j])
             }
         }
-        val result = mutableListOf<Int>()
+        val result = java.util.LinkedList<Int>()
         store.forEachIndexed { index, list ->
             if (index % 2 == 0) {
-                for (i in (list.lastIndex downTo 0)) {
-                    result.add(list[i])
-                }
-            } else{
-                for (i in list) {
-                    result.add(i)
-                }
+                result.addAll(list.reversed())
+            } else {
+                result.addAll(list)
             }
         }
+        return result.toIntArray()
+    }
+}
+
+/**
+ * 32 / 32 test cases passed.
+ * Status: Accepted
+ * Runtime: 400 ms
+ */
+class Solution2 {
+    fun findDiagonalOrder(matrix: Array<IntArray>): IntArray {
+        if (matrix.isEmpty()) return intArrayOf()
+        val rows = matrix.size
+        val cols = matrix[0].size
+        val store = MutableList(rows + cols - 1) { java.util.LinkedList<Int>() }
+        for (i in (0 until rows)) {
+            for (j in (0 until cols)) {
+                if (((i + j) and 1) == 0) store[i + j].add(0, matrix[i][j])
+                else store[i + j].add(matrix[i][j])
+            }
+        }
+        val result = java.util.LinkedList<Int>()
+        store.forEach { result.addAll(it) }
         return result.toIntArray()
     }
 }
