b35546be1af6f11a616d027d7e2dcb3311a351d8
InnoFang <innofang@outlook.com>
2018-03-27 19:49:41 +0800

    :art: update T796 kotlin solution

--- leetcode/796. Rotate String/Solution.kt
+++ leetcode/796. Rotate String/Solution.kt
@@ -13,6 +13,29 @@ class Solution {
     }
 }
 
+/**
+ * 39 / 39 test cases passed.
+ * Status: Accepted
+ * Runtime: 224 ms
+ */
+class Solution2 {
+    fun rotateString(A: String, B: String): Boolean {
+        if (A.length != B.length) return false
+        if (A.isEmpty()) return false
+        val store = mutableListOf<Int>()
+        A.forEachIndexed { idx, c ->
+            if (c == B[0])
+                store.add(idx)
+        }
+        if (store.isEmpty()) return false
+        store.forEach { idx ->
+            if ((A.substring(idx) + A.substring(0, idx)) == B) return true
+        }
+        return false
+    }
+}
+
+
 fun main(args: Array<String>) {
     Solution().rotateString("aa", "a").let(::println)
 }
