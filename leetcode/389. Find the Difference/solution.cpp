33bc8ee7c3fecf7fbdfb49fc19a2bfd700ad0eb2
InnoFang <innofang@outlook.com>
2018-06-13 08:30:08 +0800

    :art: update

--- leetcode/389. Find the Difference/solution.cpp
+++ leetcode/389. Find the Difference/solution.cpp
@@ -1,10 +1,11 @@
 //
 // Created by InnF on 2018/6/12.
 //
-//
+ 
 // 54 / 54 test cases passed.
 // Status: Accepted
 // Runtime: 6 ms
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
@@ -12,6 +13,21 @@ public:
         char res = tmp[0];
         for (int i = 1; i < tmp.length(); ++i) {
             res ^= tmp[i];
+        }
+        return res;
+    }
+};
+
+// 54 / 54 test cases passed.
+// Status: Accepted
+// Runtime: 6 ms
+
+class Solution2 {
+public:
+    char findTheDifference(string s, string t) {
+        char res = 0;
+        for (auto c: s + t) {
+            res ^= c;
         }
         return res;
     }
