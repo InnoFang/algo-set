a25dd58de26a8a858e9e44c3dff8b15fd7e75ce2
InnoFang <innofang@outlook.com>
2018-07-13 22:01:35 +0800

    :art: optimize new CodeWars solution: whats perfect power anyway

--- codewars/python/5 kyu/What's a Perfect Power anyway/Solution.py
+++ codewars/python/5 kyu/What's a Perfect Power anyway/Solution.py
@@ -7,3 +7,16 @@ def isPP(n):
         if abs(m - round(m)) < 1e-10:
             return [k - i, round(m)]
     return None
+
+
+# the follow code is more concise 
+
+# import math
+
+# def isPP(n):
+#     k = int(math.sqrt(n))
+#     for i in range(2, k + 1):
+#         m = math.log(n, i)
+#         if abs(m - round(m)) < 1e-10:
+#             return [i, round(m)]
+#     return None
