8905450ae9ecd9dfcb7f00b01443cf3dfe8c95e3
InnoFang <innofang@outlook.com>
2018-08-16 18:05:17 +0800

    :sparkles: upload the Breute Force JavaScript implementation

---
+++ Data Structures/Search/Pattern Match/Brute Force/main.js
@@ -0,0 +1,22 @@
+function get_index(text, pattern) {
+    let [t, p] = [0, 0]
+    while (t < text.length && p < pattern.length) {
+        if (text[t] == pattern[p]) {
+            ++t
+            ++p
+        } else {
+            t = t - p + 1
+            p = 0
+        }
+    }
+    return p == pattern.length ? t - p : -1
+}
+
+function has_substring(text, pattern) {
+    return get_index != -1
+} 
+
+const text = "abcabaaabaabcac";
+const pattern = "abaabcac";   
+console.log("Does the `text` contain the `pattern`? " + has_substring(text , pattern));
+console.log("What's the index that the `pattern` in the `text`? " + get_index(text, pattern));
