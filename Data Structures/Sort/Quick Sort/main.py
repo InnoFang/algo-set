36a59dd1d85eb3b035bde06bd4b0700e6c3a58bb
InnoFang <innofang@outlook.com>
2018-08-18 12:54:19 +0800

    :sparkles: upload Quick Sort JavaScript implementation

--- Data Structures/Sort/Quick Sort/main.py
+++ Data Structures/Sort/Quick Sort/main.py
@@ -12,7 +12,7 @@ def quick_sort(arr, left, right):
         pivot = partition(arr, left, right)
         quick_sort(arr, left, pivot - 1)
         quick_sort(arr, pivot + 1, right)
-
+ 
 def main():
     arr = [5, 7, 3, 9, 2, 0, 1, 8, 6, 4]
     quick_sort(arr, 0, len(arr) - 1)
@@ -20,4 +20,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
