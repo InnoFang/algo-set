bb3762feb9694e57f3043b61840f98f7aa29e2f1
InnoFang <innofang@outlook.com>
2018-08-25 20:33:58 +0800

    :sparkles: upload the in-order traversal for binary tree C++ implementation

--- Data Structures/Tree/Traversal/In Order/main.cpp
+++ Data Structures/Tree/Traversal/In Order/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream> 
-
+#include <stack>
+ 
+using namespace std; 
+ 
 typedef struct TreeNode {
     int val;
     TreeNode * left;
@@ -10,20 +13,30 @@ typedef struct TreeNode {
 void inorderTraversalByRecursion(TreeNode * root) {
     if (root) {
         inorderTraversalByRecursion(root->left);
-        std::cout << root->val << std::endl;
+        std::cout << root->val << " ";
         inorderTraversalByRecursion(root->right);
     }
 } 
 
-void inorderTraversal(TreeNode* root) {
-     
+void inorderTraversal(TreeNode * root) {
+     std::stack<TreeNode*> nodeStack;
+     while (root || !nodeStack.empty()) {
+     	while (root) {
+     		nodeStack.push(root);
+     		root = root->left;
+		} 
+		root = nodeStack.top();
+		nodeStack.pop();
+		std::cout << root->val << " ";
+		root = root->right;
+	 }
 } 
 
 /* Generate a binary tree by given a node array */
 TreeNode * generateBTree(int nodes[], int len, int index) {
     TreeNode * node = NULL;
 
-    if (index < len && nodes[index] != NULL) {
+    if (index < len && nodes[index]) {
         node = new TreeNode(nodes[index]);
         if (NULL == node) return NULL; 
  
@@ -49,7 +62,6 @@ in-order traversal: 4 2 7 5 1 6 3
 */
 
 int main() {
-
     int nodes[] = {1, 2, 3, 4, 5, 6, NULL, NULL, NULL, 7};
     int len = sizeof(nodes) / sizeof(int);
     std::cout << "Then total number of nodes: " << len << std::endl;
@@ -57,6 +69,12 @@ int main() {
 	TreeNode * root = generateBTree(nodes, len, 0); 
 	
     inorderTraversalByRecursion(root);
+    std::cout << std::endl;
+    
+    inorderTraversal(root);
+    std::cout << std::endl;
+    
+    return 0;
 }
 
 
