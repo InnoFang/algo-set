62a1984fb301a8cbc250ae85ff65d082d3f27df8
InnoFang <innofang@outlook.com>
2018-08-25 20:40:20 +0800

    :sparkles: upload the pre-order traversal by recursion for binayr tree C++ implementation

---
+++ Data Structures/Tree/Traversal/Pre Order/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stack>
+
+typedef struct TreeNode {
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+} TreeNode;
+
+void preorderTraversalByRecursion(TreeNode * root) {
+    if (root) {
+        std::cout << root->val << " ";
+        preorderTraversalByRecursion(root->left);
+        preorderTraversalByRecursion(root->right);
+    }
+} 
+
+void preorderTraversal(TreeNode * root) {
+      
+} 
+
+/* Generate a binary tree by given a node array */
+TreeNode * generateBTree(int nodes[], int len, int index) {
+    TreeNode * node = NULL;
+
+    if (index < len && nodes[index]) {
+        node = new TreeNode(nodes[index]);
+        if (NULL == node) return NULL; 
+ 
+        node->left = generateBTree(nodes, len, index * 2 + 1);
+        node->right = generateBTree(nodes, len, index * 2 + 2);
+    }
+    
+    return node;
+}
+
+/*
+The binary tree in this example is shown as follow
+
+       1
+     /   \
+    2     3
+   / \   /  
+  4   5 6
+     /
+    7
+
+pre-order traversal: 1 2 4 5 7 3 6
+*/
+
+int main() {
+    int nodes[] = {1, 2, 3, 4, 5, 6, NULL, NULL, NULL, 7};
+    int len = sizeof(nodes) / sizeof(int);
+    std::cout << "Then total number of nodes: " << len << std::endl;
+
+	TreeNode * root = generateBTree(nodes, len, 0); 
+	
+    preorderTraversalByRecursion(root);
+    std::cout << std::endl;
+    
+    preorderTraversal(root);
+    std::cout << std::endl;
+    
+    return 0;
+}
