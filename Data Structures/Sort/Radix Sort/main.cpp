8488da2c4e7b106d5846707157f6bb58f744fd26
InnoFang <innofang@outlook.com>
2018-08-29 14:31:20 +0800

    :sparkles: upload the C++ implementation of the Radix Sorting

---
+++ Data Structures/Sort/Radix Sort/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <ctime>
+#include <queue> 
+
+void radixSort(int * arr, int len) {
+    int max = -1;       /* store the maximum number */
+    for (int i = 0; i < len; ++i) 
+        if (arr[i] > max) 
+            max = arr[i];
+
+    int maxDigit = 0;   /* store the maximum number of digits  */
+    while (max) {       /* calculate the maximum number of digits  */
+        maxDigit += 1;
+        max /= 10;
+    }
+
+    std::queue<int> counter[10];    /* Counter consist of ten queues that store the number of the corresponding digit. */
+ 
+    for (int i = 0, mode = 10, dev = 1; i < maxDigit; ++i, mode *= 10, dev *= 10) {
+        for (int j = 0; j < len; ++j) {
+            int bucket = (arr[j] % mode) / dev;   
+            counter[bucket].push(arr[j]);  
+        }
+        
+        int index = 0;
+        for (int j = 0; j < 10; ++j) {  /* `10` is the length of the counter */
+            while (!counter[j].empty()) {  
+                arr[index++] = counter[j].front();
+                counter[j].pop();
+            } 
+        }
+    }
+}
+
+int *generateRandomArray(int length, int rangeFrom, int rangeTo);
+void printArray(int * arr, int length);
+
+int main() {
+
+    int length = 20;
+    int rangeFrom = 1;
+    int rangeTo = 10000;
+
+    int * arr = generateRandomArray(length, rangeFrom, rangeTo); 
+ 	
+    std::cout << "Original array:" << std::endl;
+    printArray(arr, length);
+
+    radixSort(arr, length);
+
+    std::cout << "Sorted array:" << std::endl;
+    printArray(arr, length);
+
+    return 0;
+}
+
+/* Randomly generate an array with the `length`, which is from `rangeFrom` to `rangeTo` */
+int * generateRandomArray(int length, int rangeFrom, int rangeTo) {
+    int * arr = new int[length];
+    int range = rangeTo - rangeFrom + 1;
+    srand(time(NULL));
+    for (int i = 0; i < length; ++i) {
+        arr[i] = rand() % range + rangeFrom;
+    }
+    return arr;
+}
+
+/* formatted output array */
+void printArray(int * arr, int length) {
+    for (int i = 0 ; i < length; ++i) std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
