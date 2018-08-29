a3688398f8c134948c5f51d22b0ed72db119b0dd
InnoFang <innofang@outlook.com>
2018-08-29 14:46:59 +0800

    :art: udpate

--- Data Structures/Sort/Bubble Sort/main.cpp
+++ Data Structures/Sort/Bubble Sort/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 void bubbleSort(int list[], int len) {
     for (int i = 0; i < len - 1; ++i) 
@@ -7,14 +8,41 @@ void bubbleSort(int list[], int len) {
                 std::swap(list[j], list[j+1]);
 }
 
-int main() {
-    int list[] = {5, 7, 3, 9, 2, 0, 1, 8, 6, 4};
- 	int len = sizeof(list) / sizeof(int);
- 	
-    bubbleSort(list, len);
+int *generateRandomArray(int length, int rangeFrom, int rangeTo);
+void printArray(int * arr, int length);
 
-    for (int i = 0 ; i < len; ++i) std::cout << list[i] << " ";
-    std::cout << std::endl;
+int main() {
+
+    int length = 20;
+    int rangeFrom = 1;
+    int rangeTo = 10000;
+
+    int * arr = generateRandomArray(length, rangeFrom, rangeTo); 
+ 	
+    std::cout << "Original array:" << std::endl;
+    printArray(arr, length);
+
+    bubbleSort(arr, length);
+
+    std::cout << "Sorted array:" << std::endl;
+    printArray(arr, length);
 
     return 0;
+}
+
+/* Randomly generate an array with the `length`, which is from `rangeFrom` to `rangeTo` */
+int * generateRandomArray(int length, int rangeFrom, int rangeTo) {
+    int * arr = new int[length];
+    int range = rangeTo - rangeFrom + 1;
+    srand(time(NULL));
+    for (int i = 0; i < length; ++i) {
+        arr[i] = rand() % range + rangeFrom;
+    }
+    return arr;
+}
+
+/* formatted output array */
+void printArray(int * arr, int length) {
+    for (int i = 0 ; i < length; ++i) std::cout << arr[i] << " ";
+    std::cout << std::endl;
 }
