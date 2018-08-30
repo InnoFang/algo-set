54a4f9681fac6198e0a53ec6c36d2a3b637727d8
InnoFang <innofang@outlook.com>
2018-08-30 13:15:08 +0800

    :art: optimize the Java implementation of Selection Sorting

--- Data Structures/Sort/Selection Sort/Main.java
+++ Data Structures/Sort/Selection Sort/Main.java
@@ -1,28 +1,62 @@
+import java.util.Random;
 public class Main {
 
-    public static void selectionSort(Integer[] list) {
-        for (int i = 0; i < list.length; ++i) {
+    public static void selectionSort(Integer[] arr) {
+        for (int i = 0; i < arr.length; ++i) {
             int idx = i;
-            for (int j = i + 1; j < list.length; ++j) {
-                if (list[j] < list[idx]) {
+            for (int j = i + 1; j < arr.length; ++j) {
+                if (arr[j] < arr[idx]) {
                     idx = j;
                 }
             }
             if (idx != i) {
-                int tmp = list[idx];
-                list[idx] = list[i];
-                list[i] = tmp;
+                int tmp = arr[idx];
+                arr[idx] = arr[i];
+                arr[i] = tmp;
             }
         }
     }
 
     public static void main(String[] args) {
-        Integer list[] = {5, 7, 3, 9, 2, 0, 1, 8, 6, 4};
+        int num = 20;
+        int rangeLeft = 1;
+        int rangeRight = 10000;
 
-        selectionSort(list);
+        Integer[] arr = generateRandomIntegerArray(num, rangeLeft, rangeRight);
 
-        for (int i = 0; i < list.length; ++i) 
-            System.out.print(list[i] + " ");
+        System.out.println("Original array: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+
+    /**
+      * @param num          the number of array elements
+      * @param rangeLeft  the left side of the range
+      * @param rangeRight the right side of the range
+      * @return an array containing N array element sizes between rangeLeft and rangeRight
+      */
+    public static Integer[] generateRandomIntegerArray(int num, int rangeLeft, int rangeRight) {
+
+        if (rangeLeft > rangeRight)
+            throw new RuntimeException("Range is incorrect, rangeLeft must be greater than rangeRight");
+
+        Integer[] arr = new Integer[num];
+        Integer range = rangeRight - rangeLeft;
+        Random random = new Random();
+        for (int i = 0; i < num; i++) {
+            arr[i] = random.nextInt(range) + rangeLeft;
+        }
+        return arr;
+    }
+
+    public static <T> void printArray(T arr[]) {
+        for (T t : arr) {
+            System.out.print(t + " ");
+        }
         System.out.println();
     }
 }
