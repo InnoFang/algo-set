df6a35b2f8a36097a99fe0bb066e7919a09d9fec
InnoFang <innofang@outlook.com>
2018-08-30 13:28:21 +0800

    :art: update the Java implementation of Heap Sorting

--- Data Structures/Sort/Heap Sort/Main.java
+++ Data Structures/Sort/Heap Sort/Main.java
@@ -1,40 +1,75 @@
+import java.util.Random; 
+
 public class Main {
     
-    public static void shiftDown(Integer[] list, int len, int idx) {
+    public static void shiftDown(Integer[] arr, int len, int idx) {
         while (idx * 2 + 1 < len) {
             int child = idx * 2 + 1;
-            if (child + 1 < len && list[child] < list[child + 1]) ++child;
-            if (list[idx] >= list[child]) break;
-            swap(list, idx, child);
+            if (child + 1 < len && arr[child] < arr[child + 1]) ++child;
+            if (arr[idx] >= arr[child]) break;
+            swap(arr, idx, child);
             idx = child;
         }
     }
 
-    public static void heapSort(Integer[] list) {
+    public static void heapSort(Integer[] arr) {
         
-        for (int i = list.length / 2 - 1; i >= 0; --i) {
-            shiftDown(list, list.length, i);
+        for (int i = arr.length / 2 - 1; i >= 0; --i) {
+            shiftDown(arr, arr.length, i);
         }
 
-        for (int i = list.length - 1; i > 0; --i) {
-            swap(list, 0, i);
-            shiftDown(list, i, 0);
+        for (int i = arr.length - 1; i > 0; --i) {
+            swap(arr, 0, i);
+            shiftDown(arr, i, 0);
         }
     }
 
+    public static void swap(Integer[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    
     public static void main(String[] args) {
-        Integer list[] = {5, 7, 3, 9, 2, 0, 1, 8, 6, 4};
+        int num = 20;
+        int rangeLeft = 1;
+        int rangeRight = 10000;
 
-        heapSort(list);
+        Integer[] arr = generateRandomIntegerArray(num, rangeLeft, rangeRight);
 
-        for (int i = 0; i < list.length; ++i) 
-            System.out.print(list[i] + " ");
-        System.out.println();
+        System.out.println("Original array: ");
+        printArray(arr);
+
+        heapSort(arr);
+
+        System.out.println("Sorted array: ");
+        printArray(arr);
     }
 
-    public static void swap(Integer[] list, int i, int j) {
-        int tmp = list[i];
-        list[i] = list[j];
-        list[j] = tmp;
+    /**
+      * @param num          the number of array elements
+      * @param rangeLeft  the left side of the range
+      * @param rangeRight the right side of the range
+      * @return an array containing N array element sizes between rangeLeft and rangeRight
+      */
+    public static Integer[] generateRandomIntegerArray(int num, int rangeLeft, int rangeRight) {
+
+        if (rangeLeft > rangeRight)
+            throw new RuntimeException("Range is incorrect, rangeLeft must be greater than rangeRight");
+
+        Integer[] arr = new Integer[num];
+        Integer range = rangeRight - rangeLeft;
+        Random random = new Random();
+        for (int i = 0; i < num; i++) {
+            arr[i] = random.nextInt(range) + rangeLeft;
+        }
+        return arr;
+    }
+
+    public static <T> void printArray(T arr[]) {
+        for (T t : arr) {
+            System.out.print(t + " ");
+        }
+        System.out.println();
     }
 }
