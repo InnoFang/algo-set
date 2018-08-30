04621406f590a111cc74420b8e9c2edcbf94448f
InnoFang <innofang@outlook.com>
2018-08-30 13:30:05 +0800

    :art: update the Java implementation of Bubble Sorting

--- Data Structures/Sort/Bubble Sort/Main.java
+++ Data Structures/Sort/Bubble Sort/Main.java
@@ -1,22 +1,57 @@
+import java.util.Random; 
+
 public class Main {
 
-    public static void bubbleSort(Integer[] list) {
-        for (int i = 0; i < list.length - 1; ++i) 
-            for (int j = 0; j < list.length - 1 - i; ++j) 
-                if (list[j] > list[j + 1]) {
-                    int tmp = list[j];
-                    list[j] = list[j + 1];
-                    list[j + 1] = tmp;
+    public static void bubbleSort(Integer[] arr) {
+        for (int i = 0; i < arr.length - 1; ++i) 
+            for (int j = 0; j < arr.length - 1 - i; ++j) 
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
                 } 
     }
 
     public static void main(String[] args) {
-        Integer list[] = {5, 7, 3, 9, 2, 0, 1, 8, 6, 4};
+        int num = 20;
+        int rangeLeft = 1;
+        int rangeRight = 10000;
 
-        bubbleSort(list);
+        Integer[] arr = generateRandomIntegerArray(num, rangeLeft, rangeRight);
 
-        for (int i = 0; i < list.length; ++i) 
-            System.out.print(list[i] + " ");
+        System.out.println("Original array: ");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+
+    /**
+      * @param num          the number of array elements
+      * @param rangeLeft  the left side of the range
+      * @param rangeRight the right side of the range
+      * @return an array containing N array element sizes between rangeLeft and rangeRight
+      */
+    public static Integer[] generateRandomIntegerArray(int num, int rangeLeft, int rangeRight) {
+
+        if (rangeLeft > rangeRight)
+            throw new RuntimeException("Range is incorrect, rangeLeft must be greater than rangeRight");
+
+        Integer[] arr = new Integer[num];
+        Integer range = rangeRight - rangeLeft;
+        Random random = new Random();
+        for (int i = 0; i < num; i++) {
+            arr[i] = random.nextInt(range) + rangeLeft;
+        }
+        return arr;
+    }
+
+    public static <T> void printArray(T arr[]) {
+        for (T t : arr) {
+            System.out.print(t + " ");
+        }
         System.out.println();
     }
 }
