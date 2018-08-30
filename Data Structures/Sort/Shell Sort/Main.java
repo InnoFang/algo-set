a29d6c8941b5ba4e5108d0bbdd880073d1a82778
InnoFang <innofang@outlook.com>
2018-08-30 13:13:40 +0800

    :art: optmize the Java implementation of Shell Sorting

--- Data Structures/Sort/Shell Sort/Main.java
+++ Data Structures/Sort/Shell Sort/Main.java
@@ -1,30 +1,65 @@
+import java.util.Random;
+
 public class Main {
 
-    public static void shellSort(Integer[] list) {
+    public static void shellSort(Integer[] arr) {
         int gap = 1;
 
-        while (gap < list.length / 3) {
+        while (gap < arr.length / 3) {
             gap = gap * 3 + 1;
         }
 
         for (; gap > 0; gap = (gap - 1) / 3) {
-            for (int i = gap; i < list.length; i += gap) {
-                int tmp = list[i], j = i;
-                for (; j > 0 && list[j - gap] > tmp; j -= gap) {
-                    list[j] = list[j - gap];
+            for (int i = gap; i < arr.length; i += gap) {
+                int tmp = arr[i], j = i;
+                for (; j > 0 && arr[j - gap] > tmp; j -= gap) {
+                    arr[j] = arr[j - gap];
                 }
-                list[j] = tmp;
+                arr[j] = tmp;
             }
         }
     }
-
+    
     public static void main(String[] args) {
-        Integer list[] = {5, 7, 3, 9, 2, 0, 1, 8, 6, 4};
+        int num = 20;
+        int rangeLeft = 1;
+        int rangeRight = 10000;
 
-        shellSort(list);
+        Integer[] arr = generateRandomIntegerArray(num, rangeLeft, rangeRight);
 
-        for (int i = 0; i < list.length; ++i) 
-            System.out.print(list[i] + " ");
+        System.out.println("Original array: ");
+        printArray(arr);
+
+        shellSort(arr);
+
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+
+    /**
+      * @param num          the number of array elements
+      * @param rangeLeft  the left side of the range
+      * @param rangeRight the right side of the range
+      * @return an array containing N array element sizes between rangeLeft and rangeRight
+      */
+      public static Integer[] generateRandomIntegerArray(int num, int rangeLeft, int rangeRight) {
+
+        if (rangeLeft > rangeRight)
+            throw new RuntimeException("Range is incorrect, rangeLeft must be greater than rangeRight");
+
+        Integer[] arr = new Integer[num];
+        Integer range = rangeRight - rangeLeft;
+        Random random = new Random();
+        for (int i = 0; i < num; i++) {
+            arr[i] = random.nextInt(range) + rangeLeft;
+        }
+        return arr;
+    }
+
+    public static <T> void printArray(T arr[]) {
+        for (T t : arr) {
+            System.out.print(t + " ");
+        }
         System.out.println();
     }
 }
