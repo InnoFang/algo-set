8b829b7ad96bd70c22eeaddd1ce630f288b4e9d7
InnoFang <innofang@outlook.com>
2018-08-31 13:21:07 +0800

    :art: update

--- Data Structures/Sort/Heap Sort/main.js
+++ Data Structures/Sort/Heap Sort/main.js
@@ -4,31 +4,53 @@ function shiftDown(arr, len, idx) {
         largest = idx;
     
     if (left < len && arr[largest] < arr[left])  {
-        largest = left
+        largest = left;
     }
 
     if (right < len && arr[largest] < arr[right])  {
-        largest = right
+        largest = right;
     }
     
     if (idx != largest) {
-        [arr[idx], arr[largest]] = [arr[largest], arr[idx]]
-        shiftDown(arr, len, largest)
+        [arr[idx], arr[largest]] = [arr[largest], arr[idx]];
+        shiftDown(arr, len, largest);
     }
 }
 
 function heap_sort(arr) {
     for (let i = Math.floor(arr.length / 2); i >= 0; --i) {
-        shiftDown(arr, arr.length, i)
+        shiftDown(arr, arr.length, i);
     }
   
     
     for (let i = arr.length - 1; i > 0; --i) {
-        [arr[0], arr[i]] = [arr[i], arr[0]]
-        shiftDown(arr, i, 0)
+        [arr[0], arr[i]] = [arr[i], arr[0]];
+        shiftDown(arr, i, 0);
     }
+} 
+
+function generateRandomArray(num, rangeFrom, rangeTo) {
+    if (rangeTo < rangeFrom) {
+        return;
+    }
+
+    const arr = new Array(num);
+    const range = rangeTo - rangeFrom + 1;
+    for (let i = 0; i < num; ++i) {
+        arr[i] = Math.floor(Math.random() * range + rangeFrom);
+    }
+    return arr;
 }
 
-const arr = [5, 7, 3, 9, 2, 0, 1, 8, 6, 4]
-heap_sort(arr)
+const num = 20;
+const rangeFrom = 1;
+const rangeTo = 10000;
+const arr = generateRandomArray(20, 1, 1000)
+
+console.log("Original array:");
+console.log(arr);
+
+heap_sort(arr);
+
+console.log("Sorted array:");
 console.log(arr)
