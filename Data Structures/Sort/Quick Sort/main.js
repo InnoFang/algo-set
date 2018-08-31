bacb2252020c53c344a72c2bc38a83e768e74a05
InnoFang <innofang@outlook.com>
2018-08-31 13:23:13 +0800

    :art: format the code

--- Data Structures/Sort/Quick Sort/main.js
+++ Data Structures/Sort/Quick Sort/main.js
@@ -1,19 +1,19 @@
 function partition(arr, left, right) {
-    let l = left, r = right, tmp = arr[left]
+    let l = left, r = right, tmp = arr[left];
     while (l != r) {
-        while (l < r && arr[r] >= tmp) --r
-        while (l < r && arr[l] <= tmp) ++l
-        if (l < r) [arr[r], arr[l]] = [arr[l], arr[r]]
+        while (l < r && arr[r] >= tmp) --r;
+        while (l < r && arr[l] <= tmp) ++l;
+        if (l < r) [arr[r], arr[l]] = [arr[l], arr[r]];
     }
-    [arr[l], arr[left]] = [arr[left], arr[l]]
-    return l
+    [arr[l], arr[left]] = [arr[left], arr[l]];
+    return l;
 }
 
 function quick_sort(arr, left, right) {
-    if (left > right) return
-    const pivot = partition(arr, left, right)
-    quick_sort(arr, left, pivot - 1)
-    quick_sort(arr, pivot + 1, right)
+    if (left > right) return;
+    const pivot = partition(arr, left, right);
+    quick_sort(arr, left, pivot - 1);
+    quick_sort(arr, pivot + 1, right);
 } 
 
 function generateRandomArray(num, rangeFrom, rangeTo) {
