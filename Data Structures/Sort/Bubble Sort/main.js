191872af524e2f8d4cd593f73b51c57d3423bfd6
InnoFang <innofang@outlook.com>
2018-08-31 13:21:57 +0800

    :art: update

--- Data Structures/Sort/Bubble Sort/main.js
+++ Data Structures/Sort/Bubble Sort/main.js
@@ -2,9 +2,31 @@ function bubble_sort(arr) {
     for (let i = 0; i < arr.length - 1; ++i) 
         for (let j = 0; j < arr.length - 1 - i; ++j) 
             if (arr[j] > arr[j + 1])
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+} 
+
+function generateRandomArray(num, rangeFrom, rangeTo) {
+    if (rangeTo < rangeFrom) {
+        return;
+    }
+
+    const arr = new Array(num);
+    const range = rangeTo - rangeFrom + 1;
+    for (let i = 0; i < num; ++i) {
+        arr[i] = Math.floor(Math.random() * range + rangeFrom);
+    }
+    return arr;
 }
 
-const arr = [5, 7, 3, 9, 2, 0, 1, 8, 6, 4]
-bubble_sort(arr)
+const num = 20;
+const rangeFrom = 1;
+const rangeTo = 10000;
+const arr = generateRandomArray(20, 1, 1000)
+
+console.log("Original array:");
+console.log(arr);
+
+bubble_sort(arr);
+
+console.log("Sorted array:");
 console.log(arr)
