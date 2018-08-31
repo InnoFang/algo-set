8d24433c8b1472cf8f8a18788e53cc0f0754f90c
InnoFang <innofang@outlook.com>
2018-08-31 13:20:19 +0800

    :art: update

--- Data Structures/Sort/Insertion Sort/main.js
+++ Data Structures/Sort/Insertion Sort/main.js
@@ -1,13 +1,35 @@
 function insertin_sort(arr) {
     for (let i = 1; i < arr.length; ++i) {
-        [tmp, j] = [arr[i], i] 
+        [tmp, j] = [arr[i], i];
         for (; j > 0 && arr[j - 1] > tmp; --j) {
             arr[j] = arr[j - 1];
         }
         arr[j] = tmp;
     }
+} 
+
+function generateRandomArray(num, rangeFrom, rangeTo) {
+    if (rangeTo < rangeFrom) {
+        return;
+    }
+
+    const arr = new Array(num);
+    const range = rangeTo - rangeFrom + 1;
+    for (let i = 0; i < num; ++i) {
+        arr[i] = Math.floor(Math.random() * range + rangeFrom);
+    }
+    return arr;
 }
 
-const arr = [5, 7, 3, 9, 2, 0, 1, 8, 6, 4]
+const num = 20;
+const rangeFrom = 1;
+const rangeTo = 10000;
+const arr = generateRandomArray(20, 1, 1000)
+
+console.log("Original array:");
+console.log(arr);
+
 insertin_sort(arr)
+
+console.log("Sorted array:");
 console.log(arr)
