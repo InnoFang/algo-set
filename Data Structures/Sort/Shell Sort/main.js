f0a43431a123c91c57c2c8c37daa89d6bfa23640
InnoFang <innofang@outlook.com>
2018-08-31 13:22:22 +0800

    :art: format the code

--- Data Structures/Sort/Shell Sort/main.js
+++ Data Structures/Sort/Shell Sort/main.js
@@ -2,16 +2,16 @@ function shell_sort(arr) {
     let gap = 1;
 
     while (gap < arr.length / 3) {
-        gap = gap * 3 + 1
+        gap = gap * 3 + 1;
     }
 
     for (; gap > 0; gap = (gap - 1) / 3) {
         for (let i = gap; i < arr.length; i += gap) {
-            [tmp, j] = [arr[i], i]
+            [tmp, j] = [arr[i], i];
             for (; j > 0 && arr[j - gap] > tmp; j -= gap) {
-                arr[j] = arr[j - gap]
+                arr[j] = arr[j - gap];
             }
-            arr[j] = tmp
+            arr[j] = tmp;
         }
     }
 } 
