4f59f240c498a8e7d9d47f544a079abaad93886e
InnoFang <innofang@outlook.com>
2018-08-31 13:23:02 +0800

    :art: format the code

--- Data Structures/Sort/Selection Sort/main.js
+++ Data Structures/Sort/Selection Sort/main.js
@@ -1,13 +1,13 @@
 function selection_sort(arr) {
     for (let i = 0; i < arr.length; ++i) {
-        idx = i
+        idx = i;
         for (let j = i + 1; j < arr.length; ++j) {
             if (arr[idx] > arr[j]) {
-                idx = j
+                idx = j;
             }
         }
         if (idx != i) {
-            [arr[idx], arr[i]] = [arr[i], arr[idx]]
+            [arr[idx], arr[i]] = [arr[i], arr[idx]];
         }
     }
 }
