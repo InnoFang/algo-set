b4506690ceb85e0a1deab20d8448f04c1dbf25de
InnoFang <innofang@outlook.com>
2018-09-02 13:02:44 +0800

    :art: update the Radix Sort Java code

--- Data Structures/Sort/Radix Sort/Main.java
+++ Data Structures/Sort/Radix Sort/Main.java
@@ -3,16 +3,15 @@ import java.util.Random;
 public class Main {
 
     public static void radixSort(Integer[] arr, int maxDigit) {  
-        int digit = 1;                                      /* represent the each digit for given number */
-        int index = 0;                                      /* arr's index */
         Integer[][] bucket = new Integer[10][arr.length];   /* radix bucket */
         int[] cnt = new int[10];                            /* store the number of each digit */
-        for (; digit < maxDigit; digit *= 10, index = 0) {
+        for (int digit = 1; digit < maxDigit; digit *= 10) {
             for (Integer num: arr) {                        /* put each number into the bucket */
                 int d = (num / digit) % 10;
                 bucket[d][cnt[d]] = num;
                 cnt[d]++;
             }
+            int index = 0;            
             for (int i = 0; i < 10; ++i) {                  /* assign the numbers in the bucket back to arr */
                 if (cnt[i] != 0) {
                     for (int j = 0; j < cnt[i]; ++j) {
