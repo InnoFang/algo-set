e5c1ad38cdc4a432af7d817fc362f21a940105fa
InnoFang <innofang@outlook.com>
2018-09-02 13:30:32 +0800

    :art: update the Heap Sort Kotlin code

--- Data Structures/Sort/Heap Sort/main.kt
+++ Data Structures/Sort/Heap Sort/main.kt
@@ -1,33 +1,59 @@
-fun shiftDown(list: IntArray, len: Int, idx: Int) {
+import java.util.Random
+
+fun shiftDown(arr: IntArray, len: Int, idx: Int) {
     val left = idx * 2 + 1
     val right = idx * 2 + 2
     var largest = idx
 
-    if (left < len && list[largest] < list[left]) largest = left
-    if (right < len && list[largest] < list[right]) largest = right
+    if (left < len && arr[largest] < arr[left]) largest = left
+    if (right < len && arr[largest] < arr[right]) largest = right
 
     if (idx != largest) {
-        (list[idx] to list[largest]).run{ list[idx] = second; list[largest] = first }
-        shiftDown(list, len, largest)
+        (arr[idx] to arr[largest]).run{ arr[idx] = second; arr[largest] = first }
+        shiftDown(arr, len, largest)
     }
 }
 
-fun heapSort(list: IntArray) {
-    for (i in list.size / 2 - 1 downTo 0) {
-        shiftDown(list, list.size, i)
+fun heapSort(arr: IntArray) {
+    for (i in arr.size / 2 - 1 downTo 0) {
+        shiftDown(arr, arr.size, i)
     }
 
-    for (i in list.lastIndex downTo 1) {
-        (list[i] to list[0]).run{ list[i] = second; list[0] = first }
-        shiftDown(list, i, 0)
+    for (i in arr.lastIndex downTo 1) {
+        (arr[i] to arr[0]).run{ arr[i] = second; arr[0] = first }
+        shiftDown(arr, i, 0)
     }
-}
+} 
 
 fun main(args: Array<String>) {
-    val list = intArrayOf(5, 7, 3, 9, 2, 0, 1, 8, 6, 4)
+    val num = 20
+    val rangeLeft = 0
+    val rangeRight = 10000
 
-    heapSort(list)
+    val arr = generateRandomArray(num, rangeLeft, rangeRight)
 
-    list.forEach{ print("$it ") }
+    println("Original array:")
+    printIntArray(arr)
+
+    heapSort(arr)
+
+    println("Sorted array:")
+    printIntArray(arr)
+}
+
+fun generateRandomArray(num: Int, rangeLeft: Int, rangeRight: Int): IntArray {
+    val range = rangeRight - rangeLeft
+    val arr = IntArray(num)
+    val ran = Random()
+    for (i in 0 until num) {
+        arr[i] = ran.nextInt(range) + rangeLeft
+    }
+    return arr
+}
+
+fun printIntArray(arr: IntArray) {
+    for (t in arr) {
+        print("$t ")
+    }
     println()
 }
