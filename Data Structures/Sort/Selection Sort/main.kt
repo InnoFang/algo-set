79e1915fbd6136f1b9128c6afa2ec2f0299fd88b
InnoFang <innofang@outlook.com>
2018-09-02 13:20:12 +0800

    :art: update the Selection Sort Kotlin code

--- Data Structures/Sort/Selection Sort/main.kt
+++ Data Structures/Sort/Selection Sort/main.kt
@@ -1,25 +1,51 @@
-fun selectionSort(list: IntArray) {
-    for (i in 0 .. list.lastIndex) {
+import java.util.Random
+
+fun selectionSort(arr: IntArray) {
+    for (i in 0 .. arr.lastIndex) {
         var idx = i
-        for (j in i+1 .. list.lastIndex) {
-            if (list[idx] > list[j]) {
+        for (j in i+1 .. arr.lastIndex) {
+            if (arr[idx] > arr[j]) {
                 idx = j
             }
         }
         if (idx != i) {
-            (list[idx] to list[i]).run {
-                    list[idx] = second
-                    list[i] = first 
+            (arr[idx] to arr[i]).run {
+                    arr[idx] = second
+                    arr[i] = first 
                 }
         }
     }
-}
+} 
 
 fun main(args: Array<String>) {
-    val list = intArrayOf(5, 7, 3, 9, 2, 0, 1, 8, 6, 4)
+    val num = 20
+    val rangeLeft = 0
+    val rangeRight = 10000
 
-    selectionSort(list)
+    val arr = generateRandomArray(num, rangeLeft, rangeRight)
 
-    list.forEach{ print("$it ") }
+    println("Original array:")
+    printIntArray(arr)
+
+    selectionSort(arr)
+
+    println("Sorted array:")
+    printIntArray(arr)
+}
+
+fun generateRandomArray(num: Int, rangeLeft: Int, rangeRight: Int): IntArray {
+    val range = rangeRight - rangeLeft
+    val arr = IntArray(num)
+    val ran = Random()
+    for (i in 0 until num) {
+        arr[i] = ran.nextInt(range) + rangeLeft
+    }
+    return arr
+}
+
+fun printIntArray(arr: IntArray) {
+    for (t in arr) {
+        print("$t ")
+    }
     println()
 }
