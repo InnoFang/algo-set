9509ed138833f48d83510cfa4942bae9b13dcc58
InnoFang <innofang@outlook.com>
2018-09-02 13:17:37 +0800

    :art: update the Shell Sort Kotlin code

--- Data Structures/Sort/Shell Sort/main.kt
+++ Data Structures/Sort/Shell Sort/main.kt
@@ -1,24 +1,50 @@
-fun shellSort(list: IntArray) {
-    var gap = list.size / 2
+import java.util.Random
+
+fun shellSort(arr: IntArray) {
+    var gap = arr.size / 2
     while (gap > 0) {
-        for (i in gap until list.size) {
-            val tmp = list[i]
+        for (i in gap until arr.size) {
+            val tmp = arr[i]
             var j = i
-            while (j >= gap && list[j - gap] > tmp) {
-                list[j] = list[j - gap]
+            while (j >= gap && arr[j - gap] > tmp) {
+                arr[j] = arr[j - gap]
                 j -= gap
             }
-            list[j] = tmp
+            arr[j] = tmp
         }
         gap /= 2
     }
-}
+} 
 
 fun main(args: Array<String>) {
-    val list = intArrayOf(5, 7, 3, 9, 2, 0, 1, 8, 6, 4)
+    val num = 20
+    val rangeLeft = 0
+    val rangeRight = 10000
 
-    shellSort(list)
+    val arr = generateRandomArray(num, rangeLeft, rangeRight)
 
-    list.forEach{ print("$it ") }
+    println("Original array:")
+    printIntArray(arr)
+
+    shellSort(arr)
+
+    println("Sorted array:")
+    printIntArray(arr)
+}
+
+fun generateRandomArray(num: Int, rangeLeft: Int, rangeRight: Int): IntArray {
+    val range = rangeRight - rangeLeft
+    val arr = IntArray(num)
+    val ran = Random()
+    for (i in 0 until num) {
+        arr[i] = ran.nextInt(range) + rangeLeft
+    }
+    return arr
+}
+
+fun printIntArray(arr: IntArray) {
+    for (t in arr) {
+        print("$t ")
+    }
     println()
 }
