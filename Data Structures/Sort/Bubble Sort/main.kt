9922d32881300e6dbf36d2d30d63828167027536
InnoFang <innofang@outlook.com>
2018-09-02 13:31:55 +0800

    :art: update the Bubble Sort Kotlin code

--- Data Structures/Sort/Bubble Sort/main.kt
+++ Data Structures/Sort/Bubble Sort/main.kt
@@ -1,21 +1,47 @@
-fun bubbleSort(list: IntArray) {
-    for (i in 0 until list.lastIndex) {
-        for (j in 0 until list.lastIndex - i) {
-            if (list[j] > list[j + 1]) {
-                (list[j] to list[j + 1]).run {
-                    list[j] = second
-                    list[j + 1] = first 
+import java.util.Random
+
+fun bubbleSort(arr: IntArray) {
+    for (i in 0 until arr.lastIndex) {
+        for (j in 0 until arr.lastIndex - i) {
+            if (arr[j] > arr[j + 1]) {
+                (arr[j] to arr[j + 1]).run {
+                    arr[j] = second
+                    arr[j + 1] = first 
                 }
             }
         }
     }
-}
+} 
 
 fun main(args: Array<String>) {
-    val list = intArrayOf(5, 7, 3, 9, 2, 0, 1, 8, 6, 4)
+    val num = 20
+    val rangeLeft = 0
+    val rangeRight = 10000
 
-    bubbleSort(list)
+    val arr = generateRandomArray(num, rangeLeft, rangeRight)
 
-    list.forEach{ print("$it ") }
+    println("Original array:")
+    printIntArray(arr)
+
+    bubbleSort(arr)
+
+    println("Sorted array:")
+    printIntArray(arr)
+}
+
+fun generateRandomArray(num: Int, rangeLeft: Int, rangeRight: Int): IntArray {
+    val range = rangeRight - rangeLeft
+    val arr = IntArray(num)
+    val ran = Random()
+    for (i in 0 until num) {
+        arr[i] = ran.nextInt(range) + rangeLeft
+    }
+    return arr
+}
+
+fun printIntArray(arr: IntArray) {
+    for (t in arr) {
+        print("$t ")
+    }
     println()
 }
