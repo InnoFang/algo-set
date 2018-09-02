e23eb5b7e2671dfecc8d7b71a3209074400c69bb
InnoFang <innofang@outlook.com>
2018-09-02 13:28:56 +0800

    :art: update the Insertion Sort Code

--- Data Structures/Sort/Insertion Sort/main.kt
+++ Data Structures/Sort/Insertion Sort/main.kt
@@ -1,20 +1,46 @@
-fun insertionSort(list: IntArray) {
-    for (i in 1..list.lastIndex) {
-        var tmp = list[i]
+import java.util.Random
+
+fun insertionSort(arr: IntArray) {
+    for (i in 1..arr.lastIndex) {
+        var tmp = arr[i]
         var j = i
-        while (j > 0 && list[j - 1] > tmp) {
-            list[j] = list[j - 1]
+        while (j > 0 && arr[j - 1] > tmp) {
+            arr[j] = arr[j - 1]
             --j
         }
-        list[j] = tmp
+        arr[j] = tmp
     }
-}
+} 
 
 fun main(args: Array<String>) {
-    val list = intArrayOf(5, 7, 3, 9, 2, 0, 1, 8, 6, 4)
+    val num = 20
+    val rangeLeft = 0
+    val rangeRight = 10000
 
-    insertionSort(list)
+    val arr = generateRandomArray(num, rangeLeft, rangeRight)
 
-    list.forEach{ print("$it ") }
+    println("Original array:")
+    printIntArray(arr)
+
+    insertionSort(arr)
+
+    println("Sorted array:")
+    printIntArray(arr)
+}
+
+fun generateRandomArray(num: Int, rangeLeft: Int, rangeRight: Int): IntArray {
+    val range = rangeRight - rangeLeft
+    val arr = IntArray(num)
+    val ran = Random()
+    for (i in 0 until num) {
+        arr[i] = ran.nextInt(range) + rangeLeft
+    }
+    return arr
+}
+
+fun printIntArray(arr: IntArray) {
+    for (t in arr) {
+        print("$t ")
+    }
     println()
 }
