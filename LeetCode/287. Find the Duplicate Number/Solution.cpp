b5dc26bc6b1cf68718b800c5f710f831ea56b307
InnoFang <innofang@outlook.com>
2018-12-28 21:40:37 +0800

    :art: update T287 cpp solution

--- LeetCode/287. Find the Duplicate Number/Solution.cpp
+++ LeetCode/287. Find the Duplicate Number/Solution.cpp
@@ -3,17 +3,21 @@ using namespace std;
 
 /**
  * Status: Accepted
- * Runtime: 16ms
+ * Runtime: 8ms
  */
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> dict;
-        for (int i = 0; i < nums.size(); ++ i) {
-            if (dict.find(nums[i]) == dict.end()) dict[nums[i]] = 0;
-            dict[nums[i]]++;
-            if (dict[nums[i]] == 2) return nums[i];
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int cnt = 0;
+            for (int n : nums) {
+                if (n <= mid) ++cnt;
+            }
+            if (cnt <= mid) left = mid + 1;
+            else right = mid;
         }
-        return -1;
+        return left;
     }
 };
