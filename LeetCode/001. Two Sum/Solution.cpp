79de0c56e6762ea950d45d30dfd5e56f89bfabb1
InnoFang <innofang@outlook.com>
2018-12-29 11:55:09 +0800

    :art: upload T1 cpp solution

---
+++ LeetCode/001. Two Sum/Solution.cpp
@@ -0,0 +1,21 @@
+#include <unordered_map>
+/**
+ * Status: Accepted
+ * Runtime: 8ms
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> dic;
+        for (int i = 0; i < nums.size(); i++) {
+            int val = target - nums[i];
+            if (dic.find(val) != dic.end()) {
+                vector<int> res;
+                res.push_back(dic[val]);
+                res.push_back(i);
+                return res;
+            } else dic[nums[i]] = i;
+        }
+        return vector<int>();
+    }
+};
