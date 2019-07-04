3b9c8b8a52a6a2ad1c6d1718b6bb625eed1616aa
InnoFang <innofang@outlook.com>
2019-07-04 20:27:03 +0800

    :sparkles: T824 cpp solution

---
+++ LeetCode/824. Goat Latin/Solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    string toGoatLatin(string S) {
+        vector<string> words;
+        stringstream input(S);
+        unordered_set<char> vowel {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        string tmp, ans;
+        while (getline(input, tmp, ' ')) words.push_back(tmp);
+        for (int i = 0; i < words.size(); ++ i) {
+            if (vowel.find(words[i][0]) == vowel.end()) {
+                words[i] = words[i].substr(1) + words[i][0];
+            }
+            words[i] += "ma";
+            for (int j = 0; j <= i; ++ j) words[i] += 'a';
+            ans += words[i] + " ";
+        }
+        return ans.substr(0, ans.size() - 1);
+    }
+};
+
+int main () {
+    string test = "The quick brown fox jumped over the lazy dog";
+    
+    cout << Solution().toGoatLatin(test) << endl;
+    return 0;
+}
