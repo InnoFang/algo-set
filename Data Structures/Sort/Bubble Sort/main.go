bb173b09d9bd351a5e994ceb6de4bd9559c5f37b
InnoFang <innofang@outlook.com>
2020-02-15 18:36:20 +0800

    :sparkles: Bubble Sort implemented by Go

---
+++ Data Structures/Sort/Bubble Sort/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr) - 1; i++ {
+		for j := 0; j < len(arr) - 1 - i; j++ {
+			if arr[j] > arr[j + 1] {
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+			}
+		}
+	}
+}
+
+func main() {
+	total := 20
+	left  := 1
+	right := 1000
+	
+	arr := generateRandomArray(total, left, right)
+
+	fmt.Println("Origin array: ", arr)
+	
+	bubbleSort(arr)
+	
+	fmt.Println("Sorted array: ", arr)
+}
+
+func generateRandomArray(total int, left int, right int) []int {
+	if left > right {
+		panic("Range is incorrect, rangeLeft must be greater than rangeRight")
+	}
+
+	arr := make([]int, total)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < total; i++ {
+		arr[i] = rand.Intn(right - left) + left
+	}
+	return arr
+}
