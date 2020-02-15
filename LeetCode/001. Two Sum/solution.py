f9d1d46e5719a44ef565f4fdad0b39ae0c326f81
InnoFang <innofang@outlook.com>
2020-02-15 16:19:17 +0800

    :sparkles: add T1 python solution

---
+++ LeetCode/001. Two Sum/solution.py
@@ -0,0 +1,13 @@
+"""
+29 / 29 test cases passed.
+Status: Accepted
+Runtime: 72 ms
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = dict()
+        for idx, elm in enumerate(nums):
+            if (target - elm) in store:
+                return [store[target - elm], idx]
+            store[elm] = idx
+    
