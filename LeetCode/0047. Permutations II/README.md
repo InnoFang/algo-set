128704245680885f4a00dfabeec299914d355efc
InnoFang <innofang@outlook.com>
2020-03-07 18:31:24 +0800

    :sparkles: T47 kotlin solution

---
+++ LeetCode/0047. Permutations II/README.md
@@ -0,0 +1,24 @@
+# [47. Permutations II](https://leetcode.com/problems/permutations-ii/)
+
+
+![](https://img.shields.io/badge/Difficulty-Medium-F8AF40.svg)
+
+<details>
+<summary style="color:#4FC3F7">Topics</summary>
+
+* [`Backtracking/`](https://leetcode.com/tag/backtracking//)
+
+</details>
+<br />
+
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+**Example:**
+
+    **Input**: [1,1,2]
+    **Output**:
+    [
+    [1,1,2],
+    [1,2,1],
+    [2,1,1]
+    ]
---
+++ LeetCode/0047. Permutations II/Solution.kt
@@ -0,0 +1,36 @@
+package algorithm._47_Permutations_II
+
+/**
+ * Created by Inno Fang on 2018/1/2.
+ */
+class Solution {
+    private val res = ArrayList<List<Int>>()
+    fun permuteUnique(nums: IntArray): List<List<Int>> {
+        permutation(nums, 0, nums.size - 1)
+        return res
+    }
+
+    private fun permutation(list: IntArray, start: Int, length: Int) {
+        if (start == length && !res.contains(list.toMutableList())) res.add(list.toMutableList())
+        else (start..length).forEach { i ->
+            swap(list, start, i)
+            permutation(list, start + 1, length)
+            swap(list, start, i)
+        }
+    }
+
+    private fun swap(nums: IntArray, i: Int, j: Int) {
+        val temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+    }
+}
+
+fun main(vararg args: String) {
+    Solution().permuteUnique(intArrayOf(1, 1, 2)).forEach {
+        it.forEach {
+            print("$it ")
+        }
+        println()
+    }
+}
