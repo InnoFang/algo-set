e0fdbec97c0653ed2e5ece1d4d01feb6d1cbb562
InnoFang <innofang@outlook.com>
2020-03-23 22:14:03 +0800

    :sparkles: use python to generate leetcode list

---
+++ LeetCode/leetcode.py
@@ -0,0 +1,44 @@
+import os
+import re
+
+solution = {
+    'dir_name' : '',
+    'dir_url'  : '',
+    'file_urls': {}
+}
+
+suffix = ["cpp", "java", "js", "py", "go", "kt"]
+leetcode_loc = '.'
+
+def main():
+    solution_list = []
+    for dir_url, _, files in os.walk(leetcode_loc):
+        dir_name = dir_url.split('\\')[-1]
+        if re.match(r'^[0-9]{4}\. .*', dir_name):
+            solution['dir_name'] = dir_name
+            solution['dir_url']  = os.path.join('LeetCode', dir_url[2:]) 
+            solution['file_urls'] = {f.split('.')[-1] : dir_url + '/' + f for f in files}
+            solution_list.append(solution.copy())
+    solution_list = solution_list[1:]
+
+    with open('../leetcode.md', 'w', encoding="utf-8") as md:
+        md.write("# Leetcode\n**Problems have been solved:** " + str(len(solution_list)))
+        md.write("\n\n")
+        md.write("Problem | Cpp | Java | JS | Python | Go | Kotlin\n")
+        md.write("---|---|---|---|---|---|---\n")
+
+        for s in solution_list:
+            md.write("[{}]({})".format(s['dir_name'], s['dir_url'].replace(" ", "%20")))
+            for ext in suffix:
+                md.write('|')
+                if ext in s['file_urls']:
+                    md.write("[{}]({})".format(ext, s['file_urls'][ext].replace(" ", "%20")))
+            md.write('\n')
+        md.write("\n\n------\n\n")
+        md.write("来源：力扣（LeetCode）\n\n")
+        md.write("链接：https://leetcode-cn.com/problemset/all/\n\n")
+        md.write("著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。\n\n")
+
+if __name__ == '__main__':
+    main()
+    
