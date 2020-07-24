868c0ff685a4ed562e6293768ba29a879ed32cec
InnoFang <innofang@outlook.com>
2020-07-25 00:57:17 +0800

    :art: update

---
+++ oj/level-1/Q3/main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+
+    int T;
+    while(~scanf("%d", &T)) { 
+        for (int cas = 1; cas <= T; cas++) {
+            int N;
+            cin >> N;
+            
+            int cnt = 0, limit = sqrt(2 * N);
+
+            // 等差数列求和正好为 N ：N = n * (x + x + n - 1) / 2
+            // => x = (2N + n - n^2) / 2n
+            for (int n = 2; n <= limit; n++) {
+                if ((2 * N + n - n * n) % (2 * n) == 0)
+                    ++cnt;
+            }
+
+            printf("Case #%d: %d\n", cas, cnt);
+        }
+    }
+
+#ifndef ONLINE_JUDGE
+    fclose(stdin);
+#endif
+
+    return 0;
+}
