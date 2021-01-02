5f77eff1c424a6cd184cf3d884152daf8c16ecbc
InnoFang <innofang@outlook.com>
2021-01-02 11:57:50 +0800

    :sparkles: update T1464 python solution

--- LeetCode/1464. Maximum Product of Two Elements in an Array/solution.py
+++ LeetCode/1464. Maximum Product of Two Elements in an Array/solution.py
@@ -5,5 +5,16 @@ Runtime: 40 ms
 """
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        s = sorted(nums)
-        return (s[-1] - 1) * (s[-2] - 1)
+        sorted_nums = sorted(nums)
+        return (sorted_nums[-1] - 1) * (sorted_nums[-2] - 1)
+
+"""
+104 / 104 test cases passed.
+Status: Accepted
+Runtime: 44 ms
+"""
+class Solution2:
+    def maxProduct(self, nums: List[int]) -> int:
+        maximun = max(nums)
+        nums.remove(maximun)
+        return (maximun - 1) * (max(nums) - 1)
