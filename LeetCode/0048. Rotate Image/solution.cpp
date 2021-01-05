faebe2a05042ebc7bfa908fade8916f352ff49b9
InnoFang <innofang@outlook.com>
2021-01-05 10:42:17 +0800

    :tada: upload T48 cpp solution

---
+++ LeetCode/0048. Rotate Image/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * 21 / 21 test cases passed.
+ * Status: Accepted
+ * Runtime: 4 ms
+ */
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix[0].size();
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 0; j < i; ++ j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 0; j < n / 2; ++ j) {
+                swap(matrix[i][j], matrix[i][n - 1 - j]);
+            }
+        }
+    }
+};
