1d497b8865fd21e07279dca98da99404953b7c2f
InnoFang <innofang@outlook.com>
2021-01-08 11:58:58 +0800

    :tada: upload T189 cpp solution

---
+++ LeetCode/0189. Rotate Array/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * 35 / 35 test cases passed.
+ * Status: Accepted
+ * Runtime: 8 ms
+ */
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int sz = nums.size();
+        int step = k % sz;
+        for (int i = 0; i < (sz - step) / 2; ++ i) {
+            swap(nums[i], nums[sz - step - 1 - i]);
+        }
+        for (int i = 0; i < step / 2; ++ i) {
+            swap(nums[sz - step + i], nums[sz -1 - i]);
+        }
+        for (int i = 0; i < sz / 2; ++ i) {
+            swap(nums[i], nums[sz - 1 - i]);
+        }
+    }
+};
+
