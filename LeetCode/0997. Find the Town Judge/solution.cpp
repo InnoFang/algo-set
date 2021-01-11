4f8e22cf34e855305d6245a7846c0f9d4ec7d566
InnoFang <innofang@outlook.com>
2021-01-11 11:25:43 +0800

    :tada: upload T997 cpp solution

---
+++ LeetCode/0997. Find the Town Judge/solution.cpp
@@ -0,0 +1,23 @@
+/**
+ * 92 / 92 test cases passed.
+ * Status: Accepted
+ * Runtime: 384 ms
+ */
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        vector<int> outDegree(N + 1, 0);
+        vector<int> inDegree(N + 1, 0);
+        for (auto& pair: trust) {
+            outDegree[pair[0]] ++;
+            inDegree[pair[1]] ++;
+        }
+        for (int i = 1; i <= N; ++ i) {
+            if (inDegree[i] == N -1 && outDegree[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
