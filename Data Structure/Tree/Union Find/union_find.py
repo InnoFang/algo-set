2182f830e1dc83e004db8cd91d29195aabde8cad
InnoFang <innofang@outlook.com>
2021-01-21 09:50:11 +0800

    :art: update union_find.py

--- Data Structure/Tree/Union Find/union_find.py
+++ Data Structure/Tree/Union Find/union_find.py
@@ -1,8 +1,7 @@
 class UnionFind:
-    def __init__(self, count):
-        self.count  = count
-        self.parent = [i for i in range(count)]
-        self.rank   = [1 for i in range(count)]
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank   = [1] * n
 
     def find(self, p):
         if p != self.parent[p]:
@@ -13,7 +12,7 @@ class UnionFind:
         root_p, root_q = self.find(p), self.find(q)
 
         if root_p == root_q:
-            return 
+            return False
         
         rank_p, rank_q = self.rank[p], self.rank[q]
         if rank_p < rank_q:
@@ -23,6 +22,8 @@ class UnionFind:
         else:
             self.parent[root_p] = root_q
             self.rank[root_p] += 1
+        
+        return True
         
     def is_connected(self, p, q):
         return self.find(p) == self.find(q) 
