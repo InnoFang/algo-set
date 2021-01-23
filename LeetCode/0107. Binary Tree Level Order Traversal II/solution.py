52322b15908517c8ab67e91ef7a82f1548dfa9c2
InnoFang <innofang@outlook.com>
2021-01-23 13:54:53 +0800

    :sparkles: upload T107 python solution

---
+++ LeetCode/0107. Binary Tree Level Order Traversal II/solution.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+"""
+34 / 34 test cases passed.
+Status: Accepted
+Runtime: 32 ms
+"""
+from collections import deque
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+        que = deque([root])
+        que_size = len(que)
+        traversal, level = [], []
+        while len(que) != 0:
+            node = que.popleft()
+            level.append(node.val)
+            if node.left:
+                que.append(node.left)
+            if node.right:
+                que.append(node.right)
+            que_size -= 1
+            if que_size == 0:
+                traversal.append(level)
+                level = []
+                que_size = len(que)
+        
+        traversal.reverse()
+        return traversal
+
+    
