d64baa440eb9a07323c18185759b9231c8c8c411
InnoFang <innofang@outlook.com>
2021-01-29 20:44:33 +0800

    :sparkles: update T401 python solution

--- LeetCode/0401. Binary Watch/solution.py
+++ LeetCode/0401. Binary Watch/solution.py
@@ -12,3 +12,11 @@ class Solution:
                     ans.append("%d:%02d" % (h, m))
         return ans
 
+"""
+10 / 10 test cases passed.
+Status: Accepted
+Runtime: 32 ms
+"""
+class Solution2:
+    def readBinaryWatch(self, num: int) -> List[str]:
+        return ["%d:%02d" % (h, m) for h in range(12) for m in range(60) if (bin(h) + bin(m)).count('1') == num]
