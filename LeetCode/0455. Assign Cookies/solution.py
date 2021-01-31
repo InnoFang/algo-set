e2cbcf3bb822721edceab84fdd6d948df4b1178b
InnoFang <innofang@outlook.com>
2021-01-31 13:55:41 +0800

    :sparkles: update T455 python solution

--- LeetCode/0455. Assign Cookies/solution.py
+++ LeetCode/0455. Assign Cookies/solution.py
@@ -17,3 +17,18 @@ class Solution:
                 ans += 1
                 candy += 1
         return ans
+
+"""
+21 / 21 test cases passed.
+Status: Accepted
+Runtime: 68 ms
+"""
+class Solution2:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        child = 0
+        for candy in s:
+            if child < len(g) and child < len(s) and g[child] <= candy:
+                child += 1
+        return child
