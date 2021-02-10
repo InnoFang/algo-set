0a4f5fbf563a52033639a13b4b8c08087699e966
InnoFang <innofang@outlook.com>
2021-02-10 11:29:53 +0800

    :sparkles: upload T567 cpp solution

---
+++ LeetCode/0567. Permutation in String/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * 103 / 103 test cases passed.
+ * Status: Accepted
+ * Runtime: 4 ms
+ */
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        if (m > n) return false;
+
+        vector<int> cnt1(26);
+        vector<int> cnt2(26);
+        for (int i = 0; i < m; ++ i) {
+            cnt1[s1[i] - 'a'] ++;
+            cnt2[s2[i] - 'a'] ++;
+        }
+        if (cnt1 == cnt2) return true;
+
+        for (int i = m; i < n; ++ i) {
+            cnt2[s2[i - m] - 'a'] --;
+            cnt2[s2[i] - 'a'] ++;
+            if (cnt1 == cnt2)
+                return true;
+        }
+        return false;
+    }
+};
+
