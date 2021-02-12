efc65fe3a74fc6a37ea70fcf162502c1d92135de
InnoFang <innofang@outlook.com>
2021-02-13 00:37:49 +0800

    :art: update T448 cpp solution

--- LeetCode/0448. Find All Numbers Disappeared in an Array/solution.cpp
+++ LeetCode/0448. Find All Numbers Disappeared in an Array/solution.cpp
@@ -21,3 +21,24 @@ public:
     }
 };
 
+/**
+ * 34 / 34 test cases passed.
+ * Status: Accepted
+ * Runtime: 48 ms
+ */
+class Solution2 {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        for (auto& num : nums) {
+            nums[(num - 1) % nums.size()] += nums.size();
+        }
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (nums[i] <= nums.size()) {
+                ans.push_back(i + 1);
+            }
+        }
+        return ans;
+    }
+};
+
