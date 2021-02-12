b2a7fb528027af4ce81dd53a5a6a8e64e2aff0a0
InnoFang <innofang@outlook.com>
2021-02-12 13:07:16 +0800

    :art: update T41 cpp solution

--- LeetCode/0041. First Missing Positive/solution.cpp
+++ LeetCode/0041. First Missing Positive/solution.cpp
@@ -19,3 +19,24 @@ public:
     }
 };
 
+/**
+ * 169 / 169 test cases passed.
+ * Status: Accepted
+ * Runtime: 0 ms
+ */
+class Solution2 {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for (auto& num : nums) {
+            while (num > 0 && num <= nums.size() && nums[num - 1] != num) {
+                swap(nums[num - 1], num);
+            }
+        }
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (nums[i] != i + 1) 
+                return i + 1; 
+        }
+        return nums.size() + 1;
+    }
+};
+
