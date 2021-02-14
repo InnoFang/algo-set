cb79a45486a3d1ca776d8af871054171b8ac571a
InnoFang <innofang@outlook.com>
2021-02-14 23:46:02 +0800

    :art: update T765 cpp solution

--- LeetCode/0765. Couples Holding Hands/solution.cpp
+++ LeetCode/0765. Couples Holding Hands/solution.cpp
@@ -26,3 +26,37 @@ public:
     }
 };
 
+/**
+ * 56 / 56 test cases passed.
+ * Status: Accepted
+ * Runtime: 0 ms
+ */
+// refer2: https://leetcode-cn.com/problems/couples-holding-hands/solution/cbing-cha-ji-best-100wo-hen-a-by-feng-zi-iw0m/
+class Solution {
+public:
+    int find(int x) {
+        return x == p[x] ? p[x] : p[x] = find(p[x]);
+    }
+    void merge(int a, int b) {
+        p[find(a)] = p[find(b)];
+    }
+    int minSwapsCouples(vector<int>& row) {
+        int n = row.size();
+        p.resize(n);
+        for (int i = 0; i < n; ++ i) p[i] = i;
+        for (int i = 0; i < n; i += 2) {
+            merge(row[i] / 2, row[i + 1] / 2);
+        }
+        int cnt = 0;
+        for (int i = 0; i < n / 2; ++ i) {
+            if (find(i) == i) {
+                cnt ++;
+            }
+        }
+        // 情侣总数 - 并查集个数
+        return n / 2 - cnt;
+    }
+private:
+    vector<int> p;
+};
+
