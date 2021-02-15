90ff364f4db0b8ae502fbb593aabd3d65ef705fc
InnoFang <innofang@outlook.com>
2021-02-16 00:14:50 +0800

    :sparkles: upload T561 cpp solution'

---
+++ LeetCode/0561. Array Partition I/solution.cpp
@@ -0,0 +1,17 @@
+/**
+ * 83 / 83 test cases passed.
+ * Status: Accepted
+ * Runtime: 56 ms
+ */
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
+
