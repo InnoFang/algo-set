8893d4c455e408761a9adea4a1353ad51b135cdb
InnoFang <innofang@outlook.com>
2021-02-18 16:50:27 +0800

    :art: update T995 cpp solution

--- LeetCode/0995. Minimum Number of K Consecutive Bit Flips/solution.cpp
+++ LeetCode/0995. Minimum Number of K Consecutive Bit Flips/solution.cpp
@@ -3,7 +3,6 @@
  * Runtime: 84 ms
  * Memory Usage: 70.6 MB 
  */
-
 class Solution {
 public:
     int minKBitFlips(vector<int>& A, int K) {
@@ -24,3 +23,29 @@ public:
         return ans;
     }
 };
+
+/**
+ * 110 / 110  test cases passed.
+ * Runtime: 116 ms
+ * Memory Usage: 71.8 MB 
+ */
+
+class Solution2 {
+public:
+    int minKBitFlips(vector<int>& A, int K) {
+        queue<int> que;
+        int ans = 0;
+        for (int i = 0; i < A.size(); ++ i) {
+            if (!que.empty() && que.front() + K == i) {
+                que.pop();
+            }
+            if ((que.size() + A[i]) % 2 == 0) {   
+                if (i + K > A.size()) return -1;
+                ans ++;
+                que.push(i);
+            }
+        }
+        return ans;
+    }
+};
+
