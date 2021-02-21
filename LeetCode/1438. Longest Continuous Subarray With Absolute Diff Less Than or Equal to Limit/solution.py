30d5717b8f279b5e5a77816be6fb3c1a7de96d37
InnoFang <innofang@outlook.com>
2021-02-21 11:47:05 +0800

    :art: update T1438 python solution

--- LeetCode/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/solution.py
+++ LeetCode/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/solution.py
@@ -15,3 +15,31 @@ class Solution:
                 l += 1
             ans = max(ans, r - l + 1)
         return ans
+
+"""
+56 / 56 test cases passed.
+Runtime: 292 ms
+Memory Usage: 21.5 MB
+"""
+import collections
+class Solution2:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        l = r = ans = 0
+        min_q = collections.deque()
+        max_q = collections.deque()
+        for num in nums:
+            while min_q and num < min_q[-1]:
+                min_q.pop()
+            while max_q and num > max_q[-1]:
+                max_q.pop()
+            min_q.append(num)
+            max_q.append(num)
+            r += 1
+            while max_q[0] - min_q[0] > limit:
+                if max_q[0] == nums[l]:
+                    max_q.popleft()
+                if min_q[0] == nums[l]:
+                    min_q.popleft()
+                l += 1
+            ans = max(ans, r - l)
+        return ans
