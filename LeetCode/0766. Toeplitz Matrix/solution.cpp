7e67715bbdc9361a0440b25ad23d10764185970a
InnoFang <innofang@outlook.com>
2021-02-22 11:14:11 +0800

    :art: update T766 cpp solution

--- LeetCode/0766. Toeplitz Matrix/solution.cpp
+++ LeetCode/0766. Toeplitz Matrix/solution.cpp
@@ -26,3 +26,22 @@ public:
         return true;
     }
 };
+
+/**
+ * 483 / 483 test cases passed.
+ * Runtime: 16 ms
+ * Memory Usage: 17.1 MB 
+ */
+class Solution2 {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        for (int i = 1; i < matrix.size(); ++ i) {
+            for (int j = 1; j < matrix[0].size(); ++ j) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
