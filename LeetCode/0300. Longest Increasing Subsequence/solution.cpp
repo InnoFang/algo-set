68543ef65d284adc5d0e1bd944bef300ab4c4a3f
InnoFang <innofang@outlook.com>
2021-03-04 21:56:31 +0800

    :art: update T300 cpp solution

--- LeetCode/0300. Longest Increasing Subsequence/solution.cpp
+++ LeetCode/0300. Longest Increasing Subsequence/solution.cpp
@@ -16,3 +16,24 @@ public:
         return *max_element(dp.begin(), dp.end());
     }
 };
+
+/**
+ * 54 / 54 test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 10.2 MB 
+ */
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> record { nums[0] };
+        for (int i = 1; i < nums.size(); ++ i) {
+            if (nums[i] > record.back()) {
+                record.push_back(nums[i]);
+            } else {
+                auto it = lower_bound(record.begin(), record.end(), nums[i]);
+                *it = nums[i];
+            }
+        }
+        return record.size();
+    }
+};
