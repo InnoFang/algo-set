6b9d30aaa40001aafb13d5facda80b011fadff11
InnoFang <innofang@outlook.com>
2021-03-07 16:37:59 +0800

    :art: update T131 python solution

--- LeetCode/0131. Palindrome Partitioning/solution.py
+++ LeetCode/0131. Palindrome Partitioning/solution.py
@@ -25,4 +25,31 @@ class Solution:
                 self.path.append(s[i:j+1])
                 self.dfs(s, j + 1)
                 self.path.pop()
-            
+
+"""
+32 / 32 test cases passed.
+Runtime: 128 ms
+Memory Usage: 30.9 MB
+"""
+class Solution2:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        dp = [[True] * n for _ in range(n)]
+        ans = []
+        path = []
+
+        def dfs(i: int):
+            if i == n:
+                ans.append(path[:])
+                return
+            for j in range(i, n):
+                if dp[i][j]:
+                    path.append(s[i:j+1])
+                    dfs(j + 1)
+                    path.pop()
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                dp[i][j] = (s[i] == s[j]) and dp[i + 1][j - 1] 
+        dfs(0)
+        return ans
