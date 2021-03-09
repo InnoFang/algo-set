2164c5808b36e9123ad3c8e35a8268849bf63b2f
InnoFang <innofang@outlook.com>
2021-03-09 13:13:28 +0800

    :art: update T1047 cpp solution

--- LeetCode/1047. Remove All Adjacent Duplicates In String/solution.cpp
+++ LeetCode/1047. Remove All Adjacent Duplicates In String/solution.cpp
@@ -17,3 +17,23 @@ public:
         return string(stk.begin(), stk.end());
     }
 };
+
+/**
+ * 98 / 98 test cases passed.
+ * Runtime: 12 ms
+ * Memory Usage: 10 MB 
+ */
+class Solution2 {
+public:
+    string removeDuplicates(string S) {
+        string stk;
+        for (auto& c: S) {
+            if (!stk.empty() && stk.back() == c) {
+                stk.pop_back();
+            } else {
+                stk.push_back(c);
+            }
+        }
+        return stk;
+    }
+};
