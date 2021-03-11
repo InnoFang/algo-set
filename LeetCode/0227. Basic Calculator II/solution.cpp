a663a6844e357a4b0764be3a6ada21a0cbd2fe9e
InnoFang <innofang@outlook.com>
2021-03-11 09:52:02 +0800

    :art: update T227 cpp solution

--- LeetCode/0227. Basic Calculator II/solution.cpp
+++ LeetCode/0227. Basic Calculator II/solution.cpp
@@ -25,3 +25,31 @@ public:
         return accumulate(nums.begin(), nums.end(), 0);
     }
 };
+
+/**
+ * 109 / 109 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 9.8 MB 
+ */
+class Solution {
+public:
+    int calculate(string s) {
+        vector<long> nums;
+        long num = 0;
+        char sign = '+';
+        for (int i = 0; i < s.size(); ++ i) {
+            char c = s[i];
+            if (isdigit(c)) num = num * 10 + c - '0';
+            if (!isdigit(c) && c != ' ' || i == (s.size() - 1)) {
+                if (sign == '+') nums.push_back(num);
+                else if (sign == '-') nums.push_back(-num);
+                else if (sign == '*') nums.back() *= num;
+                else if (sign == '/') nums.back() /= num;
+                num = 0;
+                sign = c;
+            }
+        }
+        return accumulate(nums.begin(), nums.end(), 0);
+    }
+};
+
