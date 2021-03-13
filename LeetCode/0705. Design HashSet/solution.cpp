66b03fd1092990fe498dc43781c81732b7fef95f
InnoFang <innofang@outlook.com>
2021-03-13 10:08:05 +0800

    :art: update T705 cpp solution

--- LeetCode/0705. Design HashSet/solution.cpp
+++ LeetCode/0705. Design HashSet/solution.cpp
@@ -41,6 +41,34 @@ private:
 };
 
 /**
+ * 32 / 32 test cases passed.
+ * Runtime: 112 ms
+ * Memory Usage: 175.7 MB 
+ */
+class MyHashSet2 {
+public:
+    /** Initialize your data structure here. */
+    MyHashSet() {
+        data.resize(1000005);
+    }
+    
+    void add(int key) {
+        data[key] = 1;
+    }
+    
+    void remove(int key) {
+        data[key] = 0;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        return data[key] == 1;
+    }
+private:
+    vector<int> data;
+};
+
+/**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet* obj = new MyHashSet();
  * obj->add(key);
