0cf5abac4118fd26bb8290538ff71baa72507470
InnoFang <innofang@outlook.com>
2021-03-22 10:49:27 +0800

    :art: update T191 cpp solution

--- LeetCode/0191. Number of 1 Bits/solution.cpp
+++ LeetCode/0191. Number of 1 Bits/solution.cpp
@@ -10,4 +10,20 @@ public:
     }
 };
 
+/**
+ * 601 / 601 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 6 MB 
+ */
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while (n > 0) {
+            count += (n & 1);
+            n >>= 1;
+        }
+        return count;
+    }
+};
 
