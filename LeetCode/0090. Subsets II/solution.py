47a97916824aa8074d3099e54c9934664b68d9f0
InnoFang <innofang@outlook.com>
2021-03-31 11:59:37 +0800

    :art: update T90 python solution

--- LeetCode/0090. Subsets II/solution.py
+++ LeetCode/0090. Subsets II/solution.py
@@ -18,3 +18,25 @@ class Solution:
             path.append(nums[i])
             self.dfs(i + 1, nums, ans, path)
             path.pop()
+
+"""
+19 / 19 test cases passed.
+Runtime: 36 ms
+Memory Usage: 14.9 MB
+"""
+class Solution2:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans, path = [], []
+        nums.sort()
+        def trackback(start, path):
+            ans.append(path[:])
+            for i in range(start, len(nums)):
+                if i != start and nums[i] == nums[i - 1]:
+                    continue
+                path.append(nums[i])
+                trackback(i + 1, path)
+                path.pop()
+        trackback(0, path)
+        return ans
+
+    
