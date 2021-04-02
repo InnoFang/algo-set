209d4f31d6cdf7c94ff777176c0c78c8a172b236
InnoFang <innofang@outlook.com>
2021-04-02 22:16:52 +0800

    :art: update T42 python solution

--- LeetCode/0042. Trapping Rain Water/solution.py
+++ LeetCode/0042. Trapping Rain Water/solution.py
@@ -20,3 +20,27 @@ class Solution:
             hi += 1
             area += r - l + 1
         return area - black
+
+"""
+320 / 320 test cases passed.
+Runtime: 48 ms
+Memory Usage: 15.9 MB
+"""
+class Solution2:
+    def trap(self, height: List[int]) -> int:
+        size = len(height)
+        if size <= 2:
+            return 0
+
+        left_height = [height[0]] * size
+        for i in range(1, size):
+            left_height[i] = max(left_height[i - 1], height[i])
+        
+        right_height = [height[-1]] * size
+        for i in range(size - 2, -1, - 1):
+            right_height[i] = max(right_height[i + 1], height[i])
+
+        ans = 0 
+        for i in range(1, size):
+            ans += min(left_height[i], right_height[i]) - height[i]
+        return ans
