75d642e9016c122788826ea044c1c9820be978b8
InnoFang <innofang@outlook.com>
2021-04-06 10:46:51 +0800

    :art: update T26 cpp solution

--- LeetCode/0026. Remove Duplicates from Sorted Array/solution.cpp
+++ LeetCode/0026. Remove Duplicates from Sorted Array/solution.cpp
@@ -16,3 +16,21 @@ public:
         return slow + 1;
     }
 };
+
+/**
+ * 161 / 161 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 13.3 MB 
+ */
+class Solution2 {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (auto& num: nums) {
+            if (i < 1 || nums[i - 1] != num) {
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
+};
