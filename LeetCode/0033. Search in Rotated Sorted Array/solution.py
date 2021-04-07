afbc0c79268821e7182e5a62b542a2deafd0e799
InnoFang <innofang@outlook.com>
2021-04-07 16:00:10 +0800

    :sparkles: upload T33 python solution

---
+++ LeetCode/0033. Search in Rotated Sorted Array/solution.py
@@ -0,0 +1,23 @@
+"""
+56 / 56 test cases passed.
+Runtime: 100 ms
+Memory Usage: 15.1 MB
+"""
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + ((r - l) >> 1)
+            if target == nums[mid]:
+                return mid;
+            if nums[mid] >= nums[l]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
