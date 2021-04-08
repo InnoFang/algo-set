3b7bedc34d3cc5f8c98bff407d162517f5f7ab58
InnoFang <innofang@outlook.com>
2021-04-08 10:43:00 +0800

    :art: update T153 cpp solution

--- LeetCode/0153. Find Minimum in Rotated Sorted Array/solution.cpp
+++ LeetCode/0153. Find Minimum in Rotated Sorted Array/solution.cpp
@@ -19,3 +19,24 @@ public:
         return -1;
     }
 };
+
+/**
+ * 150 / 150 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 9.9 MB 
+ */
+class Solution2 {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + right >> 1;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+};
