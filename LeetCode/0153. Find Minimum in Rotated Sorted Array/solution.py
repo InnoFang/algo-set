8c64fa04427a2a4d97b3a83d8af5cc21ffc8ae81
InnoFang <innofang@outlook.com>
2021-04-08 11:12:42 +0800

    :art: update T153 python solution

--- LeetCode/0153. Find Minimum in Rotated Sorted Array/solution.py
+++ LeetCode/0153. Find Minimum in Rotated Sorted Array/solution.py
@@ -31,3 +31,19 @@ class Solution2:
             else:
                 left = mid + 1
         return nums[left]
+
+"""
+150 / 150 test cases passed.
+Runtime: 32 ms
+Memory Usage: 14.9 MB
+"""
+class Solution3:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + right >> 1
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
