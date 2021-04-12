b768814f64c4dd90518f80d8373cbf1aec5a929c
InnoFang <innofang@outlook.com>
2021-04-12 11:37:26 +0800

    :art: update T179 cpp solution

--- LeetCode/0179. Largest Number/solution.cpp
+++ LeetCode/0179. Largest Number/solution.cpp
@@ -20,3 +20,29 @@ public:
         return ans;
     }
 };
+
+/**
+ * 229 / 229 test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 11.1 MB 
+ */
+class Solution2 {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> strs;
+        for (auto& num: nums) {
+            strs.push_back(to_string(num));
+        }
+        sort(strs.begin(), strs.end(), [](const string& a, const string& b) {
+            return a + b > b + a;
+        });
+        
+        if (strs[0] == "0") return "0";
+
+        string ans = "";
+        for (auto& str: strs) {
+            ans += str;
+        }
+        return ans;
+    }
+};
