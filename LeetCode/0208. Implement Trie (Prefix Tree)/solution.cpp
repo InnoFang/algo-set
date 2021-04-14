4621297c64702be5b594fb70ea2dcaf05086007d
InnoFang <innofang@outlook.com>
2021-04-14 16:08:25 +0800

    :art: update T208 cpp solution

--- LeetCode/0208. Implement Trie (Prefix Tree)/solution.cpp
+++ LeetCode/0208. Implement Trie (Prefix Tree)/solution.cpp
@@ -51,6 +51,57 @@ private:
     bool isEnd;
 };
 
+
+/**
+ * 15 / 15 test cases passed.
+ * Runtime: 100 ms
+ * Memory Usage: 42 MB 
+ */
+class Trie2 {
+public:
+    /** Initialize your data structure here. */
+    Trie(): next(), isEnd(false) {}
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Trie* node = this;
+        for (auto& ch: word) {
+            if (!node->next.count(ch)) {
+                node->next[ch] = new Trie();
+            }
+            node = node->next[ch];
+        }
+        node->isEnd = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Trie* node = this;
+        for (auto& ch: word) {
+            if (!node->next.count(ch)) {
+                return false;
+            }
+            node = node->next[ch];
+        }
+        return node->isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Trie* node = this;
+        for (auto& ch: prefix) {
+            if (!node->next.count(ch)) {
+                return false;
+            }
+            node = node->next[ch];
+        }
+        return true;
+    }
+private:
+    unordered_map<char, Trie*> next;
+    bool isEnd;
+};
+
 /**
  * Your Trie object will be instantiated and called as such:
  * Trie* obj = new Trie();
