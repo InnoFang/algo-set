8959db6394b33749e46adbb80515fe87fa434af3
InnoFang <innofang@outlook.com>
2021-04-14 16:32:15 +0800

    :art: update T208 python solution

--- LeetCode/0208. Implement Trie (Prefix Tree)/solution.py
+++ LeetCode/0208. Implement Trie (Prefix Tree)/solution.py
@@ -50,6 +50,54 @@ class Trie:
         return True
 
 
+"""
+15 / 15 test cases passed.
+Runtime: 180 ms
+Memory Usage: 32.5 MB
+"""
+class Trie2:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.lookup = { '#': False }
+
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self
+        for ch in word:
+            if ch not in node.lookup:
+                node.lookup[ch] = Trie()
+            node = node.lookup[ch]
+        node.lookup['#'] = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self
+        for ch in word:
+            if ch not in node.lookup:
+                return False
+            node = node.lookup[ch]
+        return node.lookup['#']
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self
+        for ch in prefix:
+            if ch not in node.lookup:
+                return False
+            node = node.lookup[ch]
+        return True
+
+
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
