3bb98cbe1e3cb698b2dbebe95007973867e15451
InnoFang <innofang@outlook.com>
2021-04-15 20:27:17 +0800

    :art: update T198 cpp solution

--- LeetCode/0198. House Robber/solution.cpp
+++ LeetCode/0198. House Robber/solution.cpp
@@ -1,8 +1,4 @@
-/**
- * 68 / 68 test cases passed.
- * Runtime: 0 ms
- * Memory Usage: 7.5 MB 
- */
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -15,5 +11,28 @@ public:
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1]);
         }
         return dp.back();
+    }
+};
+
+/**
+ * 68 / 68 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 7.3 MB 
+ */
+class Solution2 {
+public:
+    int rob(vector<int>& nums) {
+        int len = nums.size();
+        if (len == 0) return 0;
+        if (len == 1) return nums[0];
+        if (len == 2) return max(nums[0], nums[1]);
+
+        int last = 0, curr = 0;
+        for (auto& num: nums) {
+            int tmp = curr;
+            curr = max(curr, last + num);
+            last = tmp;
+        }
+        return curr;
     }
 };
