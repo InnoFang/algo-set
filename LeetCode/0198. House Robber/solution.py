601a7f22d5fd22783c06730e62593e8b72d2ed9c
InnoFang <innofang@outlook.com>
2021-04-15 19:23:32 +0800

    :art: update T198 python solution

--- LeetCode/0198. House Robber/solution.py
+++ LeetCode/0198. House Robber/solution.py
@@ -1,7 +1,7 @@
 """
-56 / 56 test cases passed.
-Runtime: 100 ms
-Memory Usage: 15.1 MB
+68 / 68 test cases passed.
+Runtime: 40 ms
+Memory Usage: 14.7 MB
 """
 class Solution:
     def rob(self, nums: List[int]) -> int:
@@ -13,3 +13,16 @@ class Solution:
         for i in range(2, len(nums) + 1):
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
         return dp[-1]
+
+
+"""
+68 / 68 test cases passed.
+Runtime: 36 ms
+Memory Usage: 14.7 MB
+"""
+class Solution2:
+    def rob(self, nums: List[int]) -> int:
+        last, curr = 0, 0
+        for num in nums:
+            last, curr = curr, max(last + num, curr)
+        return curr
