8b9bc56c0f829a3db3232e04b043b94ad6843b1f
InnoFang <innofang@outlook.com>
2021-04-19 12:36:17 +0800

    :art: update T27 cpp solution

--- LeetCode/0027. Remove Element/Solution.cpp
+++ LeetCode/0027. Remove Element/Solution.cpp
@@ -15,3 +15,21 @@ public:
         return len;
     }
 };
+
+/**
+ * 113 / 113 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 8.5 MB 
+ */
+class Solution2 {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size() - 1;
+        for (int i = 0; i <= n; ++ i) {
+            if (nums[i] == val) {
+                swap(nums[i--], nums[n--]);
+            }
+        }
+        return n + 1;
+    }
+};
