5096b290bbfe0de9ff904405dcbb698625962e16
InnoFang <innofang@outlook.com>
2021-04-24 11:23:21 +0800

    :art: update T377 cpp solution

--- LeetCode/0377. Combination Sum IV/solution.cpp
+++ LeetCode/0377. Combination Sum IV/solution.cpp
@@ -23,3 +23,24 @@ public:
         return dfs(nums, target);
     }
 };
+
+/**
+ * 15 / 15 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 6.4 MB 
+ */
+class Solution2 {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= target; ++ i) {
+            for (auto& num: nums) {
+                if (i >= num) {
+                    dp[i] = (0LL + dp[i] + dp[i - num]) % INT_MAX;
+                }
+            }
+        }
+        return dp[target];
+    }
+};
