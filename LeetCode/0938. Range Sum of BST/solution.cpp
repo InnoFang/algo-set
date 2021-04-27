1699ef66925d33d2d1a6f27e3a7360316a2e4445
InnoFang <innofang@outlook.com>
2021-04-27 21:46:41 +0800

    :art: update T938 cpp solution

--- LeetCode/0938. Range Sum of BST/solution.cpp
+++ LeetCode/0938. Range Sum of BST/solution.cpp
@@ -29,3 +29,22 @@ public:
         return ans;
     }
 };
+
+/**
+ * 41 / 41 test cases passed.
+ * Runtime: 204 ms
+ * Memory Usage: 63.2 MB 
+ */
+class Solution2 {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (root == nullptr) return 0;
+        if (root->val > high) {
+            return rangeSumBST(root->left, low, high);
+        }
+        if (root->val < low ) {
+            return rangeSumBST(root->right, low, high);
+        }
+        return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    }
+};
