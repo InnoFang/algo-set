c9be6e1a709ca32b9dc9cea59862ee1eb8ad72f1
InnoFang <innofang@outlook.com>
2021-04-28 16:04:30 +0800

    :art: update T633 cpp solution

--- LeetCode/0633. Sum of Square Numbers/solution.cpp
+++ LeetCode/0633. Sum of Square Numbers/solution.cpp
@@ -13,3 +13,23 @@ public:
         return false;
     }
 };
+
+/**
+ * 124 / 124  test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 5.8 MB 
+ */
+class Solution2 {
+public:
+    bool judgeSquareSum(int c) {
+        long l = 0;
+        long r = sqrt(c);
+        while (l <= r) {
+            long sum = l * l + r * r;
+            if (sum == c) return true;
+            if (sum > c) r --;
+            else l ++ ;
+        }
+        return false;
+    }
+};
