7423e3ead09e624540e0efdcdb691b010559494d
InnoFang <innofang@outlook.com>
2021-05-01 12:54:37 +0800

    :sparkles: upload T690 cpp solution

--- LeetCode/0690. Employee Importance/solution.cpp
+++ LeetCode/0690. Employee Importance/solution.cpp
@@ -10,12 +10,11 @@ public:
 
 /**
  * 108 / 108 test cases passed
- * Status: Accepted
- * Runtime: 8ms
+ * Runtime: 596 ms
+ * Memory Usage: 98.7 MB 
  */
 class Solution {
 public:
-
     int dfs( unordered_map<int, Employee*> store, int id ) {
         int ipt = store[id]->importance;
         for ( int& employeeId : store[id]->subordinates ) {
@@ -33,3 +32,30 @@ public:
     }
 };
 
+/**
+ * 108 / 108 test cases passed
+ * Runtime: 36 ms
+ * Memory Usage: 13.7 MB 
+ */
+class Solution2 {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int, Employee*> store;
+        for (auto& employee: employees) {
+            store[employee->id] = employee;
+        }
+
+        int ans = 0;
+        queue<Employee*> que;
+        que.push(store[id]);
+        while (!que.empty()) {
+            vector<int> subordinates = que.front()->subordinates;
+            ans += que.front()->importance;
+            que.pop();
+            for (auto& subId: subordinates) {
+                que.push(store[subId]);
+            }
+        }
+        return ans;
+    }
+};
