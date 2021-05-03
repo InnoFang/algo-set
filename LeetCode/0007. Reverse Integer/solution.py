a128cbfdf1c76e1bb63208a656fdf87fdd7474f8
InnoFang <innofang@outlook.com>
2021-05-03 15:51:47 +0800

    :art: update T7 python solution

--- LeetCode/0007. Reverse Integer/solution.py
+++ LeetCode/0007. Reverse Integer/solution.py
@@ -17,4 +17,15 @@ class Solution:
             if ans < INT_MIN or ans > INT_MAX:
                 return 0
         return ans
-        
+
+"""
+1032 / 1032 test cases passed.
+Runtime: 36 ms
+Memory Usage: 14.8 MB
+"""
+class Solution2:
+    def reverse(self, x: int) -> int:
+        ans = str(x) if x >= 0 else str(x)[1:]
+        ans = ans[::-1]
+        ans = int(ans) if x >= 0 else -1 * int(ans)
+        return ans if - (1 << 31) <= ans <= (1 << 31) - 1 else 0
