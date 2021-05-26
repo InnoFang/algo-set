745a7d23921f56b53409f9e9054ceb1d740a0877
InnoFang <innofang@outlook.com>
2021-05-26 10:33:45 +0800

    :art: update T1190 python solution

--- LeetCode/1190. Reverse Substrings Between Each Pair of Parentheses/solution.py
+++ LeetCode/1190. Reverse Substrings Between Each Pair of Parentheses/solution.py
@@ -17,3 +17,21 @@ class Solution:
             else:
                 sub += c
         return sub
+
+"""
+38 / 38 test cases passed.
+Runtime: 44 ms
+Memory Usage: 14.8 MB
+"""
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stk = ['']
+        for c in s:
+            if c == '(':
+                stk.append('')
+            elif c == ')':
+                reversed_top = stk.pop()[::-1]
+                stk[-1] += reversed_top
+            else:
+                stk[-1] += c
+        return stk[0]
