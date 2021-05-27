7f47ea4b1ae2889bb7d88bc0f6fe488738c5045e
InnoFang <innofang@outlook.com>
2021-05-27 10:47:21 +0800

    :art: update T461 python solution

--- LeetCode/0461. Hamming Distance/solution.py
+++ LeetCode/0461. Hamming Distance/solution.py
@@ -6,3 +6,16 @@ Memory Usage: 14.7 MB
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int: 
         return str(bin(x ^ y)).count('1')
+
+"""
+56 / 56 test cases passed.
+Runtime: 44 ms
+Memory Usage: 14.7 MB
+"""
+class Solution2:
+    def hammingDistance(self, x: int, y: int) -> int: 
+        ans, n = 0, x ^ y
+        while n:
+            ans += n & 1
+            n >>= 1
+        return ans
