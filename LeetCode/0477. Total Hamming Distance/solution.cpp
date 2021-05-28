fc1050d5795feb17b186b2946cd87632dc907a81
InnoFang <innofang@outlook.com>
2021-05-28 15:40:03 +0800

    :art: update T477 cpp solution

--- LeetCode/0477. Total Hamming Distance/solution.cpp
+++ LeetCode/0477. Total Hamming Distance/solution.cpp
@@ -1,14 +1,15 @@
 /**
  * 46 / 46 test cases passed.
- * Runtime: 48 ms
- * Memory Usage: 18.5 MB 
+ * Runtime: 36 ms
+ * Memory Usage: 18.6 MB 
  */
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
         int ans = 0;
         int n = nums.size();
-        for (int i = 0; i <= 31; ++ i) {
+        // the length of the binary number of 10 ^ 9 is 30
+        for (int i = 0; i < 30; ++ i) {
             int c = 0;
             for (auto num: nums) {
                 c += (num >> i) & 1;
