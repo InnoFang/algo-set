956e3d323234c830e2c68b115465c8bc483652c9
InnoFang <innofang@outlook.com>
2021-05-30 11:11:45 +0800

    :art: update T231 python solution

--- LeetCode/0231. Power of Two/solution.py
+++ LeetCode/0231. Power of Two/solution.py
@@ -7,3 +7,13 @@ class Solution:
     larger = 2 << 30
     def isPowerOfTwo(self, n: int) -> bool:
         return n > 0 and Solution.larger % n == 0
+
+
+"""
+1108 / 1108 test cases passed.
+Runtime: 44 ms
+Memory Usage: 14.6 MB
+"""
+class Solution2: 
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and n & n - 1 == 0
