45cc8ac805670305e0cc19a429de5fee27b116be
InnoFang <innofang@outlook.com>
2021-06-15 17:11:00 +0800

    :art: update T852 cpp solution

--- LeetCode/0852. Peak Index in a Mountain Array/solution.cpp
+++ LeetCode/0852. Peak Index in a Mountain Array/solution.cpp
@@ -18,3 +18,25 @@ public:
         return l;
     }
 };
+
+/**
+ * 34 / 34 test cases passed.
+ * Runtime: 12 ms
+ * Memory Usage: 11.2 MB 
+ */
+class Solution2 {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l = 0, r = arr.size() - 1;
+        while (l < r) {
+            int m1 = l + (r - l) / 3;
+            int m2 = r - (r - l) / 3;
+            if (arr[m1] < arr[m2]) {
+                l = m1 + 1;
+            } else {
+                r = m2 - 1;
+            }
+        }
+        return l;
+    }
+};
