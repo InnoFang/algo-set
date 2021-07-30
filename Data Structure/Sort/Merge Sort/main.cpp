01d9446e60f3b7a8e96b1382f44272421f256eee
InnoFang <innofang@outlook.com>
2021-07-30 12:08:48 +0800

    :art: optimize the code of the MergeSort with C++

--- Data Structure/Sort/Merge Sort/main.cpp
+++ Data Structure/Sort/Merge Sort/main.cpp
@@ -1,77 +1,88 @@
-#include <iostream>
 #include <ctime>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
-void merge(int arr[], int left, int mid, int right) {
-    int * aux = new int[right - left + 1];
+void merge(std::vector<int>& arr, size_t left, size_t mid, size_t right) {
+    std::vector<int> aux(right - left + 1);
 
-    for (int i = left; i <= right; ++i) 
-        aux[i - left] = arr[i];
+    // copy arr[left:right] to aux[]
+    // for (size_t i = left; i <= right; ++ i) 
+    //     aux[i - left] = arr[i];
+    std::copy_n(arr.begin() + left, right - left + 1, aux.begin());
 
-    int l = left, r = mid + 1;
-    for (int i = left; i <= right; ++i) {
+    size_t l = left, r = mid + 1;
+    for (size_t i = left; i <= right; ++ i) {
         if (l > mid) {
             arr[i] = aux[r - left]; 
-            ++r;
+            ++ r;
         } else if (r > right) {
             arr[i] = aux[l - left];
-            ++l;
+            ++ l;
         } else if (aux[l - left] < aux[r - left]) {
             arr[i] = aux[l - left];
-            ++l;
+            ++ l;
         } else {
             arr[i] = aux[r - left];
-            ++r;
+            ++ r;
         }
     }
 }
 
-void mergeSort(int arr[], int left, int right) {
+void _mergeSort(std::vector<int>& arr, size_t left, size_t right) {
     if (left >= right) return;
 
-    int mid = left + (right - left) / 2;
-    mergeSort(arr, left, mid);
-    mergeSort(arr, mid + 1, right);
+    size_t mid = left + (right - left) / 2;
+    _mergeSort(arr, left, mid);
+    _mergeSort(arr, mid + 1, right);
 
     // optimization, merge arr when arr is out of order
-    if (arr[mid] > arr[mid + 1])
+    if (arr[mid] > arr[mid + 1]) {
         merge(arr, left, mid, right);
+    }
 }
 
-int *generateRandomArray(int length, int rangeFrom, int rangeTo);
-void printArray(int * arr, int length);
+void mergeSort(std::vector<int>& arr) { 
+    _mergeSort(arr, 0, arr.size() - 1);
+}
+
+std::vector<int> generateRandomArray(size_t length, size_t rangeFrom, size_t rangeTo);
+void printArray(const std::vector<int>& arr);
 
 int main() {
+    size_t dataSize = 20;
+    size_t rangeFrom = 1;
+    size_t rangeTo = 10000;
 
-    int length = 20;
-    int rangeFrom = 1;
-    int rangeTo = 10000;
-
-    int * arr = generateRandomArray(length, rangeFrom, rangeTo); 
+    auto arr = generateRandomArray(dataSize, rangeFrom, rangeTo); 
  	
     std::cout << "Original array:" << std::endl;
-    printArray(arr, length);
- 
-    mergeSort(arr, 0, length - 1);
+    printArray(arr);
+
+    mergeSort(arr);
 
     std::cout << "Sorted array:" << std::endl;
-    printArray(arr, length);
+    printArray(arr);
 
     return 0;
 }
 
-/* Randomly generate an array with the `length`, which is from `rangeFrom` to `rangeTo` */
-int * generateRandomArray(int length, int rangeFrom, int rangeTo) {
-    int * arr = new int[length];
-    int range = rangeTo - rangeFrom + 1;
-    srand(time(NULL));
-    for (int i = 0; i < length; ++i) {
+/* Randomly generate an array with the `dataSize`, which is from `rangeFrom` to `rangeTo` */
+std::vector<int> generateRandomArray(size_t dataSize, size_t rangeFrom, size_t rangeTo) {
+    size_t range = rangeTo - rangeFrom + 1;
+    srand(time(nullptr));
+
+    std::vector<int> arr(dataSize);
+    for (size_t i = 0; i < dataSize; ++ i) {
         arr[i] = rand() % range + rangeFrom;
     }
     return arr;
 }
 
 /* formatted output array */
-void printArray(int * arr, int length) {
-    for (int i = 0 ; i < length; ++i) std::cout << arr[i] << " ";
+void printArray(const std::vector<int>& arr) {
+    for (const auto& num: arr) {
+        std::cout << num << " ";
+    }
     std::cout << std::endl;
 }
