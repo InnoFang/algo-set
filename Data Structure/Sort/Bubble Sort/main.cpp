f62fb4fea0543c4267b603ceb72d59582e0f418e
InnoFang <innofang@outlook.com>
2021-07-30 11:08:17 +0800

    :art: optimize the code of the BubbleSort with C++

--- Data Structure/Sort/Bubble Sort/main.cpp
+++ Data Structure/Sort/Bubble Sort/main.cpp
@@ -1,48 +1,54 @@
-#include <iostream>
 #include <ctime>
+#include <vector>
+#include <iostream>
 
-void bubbleSort(int arr[], int len) {
-    for (int i = 0; i < len - 1; ++i) 
-        for (int j = 0; j < len - 1 - i; ++j) 
-            if (arr[j] > arr[j + 1])  
+void bubbleSort(std::vector<int>& arr) { 
+    for (size_t i = 0; i < arr.size() - 1; ++i) {
+        for (size_t j = 0; j < arr.size() - 1 - i; ++j) {
+            if (arr[j] > arr[j + 1]) {
                 std::swap(arr[j], arr[j+1]);
+            }
+        }
+    }
 }
 
-int *generateRandomArray(int length, int rangeFrom, int rangeTo);
-void printArray(int * arr, int length);
+std::vector<int> generateRandomArray(size_t length, size_t rangeFrom, size_t rangeTo);
+void printArray(const std::vector<int>& arr);
 
 int main() {
+    size_t dataSize = 20;
+    size_t rangeFrom = 1;
+    size_t rangeTo = 10000;
 
-    int length = 20;
-    int rangeFrom = 1;
-    int rangeTo = 10000;
-
-    int * arr = generateRandomArray(length, rangeFrom, rangeTo); 
+    auto arr = generateRandomArray(dataSize, rangeFrom, rangeTo); 
  	
     std::cout << "Original array:" << std::endl;
-    printArray(arr, length);
+    printArray(arr);
 
-    bubbleSort(arr, length);
+    bubbleSort(arr);
 
     std::cout << "Sorted array:" << std::endl;
-    printArray(arr, length);
+    printArray(arr);
 
     return 0;
 }
 
-/* Randomly generate an array with the `length`, which is from `rangeFrom` to `rangeTo` */
-int * generateRandomArray(int length, int rangeFrom, int rangeTo) {
-    int * arr = new int[length];
-    int range = rangeTo - rangeFrom + 1;
-    srand(time(NULL));
-    for (int i = 0; i < length; ++i) {
+/* Randomly generate an array with the `dataSize`, which is from `rangeFrom` to `rangeTo` */
+std::vector<int> generateRandomArray(size_t dataSize, size_t rangeFrom, size_t rangeTo) {
+    size_t range = rangeTo - rangeFrom + 1;
+    srand(time(nullptr));
+
+    std::vector<int> arr(dataSize);
+    for (size_t i = 0; i < dataSize; ++ i) {
         arr[i] = rand() % range + rangeFrom;
     }
     return arr;
 }
 
 /* formatted output array */
-void printArray(int * arr, int length) {
-    for (int i = 0 ; i < length; ++i) std::cout << arr[i] << " ";
+void printArray(const std::vector<int>& arr) {
+    for (const auto& num: arr) {
+        std::cout << num << " ";
+    }
     std::cout << std::endl;
 }
