e504446e2cfc4d2bd20a404355e92e4000f4f064
InnoFang <innofang@outlook.com>
2021-07-30 14:32:02 +0800

    :art: optimize the code of the QuickSort with C++

--- Data Structure/Sort/Quick Sort/main.cpp
+++ Data Structure/Sort/Quick Sort/main.cpp
@@ -1,60 +1,68 @@
-#include <iostream>
 #include <ctime>
+#include <vector>
+#include <iostream>
 
-int partition(int arr[], int left, int right) {
+int partition(std::vector<int>& arr, int left, int right) {
     int l = left, r = right;
     int tmp = arr[l];
     while (l != r) {
-        while (l < r && arr[r] >= tmp) --r;
-        while (l < r && arr[l] <= tmp) ++l;
+        while (l < r && arr[r] >= tmp) -- r;
+        while (l < r && arr[l] <= tmp) ++ l;
         if (l < r) std::swap(arr[l], arr[r]);
-    } 
+    }
     std::swap(arr[l], arr[left]);
     return l;
 }
 
-void quickSort(int arr[], int left, int right) {
+void _quickSort(std::vector<int>& arr, int left, int right) {
     if (left > right) return;
     int pivot = partition(arr, left, right);
-    quickSort(arr, left, pivot - 1);
-    quickSort(arr, pivot + 1, right);
+    _quickSort(arr, left, pivot - 1);
+    _quickSort(arr, pivot + 1, right);
 }
 
-int *generateRandomArray(int length, int rangeFrom, int rangeTo);
-void printArray(int * arr, int length);
+void quickSort(std::vector<int>& arr) {
+    _quickSort(arr, 0, arr.size() - 1);
+}
+
+
+std::vector<int> generateRandomArray(size_t length, size_t rangeFrom, size_t rangeTo);
+void printArray(const std::vector<int>& arr);
 
 int main() {
+    size_t dataSize = 20;
+    size_t rangeFrom = 1;
+    size_t rangeTo = 10000;
 
-    int length = 20;
-    int rangeFrom = 1;
-    int rangeTo = 10000;
+    auto arr = generateRandomArray(dataSize, rangeFrom, rangeTo);
 
-    int * arr = generateRandomArray(length, rangeFrom, rangeTo); 
- 	
     std::cout << "Original array:" << std::endl;
-    printArray(arr, length);
- 
-    quickSort(arr, 0, length - 1);
+    printArray(arr);
+
+    quickSort(arr);
 
     std::cout << "Sorted array:" << std::endl;
-    printArray(arr, length);
+    printArray(arr);
 
     return 0;
 }
 
-/* Randomly generate an array with the `length`, which is from `rangeFrom` to `rangeTo` */
-int * generateRandomArray(int length, int rangeFrom, int rangeTo) {
-    int * arr = new int[length];
-    int range = rangeTo - rangeFrom + 1;
-    srand(time(NULL));
-    for (int i = 0; i < length; ++i) {
+/* Randomly generate an array with the `dataSize`, which is from `rangeFrom` to `rangeTo` */
+std::vector<int> generateRandomArray(size_t dataSize, size_t rangeFrom, size_t rangeTo) {
+    size_t range = rangeTo - rangeFrom + 1;
+    srand(time(nullptr));
+
+    std::vector<int> arr(dataSize);
+    for (size_t i = 0; i < dataSize; ++ i) {
         arr[i] = rand() % range + rangeFrom;
     }
     return arr;
 }
 
 /* formatted output array */
-void printArray(int * arr, int length) {
-    for (int i = 0 ; i < length; ++i) std::cout << arr[i] << " ";
+void printArray(const std::vector<int>& arr) {
+    for (const auto& num: arr) {
+        std::cout << num << " ";
+    }
     std::cout << std::endl;
 }
