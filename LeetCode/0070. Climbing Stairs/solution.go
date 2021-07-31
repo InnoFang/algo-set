fb31c14db22a1533497967a23cfb3b8f214c303b
InnoFang <innofang@outlook.com>
2021-07-31 20:29:46 +0800

    :art: update T70 go solution

---
+++ LeetCode/0070. Climbing Stairs/solution.go
@@ -0,0 +1,14 @@
+/**
+ * 45 / 45 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 1.9 MB
+ */
+func climbStairs(n int) int {
+	steps := make([]int, n+1)
+	steps[0] = 1
+	steps[1] = 1
+	for i := 2; i <= n; i++ {
+		steps[i] += steps[i-1] + steps[i-2]
+	}
+	return steps[n]
+}
