dc14ecc47cb53b6a67defc39a850bb00e1a47662
InnoFang <innofang@outlook.com>
2021-07-31 20:37:55 +0800

    :art: update T70 python solution

--- LeetCode/0070. Climbing Stairs/solution.py
+++ LeetCode/0070. Climbing Stairs/solution.py
@@ -10,4 +10,15 @@ class Solution:
         for i in range(2, n + 1):
             steps[i] = steps[i - 1] + steps[i - 2]
         return steps[n]
-    
+
+"""
+45 / 45 test cases passed.
+Runtime: 28 ms
+Memory Usage: 14.9 MB
+"""
+class Solution2:
+    def climbStairs(self, n: int) -> int:
+        a, b = 1, 1
+        for i in range(2, n + 1):
+            a, b = b, a + b
+        return b
