1de5830b48dadd0664492866159c5dff4b254a6c
InnoFang <innofang@outlook.com>
2021-08-01 22:06:51 +0800

    :art: update T198 go solution

--- LeetCode/0198. House Robber/solution.go
+++ LeetCode/0198. House Robber/solution.go
@@ -13,6 +13,20 @@ func rob(nums []int) int {
 	return dp[n]
 }
 
+/**
+ * 68 / 68 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 2 MB
+ */
+func rob2(nums []int) int {
+	n := len(nums)
+	a, b := 0, nums[0]
+	for i := 2; i <= n; i++ {
+		a, b = b, max(b, a+nums[i-1])
+	}
+	return b
+}
+
 func max(a, b int) int {
 	if a < b {
 		return b
