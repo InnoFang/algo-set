48292ee74bfa07a5c71d2344005ed69149e0d82e
InnoFang <innofang@outlook.com>
2021-08-19 09:36:53 +0800

    :sparkles: upload T345 cpp solution

---
+++ LeetCode/0345. Reverse Vowels of a String/solution.cpp
@@ -0,0 +1,21 @@
+/**
+ * 480 / 480 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 7.4 MB 
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> dic;
+        for (int i = 0; i < nums.size(); i++) {
+            int val = target - nums[i];
+            if (dic.find(val) != dic.end()) {
+                vector<int> res;
+                res.push_back(dic[val]);
+                res.push_back(i);
+                return res;
+            } else dic[nums[i]] = i;
+        }
+        return vector<int>();
+    }
+};
