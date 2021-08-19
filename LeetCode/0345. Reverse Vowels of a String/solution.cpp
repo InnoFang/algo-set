04f409bc801cf3cff285ecc3b758b587053f3548
InnoFang <innofang@outlook.com>
2021-08-19 09:38:03 +0800

    :art: update T345 cpp solution

--- LeetCode/0345. Reverse Vowels of a String/solution.cpp
+++ LeetCode/0345. Reverse Vowels of a String/solution.cpp
@@ -15,3 +15,21 @@ public:
         return s;
     }
 };
+
+/**
+ * 480 / 480 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 8 MB 
+ */
+class Solution2 {
+public:
+    unordered_set<char> vowels {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    string reverseVowels(string s) { 
+        for (int l = 0, r = s.size() - 1; l < r; ++ l, -- r) { 
+            while (l < r && !vowels.count(s[l])) ++ l;
+            while (l < r && !vowels.count(s[r])) -- r;
+            if (l < r) swap(s[l], s[r]);
+        }
+        return s;
+    }
+};
