f4a186d41166d29b4a545168554c10c1d2f56bea
InnoFang <innofang@outlook.com>
2021-08-25 22:11:08 +0800

    :art: udpate T797 cpp solution

--- LeetCode/0797. All Paths From Source to Target/solution.cpp
+++ LeetCode/0797. All Paths From Source to Target/solution.cpp
@@ -24,3 +24,32 @@ public:
         }
     }
 };
+
+/**
+ * 30 / 30 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 10.4 MB 
+ */
+class Solution2 {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> ans;
+        vector<int> curr { 0 };
+        curr.reserve(n);
+        dfs(graph, 0, n - 1, ans, curr);
+        return ans;
+    }
+
+    void dfs(vector<vector<int>>& graph, int idx, int last, vector<vector<int>>& path, vector<int>& curr) {
+        if (idx == last) {
+            path.emplace_back(curr);
+            return;
+        }
+        for (auto& next: graph[idx]) {
+            curr.emplace_back(next);
+            dfs(graph, next, last, path, curr);
+            curr.pop_back();
+        }
+    }
+};
