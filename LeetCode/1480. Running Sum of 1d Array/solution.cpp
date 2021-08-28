c449fa89dd2dbd7f57c54a488588bc63262e6dad
InnoFang <innofang@outlook.com>
2021-08-28 11:26:00 +0800

    :art: udpate T1480 cpp solution

--- LeetCode/1480. Running Sum of 1d Array/solution.cpp
+++ LeetCode/1480. Running Sum of 1d Array/solution.cpp
@@ -32,3 +32,19 @@ public:
     }
 };
 
+
+/**
+ * 53 / 53 test cases passed
+ * Status: Accepted
+ * Runtime: 0 ms
+ */
+class Solution3 {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); ++ i) {
+            ans[i] += ans[i - 1];
+        }
+        return ans;
+    }
+};
