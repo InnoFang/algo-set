3ea97076a70f25cffe1d120e5790e6a34f52d932
InnoFang <innofang@outlook.com>
2021-09-15 10:04:27 +0800

    :art: update T162 cpp solution

--- LeetCode/0162. Find Peak Element/solution.cpp
+++ LeetCode/0162. Find Peak Element/solution.cpp
@@ -10,3 +10,20 @@ public:
         return pos - nums.begin();
     }
 };
+
+/**
+ * 63 / 63 test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 8.6 MB 
+ */
+class Solution2 {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int lo = 0, hi = nums.size() - 1;
+        while (lo < hi) {
+            int mid = lo + ((hi - lo) >> 1);
+            (nums[mid] < nums[mid + 1]) ? lo = mid + 1 : hi = mid;
+        }
+        return lo;
+    }
+};
