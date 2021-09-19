c25009899fa5814358b22982c3926691792ba08a
InnoFang <innofang@outlook.com>
2021-09-19 10:13:22 +0800

    :sparkles: upload T292 javascript solution

---
+++ LeetCode/0292. Nim Game/solution.js
@@ -0,0 +1,11 @@
+/**
+ * 60 / 60 test cases passed.
+ * Runtime: 60 ms
+ * Memory Usage: 37.6 MB 
+ *
+ * @param {number} n
+ * @return {boolean}
+ */
+ var canWinNim = function(n) {
+    return n % 4 != 0;
+};
