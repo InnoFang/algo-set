f5166f844c2c568bf91760078ea6b47c65c46309
InnoFang <innofang@outlook.com>
2021-09-20 14:16:09 +0800

    :sparkles: upload T650 java solution

---
+++ LeetCode/0650. 2 Keys Keyboard/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * 126 / 126 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 35.3 MB 
+ */
+class Solution {
+    public int minSteps(int n) {
+        if (n == 1) return 0; 
+        int x;
+        for (x = n >> 1; n % x != 0; x--) {} 
+        return minSteps(x) + (n / x);
+    }
+}
