bc475b006fdb68c761cce2fedd5e5152b4800de8
InnoFang <innofang@outlook.com>
2021-09-21 09:23:00 +0800

    :art: upload T58 python solution

--- LeetCode/0058. Length of Last Word/solution.py
+++ LeetCode/0058. Length of Last Word/solution.py
@@ -6,4 +6,19 @@ Memory Usage: 14.9 MB
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         return len(s.strip().split(" ")[-1])
-    
+
+
+"""
+58 / 58 test cases passed.
+Runtime: 24 ms
+Memory Usage: 15.1 MB
+"""
+class Solution2:
+    def lengthOfLastWord(self, s: str) -> int:
+        i, ans = len(s) - 1, 0
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            ans += 1
+            i -= 1
+        return ans
