1fc6d6a536cb992fc753bdd1b7ca997cd3c4dad8
InnoFang <innofang@outlook.com>
2021-09-23 11:08:24 +0800

    :art: update T1206 cpp solution

--- LeetCode/1206. Design Skiplist/solution.cpp
+++ LeetCode/1206. Design Skiplist/solution.cpp
@@ -78,6 +78,80 @@ private:
 };
 
 /**
+ * 20 / 20 test cases passed.
+ * Runtime: 100 ms
+ * Memory Usage: 63.7 MB 
+ */
+class Skiplist2 {
+public:
+    Skiplist() {
+        head_ = make_shared<Node>();
+    }
+    
+    bool search(int target) {
+        for (auto node = head_; node; node = node->down) {
+            while (node->next && node->next->val < target) {
+                node = node->next;
+            }
+            if (node->next && node->next->val == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    void add(int num) {
+        stack<shared_ptr<Node>> stk;
+        for (auto node = head_; node; node = node->down) {
+            while (node->next && node->next->val < num) {
+                node = node->next;
+            }
+            stk.push(node);
+        }
+
+        shared_ptr<Node> down;
+        bool insert = true;
+        while (!stk.empty() && insert) {
+            auto curr = stk.top(); stk.pop();
+            curr->next = make_shared<Node>(num, curr->next, down);
+            down = curr->next; 
+            insert = rand() & 1; 
+        }
+
+        if (insert) {
+            head_ = make_shared<Node>(INT_MIN, nullptr, head_);
+        }
+    }
+    
+    bool erase(int num) {
+        bool found = false;
+        for (auto node = head_; node; node = node->down) {
+            while (node->next && node->next->val < num) {
+                node = node->next;
+            }
+            if (node->next && node->next->val == num) {
+                found = true;
+                node->next = node->next->next;
+            }
+        }
+        return found;
+    }
+
+private:
+    struct Node {
+        int val;
+        shared_ptr<Node> next;
+        shared_ptr<Node> down;
+        Node(int _val = INT_MIN,
+             shared_ptr<Node> _next = nullptr,
+             shared_ptr<Node> _down = nullptr):
+             val(_val), next(_next), down(_down) {}
+    };
+    
+    shared_ptr<Node> head_;
+};
+
+/**
  * Your Skiplist object will be instantiated and called as such:
  * Skiplist* obj = new Skiplist();
  * bool param_1 = obj->search(target);
