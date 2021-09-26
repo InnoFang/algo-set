993195fd9a6350567a7b37f57a57e403bb030aad
InnoFang <innofang@outlook.com>
2021-09-26 15:43:39 +0800

    :art: update T371 cpp solution

--- LeetCode/0371. Sum of Two Integers/solution.cpp
+++ LeetCode/0371. Sum of Two Integers/solution.cpp
@@ -9,3 +9,20 @@ public:
         return a == 0 ? b : getSum((unsigned int) (a & b) << 1, a ^ b);
     }
 };
+
+/**
+ * 13 / 13 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 5.9 MB 
+ */
+class Solution2 {
+public:
+    int getSum(int a, int b) {
+        while (a) {
+            unsigned int carry = (unsigned int) (a & b) << 1;
+            b = a ^ b;
+            a = carry;
+        }
+        return b;
+    }
+};
