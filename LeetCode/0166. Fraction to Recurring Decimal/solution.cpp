02a160ca01983c5bfd9fc808de2199b238656b82
InnoFang <innofang@outlook.com>
2021-10-03 15:04:02 +0800

    :art: update T166 cpp solution

--- LeetCode/0166. Fraction to Recurring Decimal/solution.cpp
+++ LeetCode/0166. Fraction to Recurring Decimal/solution.cpp
@@ -32,3 +32,36 @@ public:
         return decm + frac;
     }
 };
+
+/**
+ * 39 / 39 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 6.1 MB 
+ */
+class Solution2 {
+public:
+    using ll = long long;
+    string fractionToDecimal(int numerator, int denominator) {
+        ll n = numerator;
+        ll d = denominator;
+        if (n % d == 0) {
+            return to_string(n / d);
+        }
+
+        string ans = ((n < 0) ^ (d < 0)) ? "-" : "";
+        n = abs(n), d = abs(d);
+        ans += to_string(n / d) + ".";
+
+        unordered_map<ll, int> store;
+        for (int i = ans.size(); (n = n % d * 10) != 0; i++) {
+            if (store.count(n)) {
+                ans.insert(ans.begin() + store[n], '(');
+                ans += ')';
+                break;
+            }
+            store[n] = i;
+            ans += to_string(n / d);
+        }
+        return ans;
+    }
+};
