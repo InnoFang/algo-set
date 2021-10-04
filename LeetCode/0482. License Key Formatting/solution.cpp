866ff65b2593a2a7bb78d26fe7462deb01024cca
InnoFang <innofang@outlook.com>
2021-10-04 16:22:22 +0800

    :art: update T482 cpp solution

--- LeetCode/0482. License Key Formatting/solution.cpp
+++ LeetCode/0482. License Key Formatting/solution.cpp
@@ -19,3 +19,31 @@ public:
         return license;
     }
 };
+
+/**
+ * 38 / 38 test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 8.9 MB 
+ */
+class Solution2 {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        string license = "";
+        for (auto& c: s) {
+            if (c == '-') continue;
+            if ('a' <= c && c <= 'z') c -= 32;
+            license += c;
+        }  
+        int sz = license.size();
+        int first = sz % k;
+        if (first == 0) first = k;
+        string ans = license.substr(0, first);
+        ans += "-";
+        for (int i = first; i < sz; i += k) {
+            ans += license.substr(i, k);
+            ans += "-";
+        }
+        if (ans.back() == '-') ans.pop_back();
+        return ans;
+    }
+};
