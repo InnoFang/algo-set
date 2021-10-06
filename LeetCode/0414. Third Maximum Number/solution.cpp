4f9beb7e970649764f9e35545210985d87504c6f
InnoFang <innofang@outlook.com>
2021-10-06 14:50:14 +0800

    :art: update T414 cpp solution

--- LeetCode/0414. Third Maximum Number/solution.cpp
+++ LeetCode/0414. Third Maximum Number/solution.cpp
@@ -16,3 +16,28 @@ public:
         return s.size() == 3 ? *s.begin() : *s.rbegin();
     }
 };
+
+/**
+ * 31 / 31 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 8.8 MB 
+ */
+class Solution2 {
+public:
+    int thirdMax(vector<int>& nums) {
+        long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+        for (const int& num: nums) {
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (first > num && num > second) {
+                third = second;
+                second = num;
+            } else if (second > num && num > third) {
+                third = num;
+            }
+        }
+        return third == LONG_MIN ? first : third; 
+    }
+};
