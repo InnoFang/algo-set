79b18861dbfe6b875a9d524e5c9696cff00f3abf
InnoFang <innofang@outlook.com>
2021-10-10 12:43:46 +0800

    :art: update T352 cpp solution

--- LeetCode/0352. Data Stream as Disjoint Intervals/solution.cpp
+++ LeetCode/0352. Data Stream as Disjoint Intervals/solution.cpp
@@ -39,6 +39,50 @@ private:
 };
 
 /**
+ * 9 / 9 test cases passed.
+ * Runtime: 44 ms
+ * Memory Usage: 32.7 MB 
+ */
+class SummaryRanges2 {
+public:
+    SummaryRanges() { }
+    void addNum(int val) {
+        auto up = interval.upper_bound(val);
+        auto down = (up == interval.begin() ? interval.end() : prev(up));
+
+        if (down != interval.end() && (down->first <= val && val <= down->second)) {
+
+        } else if (down != interval.end() && down->second + 1 == val && val + 1 == up->first) {
+            int l = down->first, r = up->second;
+            interval.erase(down->first);
+            interval.erase(up->first);
+            interval.emplace(l, r);
+        } else if (down != interval.end() && down->second + 1 == val) {
+            int l = down->first, r = val;
+            interval.erase(down->first);
+            interval.emplace(l, r);
+        } else if (up != interval.end() && val + 1 == up->first) {
+            int l = val, r = up->second;
+            interval.erase(up->first);
+            interval.emplace(l, r);
+        } else {
+            interval.emplace(val, val);
+        }
+    }
+    
+    vector<vector<int>> getIntervals() {
+        vector<vector<int>> ret;
+        for (auto &[l, r]: interval) {
+            ret.push_back({l, r});
+        }
+        return ret;
+    }
+
+private:
+    map<int, int> interval;
+};
+
+/**
  * Your SummaryRanges object will be instantiated and called as such:
  * SummaryRanges* obj = new SummaryRanges();
  * obj->addNum(val);
