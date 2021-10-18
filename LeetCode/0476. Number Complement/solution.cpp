3387c32f8b1ef3dd02d2613d9d47500fed25917a
InnoFang <innofang@outlook.com>
2021-10-18 16:48:29 +0800

    :art: update T476 cpp solution

--- LeetCode/0476. Number Complement/solution.cpp
+++ LeetCode/0476. Number Complement/solution.cpp
@@ -9,3 +9,21 @@ public:
         return ((1ul << (32 - __builtin_clz(num))) - 1) ^ num;
     }
 };
+
+/**
+ * 149 / 149 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 5.8 MB 
+ */
+class Solution2 {
+public:
+    int findComplement(int num) {
+        int mask = num;
+        mask |= mask >> 1;
+        mask |= mask >> 2;
+        mask |= mask >> 4;
+        mask |= mask >> 8;
+        mask |= mask >> 16;
+        return mask ^ num;
+    }
+};
