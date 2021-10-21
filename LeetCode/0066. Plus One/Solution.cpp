702ab41d370997733e1911654179fe719991aabf
InnoFang <innofang@outlook.com>
2021-10-21 23:00:40 +0800

    :art: update T66 cpp solution

--- LeetCode/0066. Plus One/Solution.cpp
+++ LeetCode/0066. Plus One/Solution.cpp
@@ -17,3 +17,23 @@ public:
         return digits;
     }
 };
+
+// 111 / 111 test cases passed.
+// Status: Accepted
+// Runtime: 4 ms
+class Solution2 {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] == 9) digits[i] = 0;
+            else {
+                digits[i] += 1;
+                return digits;
+            }
+        }  
+        vector<int> ans(n + 1);
+        ans[0] = 1;
+        return ans;
+    }
+};
