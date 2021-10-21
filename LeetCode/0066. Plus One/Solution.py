8701a39ff02ba26a0378dcb3d33833375e0f0ed7
InnoFang <innofang@outlook.com>
2021-10-21 23:03:54 +0800

    :art: update T66 python solution

--- LeetCode/0066. Plus One/Solution.py
+++ LeetCode/0066. Plus One/Solution.py
@@ -12,3 +12,22 @@ class Solution:
         num = reduce(lambda acc, x: acc * 10 + x, digits)
         num += 1
         return list(map(lambda x: int(x), str(num)))
+
+class Solution2:
+    """
+    111 / 111 test cases passed.
+    Status: Accepted
+    Runtime: 32 ms
+    ----------------
+    :type digits: List[int]
+    :rtype: List[int]
+    """
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + [0] * n
