405e751079022f78374154f12df55043b07d7b26
InnoFang <innofang@outlook.com>
2021-10-22 10:12:46 +0800

    :art: update T229 cpp solution

--- LeetCode/0229. Majority Element II/solution.cpp
+++ LeetCode/0229. Majority Element II/solution.cpp
@@ -20,3 +20,45 @@ public:
         return ans;
     }
 };
+
+/**
+ * 82 / 82 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 15.4 MB 
+ */
+class Solution2 {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        auto cand1 = make_pair(0, 0);
+        auto cand2 = make_pair(0, 0);
+        
+        // voting stage
+        for (auto &num : nums) {
+            if (num == cand1.first) cand1.second++;
+            else if (num == cand2.first) cand2.second++;
+            else if (cand1.second && cand2.second) {
+                cand1.second--;
+                cand2.second--;
+            } else if (!cand1.second) {
+                cand1.first = num;
+                cand1.second = 1;
+            } else if (!cand2.second) {
+                cand2.first = num;
+                cand2.second = 1;
+            }
+        }
+
+        // counting stage
+        cand1.second = cand2.second = 0;
+        for (auto &num : nums) {
+            if (num == cand1.first) cand1.second++;
+            else if (num == cand2.first) cand2.second++;
+        }
+
+        int boundary = nums.size() / 3;
+        vector<int> ans;
+        if (cand1.second > boundary) ans.emplace_back(cand1.first);
+        if (cand2.second > boundary) ans.emplace_back(cand2.first);
+        return ans;
+    }
+};
