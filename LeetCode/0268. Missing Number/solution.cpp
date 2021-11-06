adf7b6bcb5438e304758805d63f82aa0113f941d
InnoFang <innofang@outlook.com>
2021-11-06 21:10:16 +0800

    :art: update T268 cpp solution

--- LeetCode/0268. Missing Number/solution.cpp
+++ LeetCode/0268. Missing Number/solution.cpp
@@ -14,3 +14,19 @@ public:
         return ans;
     }
 };
+
+/**
+ * 122 / 122 test cases passed.
+ * Runtime: 12 ms
+ * Memory Usage: 17.5 MB 
+ */
+class Solution2 {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            ans ^= (i + 1) ^ (nums[i]);
+        }
+        return ans;
+    }
+};
