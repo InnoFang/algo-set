30f30a5ac959b246148f2e5ace491254ac8d092a
InnoFang <innofang@outlook.com>
2021-11-10 22:19:45 +0800

    :art: update T495 cpp solution

--- LeetCode/0495. Teemo Attacking/solution.cpp
+++ LeetCode/0495. Teemo Attacking/solution.cpp
@@ -18,3 +18,19 @@ public:
         return ans;
     }
 };
+
+/**
+ * 38 / 38 test cases passed.
+ * Runtime: 32 ms
+ * Memory Usage: 25.2 MB 
+ */
+class Solution2 {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int ans = duration;
+        for (int i = 1; i < timeSeries.size(); ++i) {
+            ans += min(duration, timeSeries[i] - timeSeries[i - 1]);
+        }
+        return ans;
+    }
+};
