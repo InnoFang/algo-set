19f936f4a10dad1ae9e8d6bfc5e2362e3d62ca10
InnoFang <innofang@outlook.com>
2021-11-13 19:12:49 +0800

    :art: update T520 cpp solution

--- LeetCode/0520. Detect Capital/solution.cpp
+++ LeetCode/0520. Detect Capital/solution.cpp
@@ -20,3 +20,26 @@ public:
         return true;
     }
 };
+
+/**
+ * 550 / 550 test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 5.9 MB 
+ */
+class Solution2 {
+public:
+    bool detectCapitalUse(string word) {    
+        int type = word[0] >= 'a' ? 0 : 1;
+        if (type && word.size() > 1 && word[1] >= 'a') type = 0;
+        if (type) {
+            for (int i = 1; i < word.size(); i++) {
+                if (word[i] >= 'a') return false;
+            }
+        } else {
+            for (int i = 1; i < word.size(); i++) {
+                if (word[i] < 'a') return false;
+            }
+        }  
+        return true;
+    }
+};
