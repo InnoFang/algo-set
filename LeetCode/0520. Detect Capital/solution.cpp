8d21cdbc5c44aa1332ac5d9cf34143e1374fa533
InnoFang <innofang@outlook.com>
2021-11-13 19:15:07 +0800

    :art: update T520 cpp solution

--- LeetCode/0520. Detect Capital/solution.cpp
+++ LeetCode/0520. Detect Capital/solution.cpp
@@ -29,17 +29,16 @@ public:
 class Solution2 {
 public:
     bool detectCapitalUse(string word) {    
-        int type = word[0] >= 'a' ? 0 : 1;
-        if (type && word.size() > 1 && word[1] >= 'a') type = 0;
-        if (type) {
+        bool allUpper = word[0] < 'a' && !(word.size() > 1 && word[1] >= 'a');
+        if (allUpper) {
             for (int i = 1; i < word.size(); i++) {
-                if (word[i] >= 'a') return false;
+                if (word[i] >= 'a') return  false;
             }
         } else {
             for (int i = 1; i < word.size(); i++) {
-                if (word[i] < 'a') return false;
+                if (word[i] < 'a') return  false;
             }
-        }  
+        }
         return true;
     }
 };
