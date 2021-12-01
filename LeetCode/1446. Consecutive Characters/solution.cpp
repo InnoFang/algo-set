5d465515f8f0ad568d19eb3148bf50430ae993fb
InnoFang <innofang@outlook.com>
2021-12-01 09:59:34 +0800

    :art: update T1446 cpp solution

--- LeetCode/1446. Consecutive Characters/solution.cpp
+++ LeetCode/1446. Consecutive Characters/solution.cpp
@@ -20,3 +20,24 @@ public:
         return ans;
     }
 };
+
+/**
+ * 333 / 333 test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 6.6 MB 
+ */
+class Solution2 {
+public:
+    int maxPower(string s) {
+        int ans = 1, length = 1;
+        for(int r = 1; r < s.size(); ++r) {
+            if (s[r] == s[r - 1]) {
+                ++ length;                
+                ans = max(ans, length);
+            } else { 
+                length = 1;
+            }
+        } 
+        return ans;
+    }
+};
