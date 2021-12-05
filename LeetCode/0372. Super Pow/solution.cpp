d81be7210d8aca5bddc4dd45cec681cedc587c24
InnoFang <innofang@outlook.com>
2021-12-05 14:11:47 +0800

    :art: update T372 cpp solution

--- LeetCode/0372. Super Pow/solution.cpp
+++ LeetCode/0372. Super Pow/solution.cpp
@@ -21,3 +21,30 @@ public:
         return ret;
     }
 };
+
+/**
+ * 55 / 55 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 11.4 MB 
+ */
+class Solution2 {
+public:
+    const int mod = 1337;
+    int superPow(int a, vector<int>& b) {
+        int ans = 1;
+        for (int i = b.size() - 1; i >= 0; --i) {
+            ans = ans * quick_mod(a, b[i]) % mod;
+            a = quick_mod(a, 10);
+        }
+        return ans;
+    }
+    size_t quick_mod(size_t x, int y) {
+        size_t ret = 1;
+        while (y) {
+            if (y & 1) ret = (ret * x) % mod;
+            x  = x * x % mod;
+            y >>= 1;
+        }
+        return ret;
+    }
+};
