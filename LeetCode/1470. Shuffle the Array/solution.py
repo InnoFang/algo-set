ca63b63fa66d0537ab5c213ec73d928a086b8164
InnoFang <innofang@outlook.com>
2021-12-14 19:23:28 +0800

    :art: update T1470 python solution

--- LeetCode/1470. Shuffle the Array/solution.py
+++ LeetCode/1470. Shuffle the Array/solution.py
@@ -6,4 +6,14 @@ Memory Usage: 15.2 MB
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         return [nums[(i >> 1) + n] if i & 1 else nums[(i >> 1)] for i in range(2 * n)]
+
+
+"""
+53 / 53 test cases passed.
+Runtime: 50 ms
+Memory Usage: 15.1 MB
+"""
+class Solution2:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        return sum(zip(nums[:n], nums[n:]), ())
     
