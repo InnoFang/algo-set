0e9d2a8d318ee928cb174f4113afdcb929a72861
InnoFang <innofang@outlook.com>
2021-12-16 09:44:49 +0800

    :sparkles: upload  T21 python solution

---
+++ LeetCode/0021. Merge Two Sorted Lists/solution.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+"""
+208 / 208 test cases passed.
+Runtime: 32 ms
+Memory Usage: 14.9 MB
+"""
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
