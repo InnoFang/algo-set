0a3e8506a62e023e86e99cb099013082a3ce858a
InnoFang <innofang@outlook.com>
2021-12-22 21:57:35 +0800

    :sparkles: upload T686 python solution

---
+++ LeetCode/0686. Repeated String Match/solution.py
@@ -0,0 +1,23 @@
+"""
+57 / 57 test cases passed.
+Runtime: 40 ms
+Memory Usage: 15.1 MB
+"""
+from itertools import reduce
+import operator
+
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        a_bmp = reduce(operator.or_, map(lambda x: 1 << ord(x), list(a)), 0)
+        b_bmp = reduce(operator.or_, map(lambda x: 1 << ord(x), list(b)), 0)
+        if a_bmp & b_bmp != b_bmp:
+            return -1
+        times = (len(b) - 1) // len(a) + 1
+        temp = a * times
+        if b in temp:
+            return times
+        temp += a
+        if b in temp:
+            return times + 1
+        else:
+            return -1
