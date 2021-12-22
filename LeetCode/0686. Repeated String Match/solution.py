25926ad41dc15d1a6ccffa68d80294e101503b53
InnoFang <innofang@outlook.com>
2021-12-22 21:58:45 +0800

    :art: update T686 python solution

--- LeetCode/0686. Repeated String Match/solution.py
+++ LeetCode/0686. Repeated String Match/solution.py
@@ -21,3 +21,23 @@ class Solution:
             return times + 1
         else:
             return -1
+
+"""
+57 / 57 test cases passed.
+Runtime: 24 ms
+Memory Usage: 15 MB
+"""
+class Solution2:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        a_set, b_set = set(a), set(b)
+        if a_set & b_set != b_set:
+            return -1
+        times = (len(b) - 1) // len(a) + 1
+        temp = a * times
+        if b in temp:
+            return times
+        temp += a
+        if b in temp:
+            return times + 1
+        else:
+            return -1
