929250492ce6bfc35839e941632b906671a84fbc
InnoFang <innofang@outlook.com>
2021-12-31 21:36:15 +0800

    :art: update T507 python solution

--- LeetCode/0507. Perfect Number/solution.py
+++ LeetCode/0507. Perfect Number/solution.py
@@ -6,3 +6,19 @@ Memory Usage: 14.9 MB
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         return num in [6, 28, 496, 8128, 33550336]
+
+"""
+98 / 98 test cases passed.
+Runtime: 40 ms
+Memory Usage: 15.1 MB
+"""
+class Solution2:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num == 1:
+            return False
+        total = 1
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                total += i
+                total += num / i if i * i != num else 0
+        return total == num
