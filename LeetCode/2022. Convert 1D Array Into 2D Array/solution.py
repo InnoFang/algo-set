473a1ae5e00588f6488599b55037671277095786
InnoFang <innofang@outlook.com>
2022-01-01 15:52:19 +0800

    :art: update T2022 python solution

--- LeetCode/2022. Convert 1D Array Into 2D Array/solution.py
+++ LeetCode/2022. Convert 1D Array Into 2D Array/solution.py
@@ -13,3 +13,11 @@ class Solution:
                 mat[i][j] = original[i * n + j]
         return mat
     
+"""
+107 / 107 test cases passed.
+Runtime: 56 ms
+Memory Usage: 19.8 MB
+"""
+class Solution2:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        return [original[i:i+n] for i in range(0, len(original), n)] if len(original) == m * n else []
