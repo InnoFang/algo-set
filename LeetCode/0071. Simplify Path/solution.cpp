9c0769047059c83633ebaecac742280bd7b1ae3d
InnoFang <innofang@outlook.com>
2022-01-06 10:39:30 +0800

    :sparkles: upload T71 cpp solution

---
+++ LeetCode/0071. Simplify Path/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * 256 / 256 test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 7.6 MB 
+ */
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> p;
+        string sub;
+        path += "/";
+        for (auto &c : path) {
+            if (c == '/') {
+                if (sub == "." || sub == "") { 
+
+                } else if (sub == "..") {
+                    if (!p.empty()) p.pop_back();
+                } else if (!sub.empty()) {
+                    p.push_back(sub);
+                }
+                sub.clear();
+            } else {
+                sub += c;
+            }
+        } 
+        if (p.empty()) return "/";
+        string ret = "";
+        for (auto &sub : p) {
+            ret += "/";
+            ret += sub;
+        }
+        return ret;
+    }
+};
