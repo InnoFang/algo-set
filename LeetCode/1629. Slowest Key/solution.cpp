29845621108fe25cea1d9639f0464f529691d8a1
InnoFang <innofang@outlook.com>
2022-01-09 14:51:40 +0800

    :art: update T1629 cpp solution

--- LeetCode/1629. Slowest Key/solution.cpp
+++ LeetCode/1629. Slowest Key/solution.cpp
@@ -22,3 +22,27 @@ public:
         return keysPressed[max_cost_idx];
     }
 };
+
+/**
+ * 109 / 109 test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 10.7 MB 
+ */
+class Solution2 {
+public:
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        int n = keysPressed.size();
+        vector<int> cost(n);
+        int max_cost = releaseTimes[0];
+        char slowest_key = keysPressed[0];
+        for (int i = 1; i < n; ++ i) {
+            int cost = releaseTimes[i] - releaseTimes[i - 1];
+            if (cost > max_cost ||
+                cost == max_cost && keysPressed[i] > slowest_key) {
+                    slowest_key = keysPressed[i];
+                    max_cost = cost;
+            }
+        }
+        return slowest_key;
+    }
+};
