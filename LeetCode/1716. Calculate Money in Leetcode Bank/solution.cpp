b003c0585fbfa71cdf9363a20521cdcdee8e7a7b
InnoFang <innofang@outlook.com>
2022-01-15 11:42:46 +0800

    :art: update T1716 cpp solution

--- LeetCode/1716. Calculate Money in Leetcode Bank/solution.cpp
+++ LeetCode/1716. Calculate Money in Leetcode Bank/solution.cpp
@@ -15,3 +15,20 @@ public:
         return ans;
     }
 };
+
+
+/**
+ * 106 / 106 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 5.8 MB 
+ */
+class Solution2 {
+public:
+    int totalMoney(int n) {
+        int row = n / 7, col = n % 7;
+        int a1 = (1 + 7) * 7 / 2, an = (row + row + 6) * 7 / 2;
+        int ans = (a1 + an) * row / 2;
+        ans += col * (row + 1 + row + col) / 2;
+        return ans;
+    }
+};
