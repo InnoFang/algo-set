bb57db87facbacddb123e32cb206c337ca20bc3a
InnoFang <innofang@outlook.com>
2022-01-19 21:15:11 +0800

    :art: update T219 go solution

--- LeetCode/0219. Contains Duplicate II/solution.go
+++ LeetCode/0219. Contains Duplicate II/solution.go
@@ -16,3 +16,21 @@ func containsNearbyDuplicate(nums []int, k int) bool {
     }
     return false
 }
+
+/**
+ * 51 / 51 test cases passed.
+ * Runtime: 104 ms
+ * Memory Usage: 11 MB 
+ */
+func containsNearbyDuplicate(nums []int, k int) bool {
+    m := make(map[int]int, 0)
+    for i := 0; i < len(nums); i++ {
+        if _, ok := m[nums[i]]; ok {
+            if i - m[nums[i]] <= k {
+                return true
+            }
+        }
+        m[nums[i]] = i
+    }
+    return false
+}
