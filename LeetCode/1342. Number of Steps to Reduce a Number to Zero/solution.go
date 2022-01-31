f643da735a3edfd2b15d85025f0143f9a90bdd52
InnoFang <innofang@outlook.com>
2022-01-31 19:41:18 +0800

    :sparkles: upload T1342 go solution

--- LeetCode/1342. Number of Steps to Reduce a Number to Zero/solution.go
+++ LeetCode/1342. Number of Steps to Reduce a Number to Zero/solution.go
@@ -1,16 +1,17 @@
 /**
- * 60 / 60 test cases passed.
+ * 204 / 204 test cases passed.
  * Runtime: 0 ms
- * Memory Usage: 5.9 MB 
+ * Memory Usage: 1.9 MB 
  */
-func twoSum(nums []int, target int) []int { 
-    store := make(map[int]int)
-    for idx, n := range nums {
-        if diffIdx, ok := store[target - n]; ok {
-            return []int{diffIdx, idx}
-        } else {
-            store[n] = idx
+func numberOfSteps(num int) int {
+    step := 0 
+    for num > 0 {
+        if num & 1 == 1 { 
+            num-- 
+        } else { 
+            num >>= 1 
         }
+        step++
     }
-    return []int{}
+    return step
 }
