d738dfea45e54c78fd3bbca549a2fd0cbbfa925b
InnoFang <innofang@outlook.com>
2022-02-04 22:33:14 +0800

    :art: update T1725 cpp solution

--- LeetCode/1725. Number Of Rectangles That Can Form The Largest Square/solution.cpp
+++ LeetCode/1725. Number Of Rectangles That Can Form The Largest Square/solution.cpp
@@ -16,3 +16,25 @@ public:
         return cnt[maxLen];
     }
 };
+
+/**
+ * 68 / 68 test cases passed.
+ * Runtime: 44 ms
+ * Memory Usage: 17.9 MB 
+ */
+class Solution2 {
+public:
+    int countGoodRectangles(vector<vector<int>>& rectangles) { 
+        int maxLen = 0, ans = 0;
+        for (auto &rect: rectangles) {
+            int len = min(rect[0], rect[1]);
+            if (len > maxLen) {
+                maxLen = len;
+                ans = 1;
+            } else if (len == maxLen) {
+                ++ ans;
+            }
+        }
+        return ans;
+    }
+};
