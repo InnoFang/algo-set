719aa855f7a1f4936a165ce851f7c779ad349b8b
InnoFang <innofang@outlook.com>
2022-02-10 22:59:12 +0800

    :art: update T1447 cpp solution

--- LeetCode/1447. Simplified Fractions/solution.cpp
+++ LeetCode/1447. Simplified Fractions/solution.cpp
@@ -19,3 +19,22 @@ public:
         return ans;
     }
 };
+
+/**
+ * 100 / 100 test cases passed.
+ * Runtime: 44 ms
+ * Memory Usage: 21.4 MB 
+ */
+class Solution2 {
+public:
+    vector<string> simplifiedFractions(int n) { 
+        vector<string> ans;
+        for (int i = 1; i <= n - 1; ++ i) {
+            for (int j = i + 1; j <= n; ++ j) {  
+                if (__gcd(i, j) == 1)
+                    ans.push_back(to_string(i) + "/" + to_string(j));  
+            }
+        }
+        return ans;
+    }
+};
