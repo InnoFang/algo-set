3d0b9a83fa9859608bd5db38bf6c0e24fb9c91db
InnoFang <innofang@outlook.com>
2022-02-14 23:17:16 +0800

    :art: update T540 cpp solution

--- LeetCode/0540. Single Element in a Sorted Array/solution.cpp
+++ LeetCode/0540. Single Element in a Sorted Array/solution.cpp
@@ -11,3 +11,21 @@ public:
         return ans;
     }
 };
+
+/**
+ * 15 / 15 test cases passed.
+ * Runtime: 20 ms
+ * Memory Usage: 21.8 MB 
+ */
+class Solution2 {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == nums[mid ^ 1]) l = mid + 1;
+            else r = mid;
+        }
+        return nums[l];
+    }
+};
