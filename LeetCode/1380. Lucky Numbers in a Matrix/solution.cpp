7e14c3f65a3c7e45b029fe29494464ccc8bf2b35
InnoFang <innofang@outlook.com>
2022-02-15 22:58:06 +0800

    :sparkles: upload T1380 cpp solution

---
+++ LeetCode/1380. Lucky Numbers in a Matrix/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * 104 / 104 test cases passed.
+ * Runtime: 20 ms
+ * Memory Usage: 11.3 MB 
+ */
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> minn(m, INT_MAX);
+        vector<int> maxn(n);
+        for (int i = 0; i < m; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                minn[i] = min(minn[i], matrix[i][j]);
+                maxn[j] = max(maxn[j], matrix[i][j]);
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < m; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                if (matrix[i][j] == minn[i] && matrix[i][j] == maxn[j]) {
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
