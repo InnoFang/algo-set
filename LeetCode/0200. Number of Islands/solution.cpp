04c408af4db758a194065dc25d16a127ec8ba7a3
InnoFang <innofang@outlook.com>
2022-02-18 21:22:27 +0800

    :art: update T200 cpp solution

--- LeetCode/0200. Number of Islands/solution.cpp
+++ LeetCode/0200. Number of Islands/solution.cpp
@@ -35,3 +35,38 @@ public:
         return ans;
     }
 };
+
+/**
+ * 49 / 49 test cases passed.
+ * Runtime: 32 ms
+ * Memory Usage: 12.3 MB 
+ */
+class Solution2 {
+public:
+    int direct[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    vector<vector<bool>> used;
+    void dfs(vector<vector<char>> &grid, int x, int y) {
+       used[x][y] = true;
+       for (auto &d : direct) {
+           int xx = x + d[0];
+           int yy = y + d[1];
+           if (xx < 0 || xx >= grid.size() || yy < 0 || yy >= grid[0].size()) continue;
+           if (grid[xx][yy] == '0' || used[xx][yy]) continue;
+           dfs(grid, xx, yy);
+       }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int ans = 0;
+        int m = grid.size(), n = grid[0].size();
+        used.resize(m, vector<bool>(n));
+        for (int i = 0; i < grid.size(); ++ i) {
+            for (int j = 0; j < grid[0].size(); ++ j) {
+                if (grid[i][j] == '1' && !used[i][j]) {
+                    ++ ans;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return ans;
+    }
+};
