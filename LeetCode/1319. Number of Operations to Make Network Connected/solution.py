be60446601d4db35be24a0f66abc29483c7ace03
InnoFang <innofang@outlook.com>
2022-03-02 00:08:11 +0800

    :art: update T1319 python solution

--- LeetCode/1319. Number of Operations to Make Network Connected/solution.py
+++ LeetCode/1319. Number of Operations to Make Network Connected/solution.py
@@ -26,3 +26,44 @@ class Solution:
                 ans += 1
         return ans - 1
        
+
+"""
+36 / 36 test cases passed.
+Runtime: 148 ms
+Memory Usage: 26.3 MB
+"""
+class Solution2:
+    class UF:
+        def __init__(self, n):
+            self.parent = list(range(n))
+            self.rank = [1] * n
+            self.n = n
+            self.count = n
+
+        def find(self, i):
+            if self.parent[i] != i:
+                self.parent[i] = self.find(self.parent[i])
+            return self.parent[i]
+        
+        def union(self, i, j):
+            x, y = self.find(i), self.find(j)
+            if x == y:
+                return False
+            if self.rank[x] < self.rank[y]:
+                x, y = y, x
+            self.parent[y] = x
+            self.rank[x] += self.rank[y]
+            self.count -= 1
+            return True
+        
+        def connected(self, i, j):
+            return self.find(i) == self.find(j)
+
+    
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if n - 1 > len(connections):
+            return -1
+        uf = self.UF(n)
+        for x, y in connections:
+            uf.union(x, y)
+        return uf.count - 1
