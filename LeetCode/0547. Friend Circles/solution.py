23d5db2121beb745bc6d7b5cbffa44532a5f738d
InnoFang <innofang@outlook.com>
2022-03-01 23:28:52 +0800

    :art: update T547 python solution

--- LeetCode/0547. Friend Circles/solution.py
+++ LeetCode/0547. Friend Circles/solution.py
@@ -17,4 +17,28 @@ class Solution:
                 dfs(i)
                 ans += 1
         return ans
+
+"""
+113 / 113 test cases passed.
+Runtime: 48 ms
+Memory Usage: 15.4 MB
+"""
+class Solution2:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        parent = list(range(n))
+
+        def find(i):
+            if parent[i] != i:
+                parent[i] = find(parent[i])
+            return parent[i]
+
+        def union(i, j):
+            parent[find(i)] = find(j)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    union(i, j)
+        return sum(parent[i] == i for i in range(n))
         
