e88096fcd323767741c4e3315a0fcc01219301ba
InnoFang <innofang@outlook.com>
2022-03-02 12:16:49 +0800

    :art: update T53 python solution

--- LeetCode/0053. Maximum Subarray/solution.py
+++ LeetCode/0053. Maximum Subarray/solution.py
@@ -1,7 +1,7 @@
 """
-203 / 203 test cases passed.
-Runtime: 32 ms
-Memory Usage: 15.3 MB
+209 / 209 test cases passed.
+Runtime: 248 ms
+Memory Usage: 26 MB
 """
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
@@ -11,3 +11,19 @@ class Solution:
             dp[i + 1] = max(dp[i] + num, num)
             ans = max(ans, dp[i + 1])
         return ans
+
+
+"""
+209 / 209 test cases passed.
+Runtime: 180 ms
+Memory Usage: 25.7 MB
+"""
+class Solution2:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ans, prev = nums[0], 0
+        for i, num in enumerate(nums):
+            curr = max(prev + num, num)
+            ans = max(ans, curr)
+            prev = curr
+        return ans
+        
