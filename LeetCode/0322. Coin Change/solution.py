4b5273a6fe012a4382a78ed02744289f9dd701bc
InnoFang <innofang@outlook.com>
2022-03-02 19:47:51 +0800

    :sparkles: upload T322 python solution

---
+++ LeetCode/0322. Coin Change/solution.py
@@ -0,0 +1,13 @@
+"""
+188 / 188 test cases passed.
+Runtime: 1116 ms
+Memory Usage: 15.1 MB
+"""
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+        return dp[amount] if dp[amount] <= amount else -1
