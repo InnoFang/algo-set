d1e406015be43765452982804c37bdc215bbd9f9
InnoFang <innofang@outlook.com>
2022-03-03 10:36:41 +0800

    :sparkles: upload T122 python solution

---
+++ LeetCode/0122. Best Time to Buy and Sell Stock II/solution.py
@@ -0,0 +1,12 @@
+"""
+200 / 200 test cases passed.
+Runtime: 40 ms
+Memory Usage: 15.9 MB
+"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                ans += prices[i] - prices[i - 1]
+        return ans
