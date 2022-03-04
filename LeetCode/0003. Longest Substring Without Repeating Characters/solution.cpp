fa1423a9e708e29b19727dcc7b234b887562dbb9
InnoFang <innofang@outlook.com>
2022-03-05 01:40:22 +0800

    :zap: update T3 cpp solution

--- LeetCode/0003. Longest Substring Without Repeating Characters/solution.cpp
+++ LeetCode/0003. Longest Substring Without Repeating Characters/solution.cpp
@@ -20,3 +20,26 @@ public:
         return ans;
     }
 };
+
+
+/**
+ * 987 / 987 test cases passed.
+ * Runtime: 12 ms
+ * Memory Usage: 8.1 MB 
+ */
+class Solution2 {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l = 0, ans = 0;
+        unordered_map<char, int> last_occured;
+        for (int r = 0; r < s.size(); ++ r) {
+
+            if (last_occured.count(s[r]) && last_occured[s[r]] >= l) {
+                l = last_occured[s[r]] + 1;
+            }
+            ans = max(ans, r - l + 1);
+            last_occured[s[r]] = r;
+        }
+        return ans;
+    }
+};
