2a155ba37550adc550c8a402bee68c197608398c
InnoFang <innofang@outlook.com>
2022-03-10 17:12:12 +0800

    :sparkles: upload T343 python solution

---
+++ LeetCode/0343. Integer Break/solution.py
@@ -0,0 +1,13 @@
+"""
+50 / 50 test cases passed.
+Runtime: 40 ms
+Memory Usage: 14.9 MB
+"""
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            for j in range(i):
+                dp[i] = max(dp[i], j * (i - j), j * dp[i - j])
+        return dp[n]
+    
