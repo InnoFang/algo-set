feb80428b8688e0544abfff275951dc15752a0c4
InnoFang <innofang@outlook.com>
2022-03-11 11:33:38 +0800

    :sparkles: upload T518 python solution

---
+++ LeetCode/0518. Coin Change 2/solution.py
@@ -0,0 +1,13 @@
+"""
+28 / 28 test cases passed.
+Runtime: 124 ms
+Memory Usage: 15 MB
+"""
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [1] + [0] * amount
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+        return dp[amount]
+    
