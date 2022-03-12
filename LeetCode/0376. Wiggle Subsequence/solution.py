6e84a5f552104dedf02f6311aebf4509bdbcca33
InnoFang <innofang@outlook.com>
2022-03-12 11:16:43 +0800

    :zap: update T376 python solution

--- LeetCode/0376. Wiggle Subsequence/solution.py
+++ LeetCode/0376. Wiggle Subsequence/solution.py
@@ -18,3 +18,30 @@ class Solution:
                 elif nums[i] < nums[j]:
                     dp[i][1] = max(dp[i][1], dp[j][0] + 1) 
         return max(dp[n - 1][0], dp[n - 1][1])
+
+
+"""
+26 / 26 test cases passed.
+Runtime: 48 ms
+Memory Usage: 15 MB
+"""
+class Solution2:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return n
+        # dp[i][0] increase
+        # dp[i][1] decrease
+        dp = [[0] * 2 for _ in range(n)]
+        dp[0][0] = dp[0][1] = 1
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                dp[i][0] = max(dp[i][0], dp[i - 1][1] + 1)
+                dp[i][1] = dp[i - 1][1]
+            elif nums[i] < nums[i - 1]:
+                dp[i][0] = dp[i - 1][0]
+                dp[i][1] = max(dp[i][1], dp[i - 1][0] + 1) 
+            else:
+                dp[i][0] = dp[i - 1][0]
+                dp[i][1] = dp[i - 1][1]
+        return max(dp[n - 1][0], dp[n - 1][1])
