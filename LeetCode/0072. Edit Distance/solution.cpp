758a59f25fad9d416878228b5a2dd05156acca99
InnoFang <innofang@outlook.com>
2022-03-13 23:45:12 +0800

    :sparkles: upload T72 cpp solution

---
+++ LeetCode/0072. Edit Distance/solution.cpp
@@ -0,0 +1,24 @@
+/**
+ * 1146 / 1146 test cases passed.
+ * Runtime: 8 ms
+ * Memory Usage: 8.8 MB 
+ */
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n1 = word1.size(), n2 = word2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+        for (int i = 0; i <= n1; ++ i) dp[i][0] = i;
+        for (int i = 0; i <= n2; ++ i) dp[0][i] = i;
+        for (int i = 1; i <= n1; ++ i) {
+            for (int j = 1; j <= n2; ++ j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+};
