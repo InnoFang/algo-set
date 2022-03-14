4a1cb6ca70d82be2ddec90099dc2d57354a017da
InnoFang <innofang@outlook.com>
2022-03-14 11:04:39 +0800

    :art: update T599 python solution

--- LeetCode/0599. Minimum Index Sum of Two Lists/solution.py
+++ LeetCode/0599. Minimum Index Sum of Two Lists/solution.py
@@ -17,3 +17,17 @@ class Solution:
                     ans.append(res)
         return ans
         
+
+"""
+136 / 136 test cases passed.
+Runtime: 64 ms
+Memory Usage: 15.4 MB
+"""
+class Solution2:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        rec1 = {res : i for i, res in enumerate(list1)}
+        rec2 = {res : i for i, res in enumerate(list2)}
+        agreements = set(list1) & set(list2)
+        sum_index = {res : rec1[res] + rec2[res] for res in agreements}
+        least = min(sum_index.values())
+        return [res for res, i in sum_index.items() if i == least]
