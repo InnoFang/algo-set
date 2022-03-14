dea9b74de2581919245147d27cf5b978e50048e9
InnoFang <innofang@outlook.com>
2022-03-14 19:34:22 +0800

    :sparkles: upload T120 python solution

---
+++ LeetCode/0120. Triangle/solution.py
@@ -0,0 +1,11 @@
+"""
+44 / 44 test cases passed.
+Runtime: 56 ms
+Memory Usage: 15.8 MB
+"""
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        return triangle[0][0]
