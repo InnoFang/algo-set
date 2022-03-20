3ffcd1c8ea75f3c06d6b6564322fe42475ac1495
InnoFang <innofang@outlook.com>
2022-03-20 22:07:21 +0800

    :sparkles: upload T714 solution

---
+++ LeetCode/0714. Best Time to Buy and Sell Stock with Transaction Fee/solution.py
@@ -0,0 +1,14 @@
+"""
+44 / 44 test cases passed.
+Runtime: 472 ms
+Memory Usage: 24.8 MB
+"""
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n)]
+        dp[0][0] = -prices[0]
+        for i in range(1, n):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee)
+        return dp[n - 1][1]
