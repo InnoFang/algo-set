e190f1652d2bd5b44515115b0f1bec69cf497218
InnoFang <innofang@outlook.com>
2022-03-22 10:07:32 +0800

    :sparkles: upload T206 java solution

---
+++ LeetCode/0206. Reverse Linked List/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/**
+ * 28 / 28 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 41.3 MB 
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode node = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+}
