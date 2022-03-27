2bcd6a08a2f9709b63d4683c90aa3cc277bc0046
InnoFang <innofang@outlook.com>
2022-03-27 19:56:15 +0800

    :sparkles: upload T931 python solution

---
+++ LeetCode/0931. Minimum Falling Path Sum/solution.py
@@ -0,0 +1,17 @@
+"""
+49 / 49 test cases passed.
+Runtime: 56 ms
+Memory Usage: 15.8 MB
+"""
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        for i in range(1, m):
+            for j in range(n):
+                if j == 0:
+                    matrix[i][j] += min(matrix[i - 1][j], matrix[i - 1][j + 1])
+                elif j == n - 1:
+                    matrix[i][j] += min(matrix[i - 1][j - 1], matrix[i - 1][j])
+                else:
+                    matrix[i][j] += min(matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i - 1][j + 1])
+        return min(matrix[m - 1])
