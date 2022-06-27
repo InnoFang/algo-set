12f5b3bc867f809a7570375eba544076b4f1bba2
InnoFang <innofang@outlook.com>
2022-06-27 22:48:59 +0800

    :sparkles: upload #2235 cpp solution

---
+++ LeetCode/2235. Add Two Integers/solution.cpp
@@ -0,0 +1,11 @@
+/**
+ * 262 / 262 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 5.8 MB 
+ */
+class Solution {
+public:
+    int sum(int num1, int num2) {
+       return num1 + num2; 
+    }
+};
