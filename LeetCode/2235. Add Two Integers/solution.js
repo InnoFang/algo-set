08bb72344419498a76b4deb2d04e3cac9ac1f313
InnoFang <innofang@outlook.com>
2022-06-27 22:49:38 +0800

    :sparkles: upload #2235 JavaScript solution

---
+++ LeetCode/2235. Add Two Integers/solution.js
@@ -0,0 +1,12 @@
+/**
+ * 262 / 262 test cases passed.
+ * Runtime: 64 ms
+ * Memory Usage: 41.1 MB 
+ *
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+ var sum = function(num1, num2) {
+    return num1 + num2;
+};
