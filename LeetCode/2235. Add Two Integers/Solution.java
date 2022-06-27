2724e4842aa18744d748fbd7cd469a64e4885bbf
InnoFang <innofang@outlook.com>
2022-06-27 22:49:22 +0800

    :sparkles: upload #2235 java solution

---
+++ LeetCode/2235. Add Two Integers/Solution.java
@@ -0,0 +1,10 @@
+/**
+ * 262 / 262 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 38.4 MB 
+ */
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
