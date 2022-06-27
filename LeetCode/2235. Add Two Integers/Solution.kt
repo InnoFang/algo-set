0cc87e7a4ec5246d7093d70cdad2bef2f1132a61
InnoFang <innofang@outlook.com>
2022-06-27 22:49:50 +0800

    :sparkles: upload #2235 kotlin solution

---
+++ LeetCode/2235. Add Two Integers/Solution.kt
@@ -0,0 +1,10 @@
+/**
+ * 262 / 262 test cases passed.
+ * Runtime: 160 ms
+ * Memory Usage: 32.2 MB 
+ */
+class Solution {
+    fun sum(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
