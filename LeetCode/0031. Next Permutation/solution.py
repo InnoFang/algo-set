c0e32b587b5cc3be7c30ab07b51bf3e1ea05b62e
InnoFang <innofang@outlook.com>
2022-07-03 23:28:01 +0800

    :sparkles: upload #31 python solution

---
+++ LeetCode/0031. Next Permutation/solution.py
@@ -0,0 +1,24 @@
+"""
+265 / 265 test cases passed.
+Runtime: 44 ms
+Memory Usage: 14.9 MB
+"""
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while j >= 0 and nums[i] >= nums[j]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
