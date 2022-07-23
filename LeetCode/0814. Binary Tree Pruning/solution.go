c9637fe5a502ce91af1ccb11e8c30618e8eed7bc
InnoFang <innofang@outlook.com>
2022-07-23 23:42:50 +0800

    :sparkles: upload #814 go solution

---
+++ LeetCode/0814. Binary Tree Pruning/solution.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/**
+ * 30 / 30 test cases passed.
+ * Runtime: 0 ms
+ * Memory Usage: 2.2 MB
+ */
+func pruneTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = pruneTree(root.Left)
+	root.Right = pruneTree(root.Right)
+	if root.Left == nil && root.Right == nil && root.Val == 0 {
+		return nil
+	}
+	return root
+}
