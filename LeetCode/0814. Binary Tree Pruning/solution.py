a386453d320cea7d7ba4efad3daf3b59ac80b6e2
InnoFang <innofang@outlook.com>
2022-07-23 23:43:03 +0800

    :sparkles: upload #814 python solution

---
+++ LeetCode/0814. Binary Tree Pruning/solution.py
@@ -0,0 +1,20 @@
+"""
+30 / 30 test cases passed.
+Runtime: 36 ms
+Memory Usage: 14.9 MB
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if root.left is None and root.right is None and root.val == 0:
+            return None
+        return root
