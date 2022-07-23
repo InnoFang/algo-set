4e8c2185a9bfa855300e09a168ad531a6d9e2897
InnoFang <innofang@outlook.com>
2022-07-23 23:42:40 +0800

    :sparkles: upload #814 cpp solution

---
+++ LeetCode/0814. Binary Tree Pruning/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/**
+ * 30 / 30 test cases passed.
+ * Runtime: 4 ms
+ * Memory Usage: 8.5 MB 
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if (!root) return nullptr;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (!root->left && !root->right && root->val == 0) return nullptr;
+        return root;
+    }
+};
