479062d02f3ba3f269da6d0dc5787d0aad5fd841
InnoFang <innofang@outlook.com>
2022-08-02 23:26:58 +0800

    :sparkles: upload #622 python solution

---
+++ LeetCode/0622. Design Circular Queue/solution.py
@@ -0,0 +1,47 @@
+"""
+58 / 58 test cases passed.
+Runtime: 76 ms
+Memory Usage: 15.6 MB
+"""
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        return n % 4 != 0
+    class MyCircularQueue:
+        def __init__(self, k: int):
+        self.front = self.rear = 0
+        self.elements = [0] * (k + 1)
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.elements[self.rear] = value
+        self.rear = (self.rear + 1) % len(self.elements)
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) % len(self.elements)
+        return True
+
+    def Front(self) -> int:
+        return -1 if self.isEmpty() else self.elements[self.front]
+
+    def Rear(self) -> int:
+        return -1 if self.isEmpty() else self.elements[(self.rear - 1) % len(self.elements)]
+
+    def isEmpty(self) -> bool:
+        return self.rear == self.front
+
+    def isFull(self) -> bool:
+        return (self.rear + 1) % len(self.elements) == self.front
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
