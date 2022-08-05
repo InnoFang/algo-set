7059cfb778636cede7914c66408b922d3082be62
InnoFang <innofang@outlook.com>
2022-08-06 00:09:25 +0800

    :sparkles: update #623 python solution

--- LeetCode/0623. Add One Row to Tree/solution.py
+++ LeetCode/0623. Add One Row to Tree/solution.py
@@ -26,3 +26,22 @@ class Solution:
             node.left = TreeNode(val, node.left, None)
             node.right = TreeNode(val, None, node.right)
         return root
+
+"""
+109 / 109 test cases passed.
+Runtime: 60 ms
+Memory Usage: 18.2 MB
+"""
+class Solution2:
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        if root == None:
+            return
+        if depth == 1:
+            return TreeNode(val, root, None)
+        if depth == 2:
+            root.left = TreeNode(val, root.left, None)
+            root.right = TreeNode(val, None, root.right)
+        else:
+            root.left = self.addOneRow(root.left, val, depth - 1)
+            root.right = self.addOneRow(root.right, val, depth - 1)
+        return root
