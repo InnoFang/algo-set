25fd71ce9ff07494d4ae012f40f7e8f1277c3660
InnoFang <innofang@outlook.com>
2022-08-14 18:15:51 +0800

    :sparkles: upload #1422 python solution

---
+++ LeetCode/1422. Maximum Score After Splitting a String/solution.py
@@ -0,0 +1,12 @@
+"""
+104 / 104 test cases passed.
+Runtime: 48 ms
+Memory Usage: 14.9 MB
+"""
+class Solution:
+    def maxScore(self, s: str) -> int:
+        ans = score = (s[0] == '0') + s[1:].count('1')
+        for c in s[1:-1]:
+            score += 1 if c == '0' else -1
+            ans = max(ans, score)
+        return ans
