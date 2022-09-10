9fe6c6edae2e3352d419e1673d38be7c3bcfb71e
InnoFang <innofang@outlook.com>
2022-09-10 16:02:59 +0800

    :sparkles: upload #669 python solution

---
+++ LeetCode/0669. Trim a Binary Search Tree/solution.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+78 / 78 test cases passed.
+Runtime: 52 ms
+Memory Usage: 19.1 MB
+"""
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        if not root:
+            return root;
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+        else:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+            return root
+ 
