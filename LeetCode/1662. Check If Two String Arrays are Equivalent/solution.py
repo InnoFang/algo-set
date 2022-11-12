9d796fb05f2219d45259faa220c03076d79929a3
InnoFang <innofang@outlook.com>
2022-11-12 23:42:07 +0800

    :sparkles: upload #1662 python solution

---
+++ LeetCode/1662. Check If Two String Arrays are Equivalent/solution.py
@@ -0,0 +1,8 @@
+"""
+Runtime: 32 ms
+Memory Usage: 15.1 MB
+"""
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return ''.join(word1) == ''.join(word2)
+        
