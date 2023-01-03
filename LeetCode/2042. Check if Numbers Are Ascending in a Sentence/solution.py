8c2410fdc187422b79887ed7dc3de66fbde085cf
InnoFang <innofang@outlook.com>
2023-01-03 23:35:08 +0800

    :sparkles: upload #2042 python solution

---
+++ LeetCode/2042. Check if Numbers Are Ascending in a Sentence/solution.py
@@ -0,0 +1,13 @@
+"""
+Runtime: 44 ms
+Memory Usage: 15 MB
+"""
+class Solution:
+    def areNumbersAscending(self, s: str) -> bool:
+        pre = 0
+        for t in s.split():
+            if t[0].isdigit():
+                if (cur := int(t)) <= pre:
+                    return False
+                pre = cur
+        return True
