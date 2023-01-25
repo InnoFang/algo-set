3efcb5dfa24c87b8d0cfa11f9fca6e9b9341adf8
InnoFang <innofang@outlook.com>
2023-01-25 18:43:19 +0800

    :sparkles: upload 1281st cpp solution

---
+++ LeetCode/1281. Subtract the Product and Sum of Digits of an Integer/solution.cpp
@@ -0,0 +1,16 @@
+/**
+ * Runtime: 0 ms
+ * Memory Usage: 5.7 MB 
+ */
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int p = 1, s = 0;
+        while (n > 0) {
+            p *= n % 10;
+            s += n % 10;
+            n /= 10;
+        }
+        return p - s;
+    }
+};
