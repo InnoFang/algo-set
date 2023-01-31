e5bd16ffa6b1979a084205a439d10fabe26b2244
InnoFang <innofang@outlook.com>
2023-01-31 23:24:35 +0800

    :sparkles: upload 2319th cpp solution

---
+++ LeetCode/2319. Check if Matrix Is X-Matrix/solution.cpp
@@ -0,0 +1,20 @@
+/**
+ * Runtime: 28 ms
+ * Memory Usage: 15.9 MB 
+ */
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                if (i == j || i + j == n - 1) {
+                    if (!grid[i][j]) return false;
+                } else if (grid[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
