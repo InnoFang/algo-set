bca2e0450576d81474712a2350dff0b11b29908e
InnoFang <innofang@outlook.com>
2023-02-22 10:11:37 +0800

    :sparkles: upload 1370th python solution

--- LeetCode/1370. Increasing Decreasing String/solution.py
+++ LeetCode/1370. Increasing Decreasing String/solution.py
@@ -12,3 +12,24 @@ class Solution:
             reverse = not reverse
             cnt -= Counter(cnt.keys())
         return ''.join(ans)
+
+
+"""
+Runtime: 52 ms
+Memory Usage: 14.9 MB
+"""
+class Solution2:
+    def sortString(self, s: str) -> str:
+        cnt = dict(sorted(Counter(s).items(), key=lambda x: x[0]))
+        l = list(cnt)
+        ans = ''
+        while len(ans) < len(s):
+            for i in l:
+                if cnt[i] > 0:
+                    ans += i
+                    cnt[i] -= 1
+            for i in l[::-1]:
+                if cnt[i] > 0:
+                    ans += i
+                    cnt[i] -= 1
+        return ans
